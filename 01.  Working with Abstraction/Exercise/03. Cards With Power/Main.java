b0fad8d901cf3ee797b5b58e867bb2b2286778f1
wolfie <yavorhristozov@gmail.com>
2024-02-04 13:53:34 +0200

    refact : Main
    minor changes in code formatting

--- 01.  Working with Abstraction/Exercise/03. Cards With Power/Main.java
+++ 01.  Working with Abstraction/Exercise/03. Cards With Power/Main.java
@@ -1,14 +1,17 @@
+import enums.CardRank;
+import enums.CardSuit;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String rank = scanner.nextLine();
-        String suit = scanner.nextLine();
 
-        Card card = new Card(CardRanks.valueOf(rank), CardSuits.valueOf(suit));
+        String currentCard = scanner.nextLine();
+        String currentSuit = scanner.nextLine();
+
+        Card card = new Card(CardRank.valueOf(currentCard), CardSuit.valueOf(currentSuit));
 
         System.out.println(card);
-
     }
 }
