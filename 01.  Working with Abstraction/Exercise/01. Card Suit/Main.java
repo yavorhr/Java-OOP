e9624f24a0a4084e09458b45800f58d5c2026001
wolfie <yavorhristozov@gmail.com>
2024-02-04 12:56:52 +0200

    refact : Main
    minor changes in code formatting

--- 01.  Working with Abstraction/Exercise/01. Card Suit/Main.java
+++ 01.  Working with Abstraction/Exercise/01. Card Suit/Main.java
@@ -1,10 +1,19 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Card Suits:");
-        for (CardSuits suit : CardSuits.values()) {
-            System.out.println(String.format("Ordinal value: %d; Name value: %s", suit.ordinal(), suit));
-        }
-        System.out.println();
+        printOutput();
+    }
+
+    private static void printOutput() {
+        StringBuilder sb = new StringBuilder("Card Suits:\n");
+
+        Arrays.stream(CardSuit
+                .values())
+                .forEach(c ->
+                        sb.append(String.format("Ordinal value: %d; Name value: %s\n", c.ordinal(), c)));
+
+        System.out.println(sb);
     }
 }
