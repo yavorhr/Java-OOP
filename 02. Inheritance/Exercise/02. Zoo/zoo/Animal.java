ea812183eba306e7616f7c7b2158c1c3edafda4a
wolfie <yavorhristozov@gmail.com>
2024-02-08 17:03:40 +0200

    add : zoo package
    Add package zoo with all animal classes

---
+++ 02. Inheritance/Exercise/02. Zoo/zoo/Animal.java
@@ -0,0 +1,13 @@
+package zoo;
+
+public abstract class Animal {
+  private String name;
+
+  public Animal(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+}
---
+++ 02. Inheritance/Exercise/02. Zoo/zoo/Mammal.java
@@ -0,0 +1,7 @@
+package zoo;
+
+public abstract class Mammal extends Animal {
+    public Mammal(String name ){
+        super(name);
+    }
+}
