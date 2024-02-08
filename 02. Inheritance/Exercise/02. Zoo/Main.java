d78b6bce20d8d21169fd783f19009defb9edbd70
wolfie <yavorhristozov@gmail.com>
2024-02-08 17:02:26 +0200

    refact : Main
    extend Main

--- 02. Inheritance/Exercise/02. Zoo/Main.java
+++ 02. Inheritance/Exercise/02. Zoo/Main.java
@@ -1,2 +1,24 @@
+package zoo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+  public static void main(String[] args) {
+    Animal bear = new Bear("Pete");
+    Animal gorilla = new Gorilla("King Kong");
+    Animal snake = new Snake("Sassy");
+    Animal lizard = new Lizard("Sneaky");
+
+    List<Animal> animals = new ArrayList<>();
+
+    animals.add(gorilla);
+    animals.add(snake);
+    animals.add(lizard);
+    animals.add(bear);
+
+    animals.forEach(a -> System.out.println(a.getName()));
+
+  }
 }
