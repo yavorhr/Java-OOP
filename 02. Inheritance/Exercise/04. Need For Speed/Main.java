c4a345434a25395154b76838d3f0298f0c857e38
wolfie <yavorhristozov@gmail.com>
2024-02-08 20:12:55 +0200

    refact : Main
    extend the Main class by creating different types of Vehicles and adding them in a collection

--- 02. Inheritance/Exercise/04. Need For Speed/Main.java
+++ 02. Inheritance/Exercise/04. Need For Speed/Main.java
@@ -1,5 +1,23 @@
+import vehicle.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+
+        List<Vehicle> vehicles = new ArrayList<>();
+
+        driveVehicle(new SportCar(50, 350), vehicles);
+        driveVehicle(new RacerMotorcycle(50, 220), vehicles);
+        driveVehicle(new FamilyCar(50, 130), vehicles);
+        driveVehicle(new Car(50, 180), vehicles);
+
+        vehicles.forEach(System.out::println);
+    }
+
+    private static void driveVehicle(Vehicle vehicle, List<Vehicle> vehicles) {
+        vehicle.drive(200);
+        vehicles.add(vehicle);
     }
 }
