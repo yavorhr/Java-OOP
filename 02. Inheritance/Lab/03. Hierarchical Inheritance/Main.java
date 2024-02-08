a99865bc714c1538fb7cd6989c8fbf17c4169f99
wolfie <yavorhristozov@gmail.com>
2024-02-08 16:09:48 +0200

    refact : Main
    practice inheritance

--- 02. Inheritance/Lab/03. Hierarchical Inheritance/Main.java
+++ 02. Inheritance/Lab/03. Hierarchical Inheritance/Main.java
@@ -1,15 +1,20 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        Puppy puppy = new Puppy();
-        puppy.eat();
-        puppy.bark();
-        puppy.weep();
+        Dog dog = new Dog();
+        dog.eat();
+        dog.bark();
 
         Cat cat = new Cat();
+        cat.eat();
         cat.meow();
+
+        // methods are defined in the inherited classes,
+        // but objects Cat and Dog are initialized in the Animal class, which has no access to both methods
+
+        Animal cat2 = new Cat();
+        cat2.meow();
+        Animal dog2 = new Dog();
+        dog2.bark();
     }
 }
