c708ef4266163aeac4cea2a4d0d2e6b44a70cb70
wolfie <yavorhristozov@gmail.com>
2024-02-08 15:16:42 +0200

    practice inheritance

--- 02. Inheritance/Lab/Demo/Main.java
+++ 02. Inheritance/Lab/Demo/Main.java
@@ -4,8 +4,15 @@ public class Main {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
 
-    Animal wolf = new Wolf("nss", 31, false);
+    Animal wolf = new Wolf("Vulcho", 31, false);
     wolf.move();
+
+    //bark is implemented in Wolf class.
+    wolf.bark();
+
+    Wolf wolf2 = new Wolf("Anastas", 42, true);
+    wolf2.bark();
+
     System.out.println(wolf);
   }
 }
