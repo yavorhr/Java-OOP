2a4cdf750f8c9868d3cc103262c8e91cd252f8c7
wolfie <yavorhristozov@gmail.com>
2024-02-09 17:10:00 +0200

    delete : all Vehicle classes

--- 02. Inheritance/Exercise/04. Need For Speed/RaceMotorcycle.java
+++
@@ -1,9 +0,0 @@
-public class RaceMotorcycle extends Motorcycle {
-
-    protected final static double DEFAULT_FUEL_CONSUMPTION = 8.00;
-
-    public RaceMotorcycle(double fuel,int horsePower) {
-        super(fuel, horsePower);
-        setFuelConsumption(DEFAULT_FUEL_CONSUMPTION);
-    }
-}
--- 02. Inheritance/Exercise/04. Need For Speed/SportCar.java
+++
@@ -1,8 +0,0 @@
-public class SportCar extends Car {
-    protected final static double DEFAULT_FUEL_CONSUMPTION = 10.00;
-
-    public SportCar(double fuel, int horsePower) {
-        super(fuel, horsePower);
-        super.setFuelConsumption(DEFAULT_FUEL_CONSUMPTION);
-    }
-}
