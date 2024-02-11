158db3f549109216c4a4535294d24e3d258577b0
wolfie <yavorhristozov@gmail.com>
2024-02-11 11:04:42 +0200

    add : Product classes
    Add all product classes in separate "restaurant" package

---
+++ 02. Inheritance/Exercise/05. Restaurant/restaurant/HotBeverage.java
@@ -0,0 +1,9 @@
+package restaurant;
+
+import java.math.BigDecimal;
+
+public class HotBeverage extends Beverages{
+  public HotBeverage(String name, BigDecimal price, double milliliters) {
+    super(name, price, milliliters);
+  }
+}
