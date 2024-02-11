d42b1af7ab65f30c27cc7269e005aac697be6551
wolfie <yavorhristozov@gmail.com>
2024-02-11 13:05:36 +0200

    refact : Kitten class

--- 02. Inheritance/Exercise/06. Animals/Kitten.java
+++ 02. Inheritance/Exercise/06. Animals/Kitten.java
@@ -1,12 +1,14 @@
-public class Kitten extends Cat{
+package animals;
+
+public class Kitten extends Cat {
+    private static final String GENDER = "Female";
 
     public Kitten(String name, int age) {
-        super(name, age, "Female");
+        super(name, age, GENDER);
     }
 
     @Override
-    public String produceSound() {
-      return "Meow";
+    public void produceSound() {
+        System.out.println("Meow");
     }
 }
-
