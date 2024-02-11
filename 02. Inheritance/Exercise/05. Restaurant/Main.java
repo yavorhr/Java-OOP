8790c84dfd51a5e400d0cb66a149a04bc6483f0e
wolfie <yavorhristozov@gmail.com>
2024-02-11 11:53:34 +0200

    refact : extend Main
    Made instances of Tea and Coffee for testing purposes

--- 02. Inheritance/Exercise/05. Restaurant/Main.java
+++ 02. Inheritance/Exercise/05. Restaurant/Main.java
@@ -1,6 +1,21 @@
+import restaurant.Coffee;
+import restaurant.HotBeverage;
+import restaurant.Tea;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-    }
+    HotBeverage coffee = new Coffee("Cappuccino", 5);
+    HotBeverage tea = new Tea("Mint", BigDecimal.valueOf(31), 50);
+
+    List<HotBeverage> beverages = new ArrayList<>();
+    beverages.add(coffee);
+    beverages.add(tea);
+
+    beverages.forEach(System.out::println);
+  }
 }
