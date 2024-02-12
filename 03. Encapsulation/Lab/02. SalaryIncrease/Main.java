7e12f028d396c17989c24ffabe003ac9e29f104e
wolfie <yavorhristozov@gmail.com>
2024-02-12 15:59:34 +0200

    refact : Main
    Re-write the Main by extracting simple methods and rename some variables in order to :
    - reduce code complexity
    - make the program more readable
    
    The main functionality remains the same

--- 03. Encapsulation/Lab/02. SalaryIncrease/Main.java
+++ 03. Encapsulation/Lab/02. SalaryIncrease/Main.java
@@ -6,16 +6,41 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(reader.readLine());
+
         List<Person> people = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
+
+        while (n-- > 0) {
             String[] input = reader.readLine().split(" ");
-            people.add(new Person(input[0], input[1], Integer.parseInt(input[2]), Double.parseDouble(input[3])));
+            addPerson(people, input);
         }
+
         double bonus = Double.parseDouble(reader.readLine());
+
+        increaseSalaries(people, bonus);
+
+        printList(people);
+    }
+
+    private static void increaseSalaries(List<Person> people, double bonus) {
         for (Person person : people) {
             person.increaseSalary(bonus);
+        }
+    }
+
+    private static void addPerson(List<Person> people, String[] input) {
+        String firstName = input[0];
+        String lastName = input[1];
+        int age = Integer.parseInt(input[2]);
+        double salary = Double.parseDouble(input[3]);
+
+        people.add(new Person(firstName, lastName, age, salary));
+    }
+
+    private static void printList(List<Person> people) {
+        for (Person person : people) {
             System.out.println(person.toString());
         }
     }
