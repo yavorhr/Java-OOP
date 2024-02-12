ac84f66098573fe2933be5acf7690069563df2e9
wolfie <yavorhristozov@gmail.com>
2024-02-12 16:27:56 +0200

    refact : Person

--- 03. Encapsulation/Lab/04. First And Reserve Team/Person.java
+++ 03. Encapsulation/Lab/04. First And Reserve Team/Person.java
@@ -4,51 +4,12 @@ public class Person {
     private int age;
     private double salary;
 
-    public Person(String name, String lastName, int age, double salary) {
-        this.setFirstName(name);
+    public Person(String firstName, String lastName, int age, double salary) {
+        this.setFirstName(firstName);
         this.setLastName(lastName);
         this.setAge(age);
         this.setSalary(salary);
     }
-
-    private void validateNameLength(String name, String exceptionPrefixMessage) {
-        if (name.length() < 3) {
-            throw new IllegalArgumentException(exceptionPrefixMessage + " cannot be less than 3 symbols");
-        }
-    }
-
-    private void setSalary(double salary) {
-        if (salary < 460 ) {
-            throw new IllegalArgumentException("Salary cannot be less than 460 leva");
-        }
-        this.salary = salary;
-    }
-
-    public void setFirstName(String firstName) {
-        validateNameLength(firstName, "First name");
-        this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        validateNameLength(lastName, "Last name");
-        this.lastName = lastName;
-    }
-
-
-    public void setAge(int age) {
-        if (age < 0) {
-            throw new IllegalArgumentException("Age cannot be zero or negative integer");
-        }
-        this.age=age;
-    }
-
-    public void increaseSalary(double bonus) {
-        if (this.age < 30) {
-            bonus /= 2;
-        }
-        this.salary = this.salary * (1 + bonus / 100);
-    }
-
 
     public String getFirstName() {
         return this.firstName;
@@ -66,9 +27,39 @@ public class Person {
         return this.salary;
     }
 
+    private void setSalary(double salary) {
+        if (salary < 460) {
+            throw new IllegalStateException("Salary cannot be less than 460 leva");
+        }
+        this.salary = salary;
+    }
+
+    public void setFirstName(String firstName) {
+        validateName("First name", firstName);
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        validateName("Last name", lastName);
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age) {
+        if (age <= 0) {
+            throw new IllegalStateException("Age cannot be zero or negative integer");
+        }
+        this.age = age;
+    }
 
     @Override
     public String toString() {
-        return String.format("%s %s gets %s leva", this.firstName, this.lastName, this.salary);
+        return String.format("%s %s gets %.1f leva",
+                this.firstName, this.lastName, this.salary);
+    }
+
+    private void validateName(String prefix, String name){
+        if (name.length() < 3) {
+            throw new IllegalStateException(prefix + " cannot be less than 3 symbols");
+        }
     }
 }
