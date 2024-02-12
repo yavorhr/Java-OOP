2a2cd34a37f63221b5897473d491d6b7efbbce15
wolfie <yavorhristozov@gmail.com>
2024-02-12 16:08:47 +0200

    refact : Main
    Re-write the program by extracting simple methods and rename some variables in order to :
    - reduce code complexity
    - make the program more readable
    
    The main functionality remains the same

--- 03. Encapsulation/Lab/03. Validation Data/Main.java
+++ 03. Encapsulation/Lab/03. Validation Data/Main.java
@@ -4,7 +4,6 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class Main {
     public static void main(String[] args) throws IOException {
 
@@ -12,17 +11,40 @@ public class Main {
         int n = Integer.parseInt(reader.readLine());
 
         List<Person> people = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            String[] input = reader.readLine().split(" ");
+
+        while (n-- > 0) {
             try {
-                people.add(new Person(input[0], input[1], Integer.parseInt(input[2]), Double.parseDouble(input[3])));
-            } catch (IllegalArgumentException ex) {
-                System.out.println(ex.getMessage());
+                String[] input = reader.readLine().split(" ");
+                addPerson(people, input);
+            } catch (Exception e){
+                System.out.println(e.getMessage());
             }
         }
+
         double bonus = Double.parseDouble(reader.readLine());
+
+        increaseSalaries(people, bonus);
+
+        printList(people);
+    }
+
+    private static void increaseSalaries(List<Person> people, double bonus) {
         for (Person person : people) {
             person.increaseSalary(bonus);
+        }
+    }
+
+    private static void addPerson(List<Person> people, String[] input) {
+        String firstName = input[0];
+        String lastName = input[1];
+        int age = Integer.parseInt(input[2]);
+        double salary = Double.parseDouble(input[3]);
+
+        people.add(new Person(firstName, lastName, age, salary));
+    }
+
+    private static void printList(List<Person> people) {
+        for (Person person : people) {
             System.out.println(person.toString());
         }
     }
