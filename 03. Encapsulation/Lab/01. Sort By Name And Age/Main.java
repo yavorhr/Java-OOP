d3c98cdf7a5d94c79f36f80be81d23012eae9e11
wolfie <yavorhristozov@gmail.com>
2024-02-12 15:22:23 +0200

    refact : Main
    Re-write the program by extracting simple methods and rename some variables in order to :
    - reduce code complexity
    - make the program more readable
    
    The main functionality remains the same

--- 03. Encapsulation/Lab/01. Sort By Name And Age/Main.java
+++ 03. Encapsulation/Lab/01. Sort By Name And Age/Main.java
@@ -2,31 +2,35 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(reader.readLine());
 
         List<Person> people = new ArrayList<>();
 
-        for (int i = 0; i < n; i++) {
+        while (n-- > 0) {
             String[] input = reader.readLine().split(" ");
-            people.add(new Person(input[0], input[1], Integer.parseInt(input[2])));
+            addPerson(people, input);
         }
 
-        Collections.sort(people, (firstPerson, secondPerson) -> {
-            int sComp = firstPerson.getFirstName().compareTo(secondPerson.getFirstName());
+        people.sort(new ComparatorByFirstNameAndAge());
 
-            if (sComp != 0) {
-                return sComp;
-            } else {
-                return Integer.compare(firstPerson.getAge(), secondPerson.getAge());
-            }
-        });
+        printList(people);
+    }
 
+    private static void addPerson(List<Person> people, String[] input) {
+        String firstName = input[0];
+        String lastName = input[1];
+        int age = Integer.parseInt(input[2]);
+
+        people.add(new Person(firstName, lastName, age));
+    }
+
+    private static void printList(List<Person> people) {
         for (Person person : people) {
             System.out.println(person.toString());
         }
