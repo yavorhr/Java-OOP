4337ec526341dd0771f3697cc16e5d34908f4c85
wolfie <yavorhristozov@gmail.com>
2024-02-13 14:47:42 +0200

    refact : Chicken class
    minor changes in code formatting

--- 03. Encapsulation/Exercise/02. Animal Farm/Chicken.java
+++ 03. Encapsulation/Exercise/02. Animal Farm/Chicken.java
@@ -3,40 +3,56 @@ public class Chicken {
     private int age;
 
     public Chicken(String name, int age) {
-        this.setName(name);
-        this.setAge(age);
-    }
-
-    private void setAge(int age) {
-        if (age < 0 || age > 15) {
-            throw new IllegalArgumentException("Age should be between 0 and 15.");
-        }
-        this.age = age;
+        setName(name);
+        setAge(age);
     }
 
     private void setName(String name) {
-        if (name == null || name.trim().isEmpty()) {
-            throw new IllegalArgumentException("Name cannot be empty.");
-        }
+        validateName(name);
         this.name = name;
+    }
+
+    private void setAge(int age) {
+        validateAge(age);
+        this.age = age;
     }
 
     public double productPerDay() {
         return calculateProductPerDay();
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
     private double calculateProductPerDay() {
         if (this.age < 6) {
-            return 2;
-        } else if (this.age < 12) {
-            return 1;
+            return 2.00;
+        } else if (this.age <= 11) {
+            return 1.00;
         } else {
             return 0.75;
         }
     }
 
+    private void validateAge(int age) {
+        if (age < 0 || age > 15) {
+            throw new IllegalStateException("Age should be between 0 and 15.");
+        }
+    }
+
+    private void validateName(String name) {
+        if (name == null || name.trim().length() <= 0) {
+            throw new IllegalStateException("Name cannot be empty.");
+        }
+    }
+
     @Override
     public String toString() {
-        return String.format("Chicken %s (age %d) can produce %.2f eggs per day.", this.name, this.age, productPerDay());
+        return String.format("Chicken %s (age %d) can produce %.2f eggs per day.", this.name, this.age, this.productPerDay());
     }
 }
