f745f4d749cc7e862bea723409e48ac887934965
wolfie <yavorhristozov@gmail.com>
2024-02-13 16:40:14 +0200

    refact : Product

--- 03. Encapsulation/Exercise/03. Shopping Spree/Product.java
+++ 03. Encapsulation/Exercise/03. Shopping Spree/Product.java
@@ -7,6 +7,14 @@ public class Product {
         this.setCost(cost);
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
     private void setName(String name) {
         if (!NameValidator.isValid(name)) {
             throw new IllegalArgumentException(ConstantMessages.INVALID_NAME_EXCEPTION_MESSAGE);
@@ -19,13 +27,5 @@ public class Product {
             throw new IllegalArgumentException(ConstantMessages.NEGATIVE_MONEY_EXCEPTION_MESSAGE);
         }
         this.cost = cost;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public double getCost() {
-        return this.cost;
     }
 }
