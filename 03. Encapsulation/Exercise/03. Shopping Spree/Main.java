4bdc99ad4818c66a492b950299c919bd12b24f02
wolfie <yavorhristozov@gmail.com>
2024-02-13 16:39:06 +0200

    refact : Main

--- 03. Encapsulation/Exercise/03. Shopping Spree/Main.java
+++ 03. Encapsulation/Exercise/03. Shopping Spree/Main.java
@@ -1,68 +1,79 @@
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        List<Person> people = Arrays.stream(scanner.nextLine().split(";")).map(personInfo -> {
-            String[] tokens = personInfo.split("=");
-            try {
-                return new Person(tokens[0], Double.parseDouble(tokens[1]));
-            } catch (IllegalArgumentException exception) {
-                System.out.println(exception.getMessage());
-            }
-            return null;
-        })
-                .collect(Collectors.toList());
+        String[] personTokens = scanner.nextLine().split(";");
+        String[] productTokens = scanner.nextLine().split(";");
 
-        List<Product> products = Arrays.stream(scanner.nextLine().split(";")).map(productInfo -> {
-            String[] tokens = productInfo.split("=");
-            try {
-                return new Product(tokens[0], Double.parseDouble(tokens[1]));
-            } catch (IllegalArgumentException exception) {
-                System.out.println(exception.getMessage());
-            }
-            return null;
-        })
-                .collect(Collectors.toList());
+        List<Person> people = new ArrayList<>();
+        List<Product> products = new ArrayList<>();
 
-        if (people.stream().anyMatch(Objects::isNull) || products.stream().anyMatch(Objects::isNull)) {
-            return;
-        }
+        addToList(people, products, personTokens, "person");
+        addToList(people, products, productTokens, "product");
 
         String input = scanner.nextLine();
-        while (!input.equals("END")) {
-            String[] tokens = input.split("\\s+");
-            String personName = tokens[0];
-            String product = tokens[1];
+        while (!"END".equals(input)) {
+            String[] inputTokens = input.split(" ");
+            String name = inputTokens[0];
+            String product = inputTokens[1];
 
-            for (Person p : people) {
-                if (p.getName().equals(personName)) {
-                    try {
-                        Product productToBuy = products.stream().filter(prod -> prod.getName().equals(product))
-                                .findFirst()
-                                .orElseThrow(Exception::new);
-
-                        p.buyProduct(productToBuy);
-                        System.out.println(personName + " bought " + product);
-                    } catch (IllegalArgumentException ex) {
-                        System.out.println(ex.getMessage());
-                    }
+            if (inputsAreValid(people, products, name, product)) {
+                try {
+                    Person person = getPerson(name, people);
+                    Product productObj = getProduct(product, products);
+                    person.buyProduct(productObj);
+                    System.out.println(name + " bought " + product);
+                } catch (Exception ex) {
+                    System.out.println(ex.getMessage());
                 }
             }
+
             input = scanner.nextLine();
         }
 
-        for (Person p : people) {
-            System.out.println(p.toString());
+        printOutput(people);
+    }
+
+    private static boolean inputsAreValid(List<Person> people, List<Product> products, String name, String product) {
+        return getPerson(name, people) != null && getProduct(product, products) != null;
+    }
+
+    private static void printOutput(List<Person> people) {
+        people.forEach(System.out::println);
+    }
+
+    private static Product getProduct(String product, List<Product> products) {
+        return products.stream().filter(p -> p.getName().equals(product)).collect(Collectors.toList()).stream().findFirst().orElse(null);
+    }
+
+    private static Person getPerson(String name, List<Person> people) {
+        return people.stream().filter(p -> p.getName().equals(name)).collect(Collectors.toList()).stream().findFirst().orElse(null);
+    }
+
+    private static void addToList(List<Person> people, List<Product> products, String[] tokens, String criteria) {
+        for (String token : tokens) {
+            String[] innerTokens = token.split("=");
+            String name = innerTokens[0];
+            double money = Double.parseDouble(innerTokens[1]);
+
+            try {
+                if (criteria.equals("person")) {
+                    Person person = new Person(name, money);
+                    people.add(person);
+                } else {
+                    Product product = new Product(name, money);
+                    products.add(product);
+                }
+            } catch (Exception exception) {
+                System.out.println(exception.getMessage());
+            }
         }
     }
+
 }
-
-
-
 
