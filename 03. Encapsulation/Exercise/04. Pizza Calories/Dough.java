eada41554697e10842b8ee976bba1fb6d46f4eae
wolfie <yavorhristozov@gmail.com>
2024-02-14 10:57:44 +0200

    refact : Dough class

--- 03. Encapsulation/Exercise/04. Pizza Calories/Dough.java
+++ 03. Encapsulation/Exercise/04. Pizza Calories/Dough.java
@@ -1,7 +1,6 @@
 import java.util.Map;
 
 public class Dough {
-
     private static final Map<String, Double> FLOUR_TYPES_WITH_MODIFIERS = Map.of(
             "White", 1.5,
             "Wholegrain", 1.0);
@@ -15,37 +14,36 @@ public class Dough {
     private String bakingTechnique;
     private double weight;
 
-
     public Dough(String flourType, String bakingTechnique, double weight) {
-        this.setBakingTechnique(bakingTechnique);
         this.setFlourType(flourType);
+        this.setBakingTechnique(bakingTechnique);
         this.setWeight(weight);
-    }
-
-    private void setWeight(double weight) {
-        if (weight < 1 || weight > 200) {
-            throw new IllegalArgumentException("Dough weight should be in the range [1..200].");
-        }
-        this.weight = weight;
-    }
-
-    private void setBakingTechnique(String bakingTechnique) {
-        if (!BAKING_TECHNIQUES.containsKey(bakingTechnique)) {
-            throw new IllegalArgumentException("Invalid type of dough.");
-        }
-        this.bakingTechnique = bakingTechnique;
     }
 
     private void setFlourType(String flourType) {
         if (!FLOUR_TYPES_WITH_MODIFIERS.containsKey(flourType)) {
-            throw new IllegalArgumentException("Invalid type of dough.");
+            throw new IllegalStateException(ConstantMessages.INVALID_DOUGH);
         }
         this.flourType = flourType;
     }
 
+    private void setBakingTechnique(String bakingTechnique) {
+        if (!BAKING_TECHNIQUES.containsKey(bakingTechnique)) {
+            throw new IllegalStateException(ConstantMessages.INVALID_DOUGH);
+        }
+        this.bakingTechnique = bakingTechnique;
+    }
+
+    private void setWeight(double weight) {
+        if (!Validator.isDoughWeightInRange(weight)) {
+            throw new IllegalArgumentException(ConstantMessages.INVALID_DOUGH_WEIGHT);
+        }
+        this.weight = weight;
+    }
+
     public double calculateCalories() {
-        return this.weight * 2
-                * FLOUR_TYPES_WITH_MODIFIERS.get(flourType)
-                * BAKING_TECHNIQUES.get(bakingTechnique);
+        return this.weight * 2 *
+                FLOUR_TYPES_WITH_MODIFIERS.get(this.flourType) *
+                BAKING_TECHNIQUES.get(this.bakingTechnique);
     }
 }
