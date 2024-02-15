28d2e0fd88fca8945da7dad5bd72e3d8b594953c
wolfie <yavorhristozov@gmail.com>
2024-02-15 13:21:45 +0200

    refact : Car interface
    practice OOP

--- 04.  Interfaces and Abstraction/Lab/01. Car Shop/Car.java
+++ 04.  Interfaces and Abstraction/Lab/01. Car Shop/Car.java
@@ -1,15 +1,7 @@
-import java.io.Serializable;
-
-public interface Car extends Serializable {
-    public static final Integer TIRES = 4;
-
+public interface Car {
+    static final int TIRES = 4;
     String getModel();
-
     String getColor();
-
     Integer getHorsePower();
-
     String countryProduced();
-
-
 }
