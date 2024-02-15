c1e3c875cb284fb7978c2e3bb19b25b560969d6c
wolfie <yavorhristozov@gmail.com>
2024-02-15 13:10:10 +0200

    refact : Validator class
    invert some boolean if-else checks

--- 03. Encapsulation/Exercise/05. Team Generator/Validator.java
+++ 03. Encapsulation/Exercise/05. Team Generator/Validator.java
@@ -1,9 +1,20 @@
+import java.util.List;
+import java.util.Map;
+
 public class Validator {
-  public static boolean validateName(String name) {
-    return name.trim().length() > 0;
+  public static boolean invalidName(String name) {
+    return name.trim().length() <= 0;
   }
 
-  public static boolean validateStat(int value) {
-    return value >= 0 && value <= 100;
+  public static boolean invalidStat(int value) {
+    return value < 0 || value > 100;
+  }
+
+  public static boolean doesTeamExist(Map<String, Team> teamsMap, String teamName) {
+    return teamsMap.containsKey(teamName);
+  }
+
+  public static boolean doesTeamContainPlayer(List<Player> players, Player player) {
+    return players.contains(player);
   }
 }
