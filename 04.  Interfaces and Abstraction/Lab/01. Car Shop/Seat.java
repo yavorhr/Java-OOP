78344c122c3cf5cc908dedd41871c23fd9417af1
wolfie <yavorhristozov@gmail.com>
2024-02-15 13:22:05 +0200

    refact : Seat
    refact toString method

--- 04.  Interfaces and Abstraction/Lab/01. Car Shop/Seat.java
+++ 04.  Interfaces and Abstraction/Lab/01. Car Shop/Seat.java
@@ -1,13 +1,12 @@
 import java.io.Serializable;
 
-public class Seat implements Car {
+public class Seat implements Serializable, Car {
     private String model;
     private String color;
-    private Integer horsePower;
+    private int horsePower;
     private String countryProduced;
 
-
-    public Seat(String model, String color, Integer horsePower, String countryProduced) {
+    public Seat(String model, String color, int horsePower, String countryProduced) {
         this.model = model;
         this.color = color;
         this.horsePower = horsePower;
@@ -36,7 +35,7 @@ public class Seat implements Car {
 
     @Override
     public String toString() {
-        return String.format("This is %s produced in %s and have 4 tires", model, color);
-
+        return String.format("This is %s produced in %s and have %d tires",
+                this.model, this.countryProduced, TIRES);
     }
 }
