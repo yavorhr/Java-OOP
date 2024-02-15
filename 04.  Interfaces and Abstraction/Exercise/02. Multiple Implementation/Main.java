33c88f94539bfef8b439cb5e61847b21ca48aa94
wolfie <yavorhristozov@gmail.com>
2024-02-15 17:12:05 +0200

    refact : Main

--- 04.  Interfaces and Abstraction/Exercise/02. Multiple Implementation/Main.java
+++ 04.  Interfaces and Abstraction/Exercise/02. Multiple Implementation/Main.java
@@ -4,29 +4,28 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
         Class[] citizenInterfaces = Citizen.class.getInterfaces();
+
         if (Arrays.asList(citizenInterfaces).contains(Birthable.class)
                 && Arrays.asList(citizenInterfaces).contains(Identifiable.class)) {
-            Method[] methods = Birthable.class.getDeclaredMethods();
-            methods = Identifiable.class.getDeclaredMethods();
-            Scanner scanner = new Scanner(System.in);
+            Method[] birthableMethods = Birthable.class.getDeclaredMethods();
+            Method[]   identifiableMethods = Identifiable.class.getDeclaredMethods();
+
             String name = scanner.nextLine();
             int age = Integer.parseInt(scanner.nextLine());
             String id = scanner.nextLine();
             String birthDate = scanner.nextLine();
-            Identifiable identifiable = new Citizen(name, age, id, birthDate);
-            Birthable birthable = new Citizen(name, age, id, birthDate);
-            System.out.println(methods.length);
-            System.out.println(methods[0].getReturnType().getSimpleName());
-            System.out.println(methods.length);
-            System.out.println(methods[0].getReturnType().getSimpleName());
+
+            Identifiable identifiable = new Citizen(name,age,id,birthDate);
+            Birthable birthable = new Citizen(name,age,id,birthDate);
+
+            System.out.println(identifiableMethods.length);
+            System.out.println(identifiableMethods[0].getReturnType().getSimpleName());
+            System.out.println(birthableMethods.length);
+            System.out.println(birthableMethods[0].getReturnType().getSimpleName());
         }
     }
 }
-
-
-
-
-
 
