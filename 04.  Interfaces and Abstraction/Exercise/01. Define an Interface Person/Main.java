335570b1ac67bd793ab322ffe6f4afd72ed55af2
wolfie <yavorhristozov@gmail.com>
2024-02-15 17:05:16 +0200

    refact Main
    minor changes in Code formatting

--- 04.  Interfaces and Abstraction/Exercise/01. Define an Interface Person/Main.java
+++ 04.  Interfaces and Abstraction/Exercise/01. Define an Interface Person/Main.java
@@ -4,28 +4,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         Class[] citizenInterfaces = Citizen.class.getInterfaces();
 
-        if (Arrays.asList(citizenInterfaces).contains(Person.class)) {
-            Method[] fields = Person.class.getDeclaredMethods();
+        if(Arrays.asList(citizenInterfaces).contains(Person.class)){
+            Method[] methods = Person.class.getDeclaredMethods();
 
             Scanner scanner = new Scanner(System.in);
             String name = scanner.nextLine();
             int age = Integer.parseInt(scanner.nextLine());
-            Person person = new Citizen(name, age);
 
-            System.out.println(fields.length);
+            Person person = new Citizen(name,age);
+
+            System.out.println(methods.length);
             System.out.println(person.getName());
             System.out.println(person.getAge());
         }
     }
 }
-
-
-
-
-
-
-
 
