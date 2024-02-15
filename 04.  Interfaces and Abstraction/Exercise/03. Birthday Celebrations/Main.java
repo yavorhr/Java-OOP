1dba5f12286c7493728f5cf9a6e78599d859ba87
wolfie <yavorhristozov@gmail.com>
2024-02-15 19:53:14 +0200

    refact : Main
    Re-write the program by extracting simple methods and rename some variables in order to :
    - reduce code complexity
    - make the program more readable
    
    The main functionality remains the same

--- 04.  Interfaces and Abstraction/Exercise/03. Birthday Celebrations/Main.java
+++ 04.  Interfaces and Abstraction/Exercise/03. Birthday Celebrations/Main.java
@@ -1,42 +1,49 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        boolean birthablesIsEmpty = true;
         List<Birthable> birthables = new ArrayList<>();
+
         String input = scanner.nextLine();
-        while (!input.equalsIgnoreCase("end")) {
-            String[] tokens = input.split("\\s+");
-            //String name, int age, String id, String birthDate
-            if (tokens[0].equals("Citizen")) {
-                birthables.add(new Citizen(tokens[1], Integer.parseInt(tokens[2]), tokens[3], tokens[4]));
-            } else if (tokens[0].equals("Pet")) {
-                birthables.add(new Pet(tokens[1], tokens[2]));
+        while (!"End".equals(input)) {
+            String[] tokens = input.split(" ");
+
+            if (!tokens[0].equals("Robot")) {
+                addToBirthables(tokens, birthables);
             }
+
             input = scanner.nextLine();
         }
 
-        String yearOutput = scanner.nextLine();
+        String date = scanner.nextLine();
+        printBirthdays(date, birthables);
+    }
 
-        for (Birthable birthable : birthables) {
-            if (birthable.getBirthDate().endsWith(yearOutput)) {
-                birthablesIsEmpty = false;
-                System.out.println(birthable.getBirthDate());
-            }
-        }
+    private static void printBirthdays(String birthday, List<Birthable> birthables) {
+        List<Birthable> filtered = birthables.stream()
+                .filter(i -> i.getBirthDate()
+                        .endsWith(birthday)).collect(Collectors.toList());
 
-        if (birthablesIsEmpty) {
+        if (filtered.isEmpty()) {
             System.out.println("<no output>");
+        } else {
+            filtered.forEach(b -> System.out.println(b.getBirthDate()));
         }
     }
+
+    private static void addToBirthables(String[] tokens, List<Birthable> birthables) {
+        if (tokens.length == 5) {
+            birthables.add(new Citizen(tokens[1], Integer.parseInt(tokens[2]), tokens[3], tokens[4]));
+        } else {
+            birthables.add(new Pet(tokens[1], tokens[2]));
+        }
+    }
+
 }
-
-
-
-
 
 
