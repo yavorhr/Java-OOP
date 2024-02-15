eb18af7e63658d9bfc1d9e415624122493cea02e
wolfie <yavorhristozov@gmail.com>
2024-02-15 13:10:24 +0200

    refact : Player

--- 03. Encapsulation/Exercise/05. Team Generator/Player.java
+++ 03. Encapsulation/Exercise/05. Team Generator/Player.java
@@ -8,50 +8,50 @@ public class Player {
 
   public Player(String name, int endurance, int sprint, int dribble, int passing, int shooting) {
     this.setName(name);
-    this.endurance = endurance;
-    this.sprint = sprint;
-    this.dribble = dribble;
-    this.passing = passing;
-    this.shooting = shooting;
+    this.setEndurance(endurance);
+    this.setSprint(sprint);
+    this.setDribble(dribble);
+    this.setPassing(passing);
+    this.setShooting(shooting);
   }
 
   private void setName(String name) {
-    if (!Validator.validateName(name)) {
+    if (Validator.invalidName(name)) {
       throw new IllegalArgumentException(ConstantMessages.INVALID_NAME);
     }
     this.name = name;
   }
 
   private void setEndurance(int endurance) {
-    if (!Validator.validateStat(endurance)) {
+    if (Validator.invalidStat(endurance)) {
       throw new IllegalArgumentException(ConstantMessages.invalidStatInout("Endurance"));
     }
     this.endurance = endurance;
   }
 
   private void setSprint(int sprint) {
-    if (!Validator.validateStat(sprint)) {
+    if (Validator.invalidStat(sprint)) {
       throw new IllegalArgumentException(ConstantMessages.invalidStatInout("Sprint"));
     }
     this.sprint = sprint;
   }
 
   private void setDribble(int dribble) {
-    if (!Validator.validateStat(dribble)) {
+    if (Validator.invalidStat(dribble)) {
       throw new IllegalArgumentException(ConstantMessages.invalidStatInout("Dribble"));
     }
     this.dribble = dribble;
   }
 
   private void setPassing(int passing) {
-    if (!Validator.validateStat(passing)) {
+    if (Validator.invalidStat(passing)) {
       throw new IllegalArgumentException(ConstantMessages.invalidStatInout("Passing"));
     }
     this.passing = passing;
   }
 
   private void setShooting(int shooting) {
-    if (!Validator.validateStat(shooting)) {
+    if (Validator.invalidStat(shooting)) {
       throw new IllegalArgumentException(ConstantMessages.invalidStatInout("Shooting"));
     }
     this.shooting = shooting;
@@ -66,6 +66,6 @@ public class Player {
   }
 
   private double calcSkillLevel() {
-    return this.endurance + this.sprint + this.shooting + this.dribble + this.passing / 5.00;
+    return (this.endurance + this.sprint + this.shooting + this.dribble + this.passing) / 5.00;
   }
 }
