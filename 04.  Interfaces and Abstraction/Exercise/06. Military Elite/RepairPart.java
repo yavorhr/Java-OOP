a7377e4faa5317049dc580cfc0e5e48aeca0870a
wolfie <yavorhristozov@gmail.com>
2024-02-16 16:29:52 +0200

    add : RepairPart class

---
+++ 04.  Interfaces and Abstraction/Exercise/06. Military Elite/RepairPart.java
@@ -0,0 +1,11 @@
+public class RepairPart {
+  private String number;
+  private int hoursWorked;
+
+  public RepairPart(String number, int hoursWorked) {
+    this.number = number;
+    this.hoursWorked = hoursWorked;
+  }
+
+
+}
