3dd0bea2cc951929645b8d087ef8f1d812c10323
wolfie <yavorhristozov@gmail.com>
2024-02-17 10:56:23 +0200

    refact : AddRemovableCollection
    refact the remove() and add() methods

--- 04.  Interfaces and Abstraction/Exercise/07. Collection Hierarchy/AddRemoveCollection.java
+++ 04.  Interfaces and Abstraction/Exercise/07. Collection Hierarchy/AddRemoveCollection.java
@@ -1,19 +1,20 @@
-public class AddRemoveCollection extends Collection implements AddRemovable, Addable {
+public class AddRemoveCollection extends Collection implements AddRemovable {
 
   @Override
   public String remove() {
     if (super.getList().isEmpty()) {
       return "List is empty, nothing to remove!";
     }
-    return super.getList().remove(0);
+    int lastIndex = super.getList().size() - 1;
+    return super.getList().remove(lastIndex);
   }
 
   @Override
   public int add(String string) {
     if (super.getMaxSize() > super.getList().size()) {
-      super.getList().add( string);
+      super.getList().add(0, string);
     }
-    return super.getList().size();
+    return super.getList().indexOf(string);
   }
 }
 
