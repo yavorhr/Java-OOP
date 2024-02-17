e091b38e020f0d0593fea3ce094726203367f2f4
wolfie <yavorhristozov@gmail.com>
2024-02-17 14:56:58 +0200

    refact : Circle class

--- 05. Polymorphism/Lab/02. Shapes/Circle.java
+++ 05. Polymorphism/Lab/02. Shapes/Circle.java
@@ -1,27 +1,26 @@
 public class Circle extends Shape {
-    private Double radius;
+    private double radius;
 
-    public Circle(Double radius) {
-        this.setRadius(radius);
+    public Circle(double radius) {
+        setRadius(radius);
         this.calculateArea();
         this.calculatePerimeter();
     }
 
-    private void setRadius(Double radius) {
-        this.radius = radius;
-    }
-
-    public final Double getRadius() {
-        return this.radius;
-    }
-
     @Override
     public void calculatePerimeter() {
-        setPerimeter(2 * Math.PI * this.radius);
+        super.setPerimeter( 2 * Math.PI * this.radius);
     }
 
     @Override
     public void calculateArea() {
-        setArea(Math.PI * this.radius * this.radius);
+        super.setArea(Math.PI * this.radius * this.radius);
+    }
+
+    private void setRadius(double radius) {
+        if (Validator.validateInput(radius)) {
+            throw new IllegalArgumentException(ConstantMessages.NOT_VALID_INPUT);
+        }
+        this.radius = radius;
     }
 }
