1ba2c606cb225801669380fd253df111f7454942
wolfie <yavorhristozov@gmail.com>
2024-02-17 14:57:22 +0200

    refact : Shape abstract class

--- 05. Polymorphism/Lab/02. Shapes/Shape.java
+++ 05. Polymorphism/Lab/02. Shapes/Shape.java
@@ -1,27 +1,25 @@
 public abstract class Shape {
-    private Double perimeter;
-    private Double area;
+    private double perimeter;
+    private double area;
 
-    protected void setPerimeter(Double perimeter) {
+    abstract void  calculatePerimeter();
+    abstract void  calculateArea();
+
+    public void setPerimeter(double perimeter) {
         this.perimeter = perimeter;
     }
 
-    public Double getPerimeter() {
-        return this.perimeter;
-    }
-
-    protected void setArea(Double area) {
+    public void setArea(double area) {
         this.area = area;
     }
 
-    public Double getArea() {
-        return this.area;
+    public double getPerimeter() {
+        return this.perimeter;
     }
 
-
-    public abstract void calculatePerimeter();
-
-    public abstract void calculateArea();
+    public double getArea() {
+        return this.area;
+    }
 }
 
 
