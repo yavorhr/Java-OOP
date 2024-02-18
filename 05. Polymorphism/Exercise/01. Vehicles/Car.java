dd4169f36d651be8104d0610d5b0cb979bcaf963
wolfie <yavorhristozov@gmail.com>
2024-02-18 16:14:24 +0200

    refact : Car

--- 05. Polymorphism/Exercise/01. Vehicles/Car.java
+++ 05. Polymorphism/Exercise/01. Vehicles/Car.java
@@ -1,7 +1,7 @@
 public class Car extends Vehicle {
-    private static final double AIR_CONDITIONER_ADDITIONAL_CONSUMPTION = 0.9;
+    protected static final double AIR_CONDITIONER_ADDITIONAL_CONSUMPTION = 0.9;
 
-    public Car(double fuel, double consumption) {
-        super(fuel, consumption + AIR_CONDITIONER_ADDITIONAL_CONSUMPTION);
+    public Car(double fuelQuantity, double fuelConsumption) {
+        super(fuelQuantity, fuelConsumption + AIR_CONDITIONER_ADDITIONAL_CONSUMPTION);
     }
 }
