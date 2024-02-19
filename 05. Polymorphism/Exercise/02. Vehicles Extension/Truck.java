b9b40528cda71f7b30a364480a445ffb0a1cccc1
wolfie <yavorhristozov@gmail.com>
2024-02-19 14:16:01 +0200

    refact : Truck

--- 05. Polymorphism/Exercise/02. Vehicles Extension/Truck.java
+++ 05. Polymorphism/Exercise/02. Vehicles Extension/Truck.java
@@ -1,13 +1,14 @@
 public class Truck extends Vehicle {
-    private static final double AIR_CONDITIONER_ADDITIONAL_CONSUMPTION = 1.6;
+    protected static final double AIR_CONDITIONER_ADDITIONAL_CONSUMPTION = 1.6;
+    protected static final double MAX_REFUEL_PERCENTAGE = 0.95;
 
-    public Truck(double fuel, double consumption,double tankCapacity) {
-        super(fuel, consumption + AIR_CONDITIONER_ADDITIONAL_CONSUMPTION, tankCapacity);
+    public Truck(double fuelQuantity, double fuelConsumption,double tankCapacity) {
+        super(fuelQuantity, fuelConsumption + AIR_CONDITIONER_ADDITIONAL_CONSUMPTION, tankCapacity);
     }
 
     @Override
     public void refuel(double liters) {
-        super.refuel(liters * 0.95);
+        super.refuel(liters * MAX_REFUEL_PERCENTAGE);
     }
 }
 
