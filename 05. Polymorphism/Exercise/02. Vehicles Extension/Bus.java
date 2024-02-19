f5be56aa49bf83ca243edc9eb440b18b62667f94
wolfie <yavorhristozov@gmail.com>
2024-02-19 14:16:18 +0200

    refact : Bus

--- 05. Polymorphism/Exercise/02. Vehicles Extension/Bus.java
+++ 05. Polymorphism/Exercise/02. Vehicles Extension/Bus.java
@@ -1,17 +1,14 @@
 public class Bus extends Vehicle {
-    private static final double AIR_CONDITIONER_ADDITIONAL_CONSUMPTION = 1.4;
+    protected static final double AIR_CONDITIONER_ADDITIONAL_CONSUMPTION = 1.4;
 
-    public Bus(double fuel, double consumption, double tankCapacity) {
-        super(fuel, consumption, tankCapacity);
+    public Bus(double fuelQuantity, double fuelConsumption, double tankCapacity) {
+        super(fuelQuantity, fuelConsumption, tankCapacity);
     }
 
     public String driveWithPassengers(double distance) {
-        //Add consumption
-        super.addConsumption(AIR_CONDITIONER_ADDITIONAL_CONSUMPTION);
-        //Drive
+        super.addConsumption();
         String out = super.drive(distance);
-        super.subtractConsumption(AIR_CONDITIONER_ADDITIONAL_CONSUMPTION);
-        //Remove consumption
+        super.subtractConsumption();
         return out;
     }
 }
