1598084e33eb73d8d63ab3f8b751cbe885719230
wolfie <yavorhristozov@gmail.com>
2024-02-19 15:51:50 +0200

    refact : Cat

--- 05. Polymorphism/Exercise/03. Wild Farm/wildFarm/Animals/Cat.java
+++ 05. Polymorphism/Exercise/03. Wild Farm/wildFarm/Animals/Cat.java
@@ -1,29 +1,26 @@
-package wildFarm.Animals;
-
-import wildFarm.Food.Food;
+package Animal;
 
 public class Cat extends Felime {
     private String breed;
 
-    public Cat(String animalName, String animalType, double animalWeight, String livingRegion, String breed) {
-        super(animalName, animalType, animalWeight, livingRegion);
+    public Cat(String animalType, String animalName, double animalWeight, String livingRegion, String breed) {
+        super(animalType, animalName, animalWeight, livingRegion);
         this.breed = breed;
     }
 
     @Override
-    public void makeSound() {
+    void makeSound() {
         System.out.println("Meowwww");
     }
 
 
-    //Модифицираме super.toString() метода от :
-    //Cat[Gray, 1.1, Home, 4]
-    //в :
-    //Cat[Gray, Persian, 1.1, Home, 4]
     @Override
     public String toString() {
-        StringBuilder baseToString = new StringBuilder(super.toString());
-        baseToString.insert(baseToString.indexOf(",") + 1, " " + this.breed + ",");
-        return baseToString.toString();
+        return String.format("%s[%s, %s, %.1f, Asia, %d]",
+                this.animalType,
+                this.animalName,
+                this.breed,
+                this.animalWeight,
+                this.foodEaten);
     }
 }
