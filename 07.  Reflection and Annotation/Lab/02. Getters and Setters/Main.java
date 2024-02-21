d67608f38273a575f297666185a5a8ed92259770
wolfie <yavorhristozov@gmail.com>
2024-02-21 15:39:35 +0200

    refact : 02. Getters and Setters

--- 07.  Reflection and Annotation/Lab/02. Getters and Setters/Main.java
+++ 07.  Reflection and Annotation/Lab/02. Getters and Setters/Main.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Comparator;
 import java.util.Set;
@@ -5,6 +6,7 @@ import java.util.TreeSet;
 import java.util.stream.Collectors;
 
 public class Main {
+
     public static class MethodComparatorByName implements Comparator<Method> {
 
         @Override
@@ -13,43 +15,57 @@ public class Main {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws
+            NoSuchMethodException,
+            IllegalAccessException,
+            InvocationTargetException,
+            InstantiationException, ClassNotFoundException {
 
-        Class<Reflection> reflectionClass = Reflection.class;
-        Method[] declaredMethods = reflectionClass.getDeclaredMethods();
+        Class<?> reflection = Class.forName("Reflection");
+        Method[] declaredMethods = reflection.getDeclaredMethods();
 
         Set<Method> getters = new TreeSet<>(new MethodComparatorByName());
         Set<Method> setters = new TreeSet<>(new MethodComparatorByName());
 
         for (Method method : declaredMethods) {
-            if (method.getName().startsWith("get")
-                    && method.getParameterCount() == 0
-                    && method.getReturnType() != void.class) {
+            if (startsWith(method, "get")
+                    && !checkIfParams(method.getParameterCount())
+                    && !chekReturnType(method.getReturnType())) {
                 getters.add(method);
-            } else if (method.getName().startsWith("set")
-                    && method.getParameterCount() == 1
-                    && method.getReturnType() == void.class) {
+            } else if (startsWith(method, "set")
+                    && checkIfParams(method.getParameterCount())
+                    && chekReturnType(method.getReturnType())) {
                 setters.add(method);
             }
         }
 
         System.out.println(getters.
                 stream().
-                map(g -> String.format("%s will return class %s", g.getName(), g.getReturnType().getName().replace("class", "")))
+                map(g -> String.format("%s will return class %s",
+                        g.getName(),
+                        g.getReturnType().getName().replace("class", "")))
                 .collect(Collectors.joining(System.lineSeparator())));
 
         System.out.println(setters.
                 stream().
-                map(s -> String.format("%s will set field of class %s", s.getName(),s.getParameterTypes()[0].getName().replace("class", "")))
+                map(s -> String.format("%s will set field of class %s",
+                        s.getName(),
+                        s.getParameterTypes()[0].getName().replace("class", "")))
                 .collect(Collectors.joining(System.lineSeparator())));
     }
+
+    private static boolean chekReturnType(Class<?> returnType) {
+        return returnType == void.class;
+    }
+
+    private static boolean checkIfParams(int parameterCount) {
+        return parameterCount > 0;
+    }
+
+    private static boolean startsWith(Method method, String getOrSet) {
+        return method.getName().startsWith(getOrSet);
+    }
+
+
 }
-
-
-
-
-
-
-
-
 
