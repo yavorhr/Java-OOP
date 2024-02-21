081445f4fba4c972693a139187f6139021e78385
wolfie <yavorhristozov@gmail.com>
2024-02-21 15:50:09 +0200

    add : 04. Create Annotation

---
+++ 07.  Reflection and Annotation/Lab/04. Create Annotation/Main.java
@@ -0,0 +1,31 @@
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+public class Main {
+  public static void main(String[] args) throws ClassNotFoundException {
+
+    Class<?> reflectionClass = Class.forName("Reflection");
+
+
+    Annotation[] annotations = reflectionClass.getAnnotations();
+    for (Annotation a: annotations) {
+      System.out.println(a);
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
