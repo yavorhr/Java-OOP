0e689359b4c15c9a7d588ccc667ea75a3773ed5b
wolfie <yavorhristozov@gmail.com>
2024-03-01 14:20:42 +0200

    add : tests for find(int n)
    Find product on first and last indexes

--- 10. Test Driven Development/Lab/INStock/src/test/java/ProductStockTest.java
+++ 10. Test Driven Development/Lab/INStock/src/test/java/ProductStockTest.java
@@ -37,9 +37,51 @@ public class ProductStockTest {
     Assert.assertTrue(stock.contains(product));
   }
 
+  // Test for valid product first in DS last in DS and in the middle of DS
+  // Test IndexOutOfBounds for less than 0 and greater or equal to size
+  @Test
+  public void testFindByIdShouldReturnCorrectIfFirstItemInStock() {
+    Product product = createProduct();
+    stock.add(product);
+
+    Product foundByIndex = stock.find(0);
+    Assert.assertNotNull(foundByIndex);
+
+    Assert.assertEquals(product.getLabel(), foundByIndex.getLabel());
+  }
+
+  @Test
+  public void testFindByIdShouldReturnCorrectIfLastItemInStock() {
+    fillProductsToStock(5);
+    Product product = createProduct();
+    stock.add(product);
+
+    Product foundByIndex = stock.find(this.stock.getCount() - 1);
+
+    Assert.assertNotNull(foundByIndex);
+    Assert.assertEquals(product.getLabel(), foundByIndex.getLabel());
+  }
 
 
+  // Helpers
   private Product createProduct() {
     return new Product("test_label", 3.00, 1);
+  }
+
+  private Product[] createMultipleProducts(int n) {
+    Product[] products = new Product[n];
+
+    for (int i = 0; i < n; i++) {
+      products[i] = new Product("test_label" + i, 3.00, 1);
+    }
+    return products;
+  }
+
+  private void fillProductsToStock(int n) {
+    Product[] products = createMultipleProducts(n);
+
+    for (Product product : products) {
+      this.stock.add(product);
+    }
   }
 }
