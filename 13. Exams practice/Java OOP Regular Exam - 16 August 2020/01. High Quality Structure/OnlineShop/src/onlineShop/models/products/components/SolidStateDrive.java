3db5639daaf756af6921b7d75998b9e692446b5c
wolfie <yavorhristozov@gmail.com>
2024-03-09 12:21:47 +0200

    add : rest of Components classes
    PowerSupply, RandomAccessMemory, SolidStateDrive, VideoCard

---
+++ 13. Exams practice/Java OOP Regular Exam - 16 August 2020/01. High Quality Structure/OnlineShop/src/onlineShop/models/products/components/SolidStateDrive.java
@@ -0,0 +1,9 @@
+package onlineShop.models.products.components;
+
+public class SolidStateDrive extends BaseComponent{
+  public static final double OVERALL_PERFORMANCE_MULTIPLIER = 1.20;
+
+  protected SolidStateDrive(int id, String manufacturer, String model, double price, double overallPerformance, int generation) {
+    super(id, manufacturer, model, price, overallPerformance * OVERALL_PERFORMANCE_MULTIPLIER, generation);
+  }
+}
