4b2ea1e1f2b9f1be2d1dc6d7bb157d1acb7a625b
wolfie <yavorhristozov@gmail.com>
2024-03-09 12:34:19 +0200

    add : Peripherals child classes
    Headset, Keyboard, Mouse and Monitor

---
+++ 13. Exams practice/Java OOP Regular Exam - 16 August 2020/01. High Quality Structure/OnlineShop/src/onlineShop/models/products/peripherals/Keyboard.java
@@ -0,0 +1,7 @@
+package onlineShop.models.products.peripherals;
+
+public class Keyboard extends BasePeripheral{
+  protected Keyboard(int id, String manufacturer, String model, double price, double overallPerformance, String connectionType) {
+    super(id, manufacturer, model, price, overallPerformance, connectionType);
+  }
+}
