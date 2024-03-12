605b4f5a0f80b12ebb002c0c1f3c39a9976b2267
wolfie <yavorhristozov@gmail.com>
2024-03-12 21:30:42 +0200

    add : Unit tests resources

---
+++ 13. Exams practice/Java OOP Retake Exam - 16 April 2020/02. Unit tests/Computers/src/test/java/computers/ComputerTest.java
@@ -0,0 +1,10 @@
+package computers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ComputerTest {
+
+   //TODO: TEST ALL THE FUNCTIONALITY OF THE PROVIDED CLASS Computer
+}
