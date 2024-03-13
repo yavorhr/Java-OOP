d866aec20cbcaed66e39d7d635eb09129b63081d
wolfie <yavorhristozov@gmail.com>
2024-03-13 13:16:37 +0200

    add : resources

---
+++ 13. Exams practice/Java OOP Retake Exam - 12 April 2020/01. High Quality Structure_Skeleton + 02. Business Logic/CounterStriker/src/CounterStriker/core/EngineImpl.java
@@ -0,0 +1,87 @@
+package CounterStriker.core;
+
+import CounterStriker.common.Command;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class EngineImpl implements Engine {
+    private Controller controller;
+    private BufferedReader reader;
+
+    public EngineImpl() {
+        // this.controller = new ControllerImpl(); //TODO implement first
+        this.reader = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            String result = null;
+            try {
+                result = processInput();
+
+                if (result.equals(Command.Exit.name())) {
+                    break;
+                }
+
+            } catch (IOException | IllegalArgumentException | NullPointerException e) {
+                result = e.getMessage();
+            }
+
+            System.out.println(result);
+        }
+    }
+
+    private String processInput() throws IOException {
+        String input = this.reader.readLine();
+        String[] tokens = input.split("\\s");
+
+        Command command = Command.valueOf(tokens[0]);
+        String[] data = Arrays.stream(tokens).skip(1).toArray(String[]::new);
+
+        String result = null;
+
+        switch (command) {
+            case AddPlayer:
+                result = this.addPlayer(data);
+                break;
+            case Exit:
+                result = Command.Exit.name();
+                break;
+            case StartGame:
+                result = this.start();
+                break;
+            case Report:
+                result = this.report();
+                break;
+            case AddGun:
+                result = this.addGun(data);
+                break;
+        }
+
+        return result;
+    }
+
+    private String addGun(String[] data) {
+        //TODO
+        return null;
+    }
+
+    private String report() {
+        //TODO
+        return null;
+    }
+
+    private String addPlayer(String[] data) {
+        //TODO
+        return null;
+    }
+
+    private String start() {
+        //TODO
+        return null;
+    }
+}
