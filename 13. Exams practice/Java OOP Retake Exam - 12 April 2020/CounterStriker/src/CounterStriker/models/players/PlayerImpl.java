dea28c3dadf6ad7ebf4d034f749b6907d878e823
wolfie <yavorhristozov@gmail.com>
2024-03-13 14:28:04 +0200

    add : PlayerImpl

---
+++ 13. Exams practice/Java OOP Retake Exam - 12 April 2020/CounterStriker/src/CounterStriker/models/players/PlayerImpl.java
@@ -0,0 +1,56 @@
+package CounterStriker.models.players;
+
+import CounterStriker.models.guns.Gun;
+
+public class PlayerImpl implements Player {
+  private String username;
+  private int health;
+  private int armor;
+  private boolean isAlive;
+  private Gun gun;
+
+  public PlayerImpl(String username, int health, int armor, Gun gun) {
+    this.username = username;
+    this.health = health;
+    this.armor = armor;
+    this.gun = gun;
+    this.isAlive = true;
+  }
+
+  @Override
+  public String getUsername() {
+    return this.username;
+  }
+
+  @Override
+  public int getHealth() {
+    return this.health;
+  }
+
+  @Override
+  public int getArmor() {
+    return this.armor;
+  }
+
+  @Override
+  public Gun getGun() {
+    return this.gun;
+  }
+
+  @Override
+  public boolean isAlive() {
+    return this.isAlive;
+  }
+
+  @Override
+  public void takeDamage(int points) {
+    if (this.armor > points) {
+      this.armor -= points;
+    } else if (this.armor == 0) {
+      this.health -= points;
+    } else {
+      int diff = Math.abs(this.armor -= points);
+      this.health -= diff;
+    }
+  }
+}
