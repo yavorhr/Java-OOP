3e8203fc251030c6c60c868b130232a54028355c
wolfie <yavorhristozov@gmail.com>
2024-03-19 10:43:08 +0200

    add : BaseBank class

---
+++ 13. Exams practice/Java OOP Regular Exam - 5 August 2023/01. High Quality Structure + 02. Business Logic/BankApp-Skeleton/src/main/java/bank/entities/bank/BaseBank.java
@@ -0,0 +1,67 @@
+package bank.entities.bank;
+
+import bank.entities.client.Client;
+import bank.entities.loan.Loan;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public abstract class BaseBank implements Bank {
+  private String name;
+  private int capacity;
+  private Collection<Loan> loans;
+  private Collection<Client> clients;
+
+  public BaseBank(String name, int capacity) {
+    this.name = name;
+    this.capacity = capacity;
+    this.loans = new ArrayList<>();
+    this.clients = new ArrayList<>();
+  }
+
+  @Override
+  public String getName() {
+    return this.name;
+  }
+
+  @Override
+  public void setName(String name) {
+
+  }
+
+  @Override
+  public Collection<Client> getClients() {
+    return this.clients;
+  }
+
+  @Override
+  public Collection<Loan> getLoans() {
+    return this.getLoans();
+  }
+
+  @Override
+  public void addClient(Client client) {
+
+  }
+
+  @Override
+  public void removeClient(Client client) {
+
+  }
+
+  @Override
+  public void addLoan(Loan loan) {
+
+  }
+
+  @Override
+  public int sumOfInterestRates() {
+    return 0;
+  }
+
+  @Override
+  public String getStatistics() {
+    return null;
+  }
+}
