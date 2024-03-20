fc8b6cb45efb141c1d955297c37e1a5b889836a6
wolfie <yavorhristozov@gmail.com>
2024-03-20 16:20:20 +0200

    add : insertDecoration() impl

--- 13. Exams practice/Java OOP Regular Exam - 10 April 2021/01. High Quality Structure + 02. Business Logic/Aquarium/src/main/java/aquarium/core/ControllerImpl.java
+++ 13. Exams practice/Java OOP Regular Exam - 10 April 2021/01. High Quality Structure + 02. Business Logic/Aquarium/src/main/java/aquarium/core/ControllerImpl.java
@@ -1,8 +1,11 @@
 package aquarium.core;
 
 import aquarium.common.ConstantMessages;
+import aquarium.common.ExceptionMessages;
 import aquarium.entities.aquariums.Aquarium;
+import aquarium.entities.decorations.Decoration;
 import aquarium.factory.AquariumFactory;
+import aquarium.factory.DecorationFactory;
 import aquarium.repositories.DecorationRepository;
 import aquarium.repositories.Repository;
 
@@ -27,13 +30,20 @@ public class ControllerImpl implements Controller {
 
   @Override
   public String addDecoration(String type) {
-    return null;
+    Decoration decoration = DecorationFactory.create(type);
+    this.decorationRepository.add(decoration);
+    return String.format(ConstantMessages.SUCCESSFULLY_ADDED_DECORATION_TYPE, type);
   }
 
   @Override
   public String insertDecoration(String aquariumName, String decorationType) {
-    return null;
+    Aquarium aquarium = this.aquariums.get(aquariumName);
+    Decoration decoration = this.getDecoration(decorationType);
+    aquarium.addDecoration(decoration);
+
+    return String.format(ConstantMessages.SUCCESSFULLY_ADDED_DECORATION_IN_AQUARIUM, decorationType, aquariumName);
   }
+
 
   @Override
   public String addFish(String aquariumName, String fishType, String fishName, String fishSpecies, double price) {
@@ -53,5 +63,16 @@ public class ControllerImpl implements Controller {
   @Override
   public String report() {
     return null;
+  }
+
+  // Helpers
+
+  private Decoration getDecoration(String decorationType) {
+    Decoration decoration = this.decorationRepository.findByType(decorationType);
+    if (decoration == null) {
+      throw new IllegalArgumentException(String.format(ExceptionMessages.NO_DECORATION_FOUND, decorationType));
+    }
+
+    return decoration;
   }
 }
