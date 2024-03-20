0b5e0885df7c0c357378cc72097e20c95461e7fa
wolfie <yavorhristozov@gmail.com>
2024-03-20 13:15:44 +0200

    add : Plant class
    child class of BaseDecoration

---
+++ 13. Exams practice/Java OOP Regular Exam - 10 April 2021/01. High Quality Structure + 02. Business Logic/Aquarium/src/main/java/aquarium/entities/decorations/Plant.java
@@ -0,0 +1,10 @@
+package aquarium.entities.decorations;
+
+public class Plant extends BaseDecoration {
+  private static final int COMFORT = 5;
+  private static final double PRICE = 19.00;
+
+  public Plant(int comfort, double price) {
+    super(COMFORT, PRICE);
+  }
+}
