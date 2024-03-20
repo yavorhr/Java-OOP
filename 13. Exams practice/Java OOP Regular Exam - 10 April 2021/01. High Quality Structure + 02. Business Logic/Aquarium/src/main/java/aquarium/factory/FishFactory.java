ef662b54ff8449d2113f575ea5da2c3e07f5d97f
wolfie <yavorhristozov@gmail.com>
2024-03-20 16:22:39 +0200

    add : FishFactory class

--- 13. Exams practice/Java OOP Regular Exam - 10 April 2021/01. High Quality Structure + 02. Business Logic/Aquarium/src/main/java/aquarium/factory/FishFactory.java
+++ 13. Exams practice/Java OOP Regular Exam - 10 April 2021/01. High Quality Structure + 02. Business Logic/Aquarium/src/main/java/aquarium/factory/FishFactory.java
@@ -1,7 +1,21 @@
 package aquarium.factory;
 
+import aquarium.common.ExceptionMessages;
 import aquarium.entities.fish.Fish;
+import aquarium.entities.fish.FreshwaterFish;
+import aquarium.entities.fish.SaltwaterFish;
 
 public class FishFactory {
+  public static Fish create(String type, String name, String species, Double price) {
+    Fish fish = null;
+
+    switch (type) {
+      case "FreshwaterFish" -> fish = new FreshwaterFish(name, species, price);
+      case "SaltwaterFish" -> fish = new SaltwaterFish(name, species, price);
+      default -> throw new NullPointerException(ExceptionMessages.INVALID_FISH_TYPE);
+    }
+    return fish;
   }
+}
+
 
