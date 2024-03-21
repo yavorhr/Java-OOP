b2a4475fc6963c144b63447395946877f6075dfa
wolfie <yavorhristozov@gmail.com>
2024-03-21 16:41:51 +0200

    add : Child classes for BaseDelicacy class

---
+++ 13. Exams practice/Java OOP Regular Exam - 10 December 2022/01. High Quality Structure + 02. Business Logic/ChristmasStoreApp/src/main/java/christmasPastryShop/entities/delicacies/Stolen.java
@@ -0,0 +1,9 @@
+package christmasPastryShop.entities.delicacies;
+
+public class Stolen extends BaseDelicacy {
+  private static final double INITIAL_PORTION = 250;
+
+  protected Stolen(String name, double price) {
+    super(name, INITIAL_PORTION, price);
+  }
+}
