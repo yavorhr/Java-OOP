6142270621056151560261b9d41ca445c77e3913
wolfie <yavorhristozov@gmail.com>
2024-03-22 16:12:00 +0200

    add : Main impl
    start EngineImpl

--- 13. Exams practice/Java OOP Regular Exam - 10 December 2022/01. High Quality Structure + 02. Business Logic/ChristmasStoreApp/src/main/java/christmasPastryShop/Main.java
+++ 13. Exams practice/Java OOP Regular Exam - 10 December 2022/01. High Quality Structure + 02. Business Logic/ChristmasStoreApp/src/main/java/christmasPastryShop/Main.java
@@ -1,30 +1,28 @@
 package christmasPastryShop;
 
+import christmasPastryShop.core.ControllerImpl;
+import christmasPastryShop.core.EngineImpl;
 import christmasPastryShop.core.interfaces.Controller;
 import christmasPastryShop.entities.delicacies.interfaces.Delicacy;
 import christmasPastryShop.entities.cocktails.interfaces.Cocktail;
 import christmasPastryShop.entities.booths.interfaces.Booth;
-import christmasPastryShop.repositories.interfaces.BoothRepository;
-import christmasPastryShop.repositories.interfaces.CocktailRepository;
-import christmasPastryShop.repositories.interfaces.DelicacyRepository;
+import christmasPastryShop.io.ConsoleReader;
+import christmasPastryShop.io.ConsoleWriter;
+import christmasPastryShop.repositories.interfaces.*;
 
 public class Main {
-    public static void main(String[] args) {
+  public static void main(String[] args) {
+    
+    DelicacyRepository<Delicacy> delicacyRepository = new DelicacyRepositoryImpl();
+    CocktailRepository<Cocktail> cocktailRepository = new CocktailRepositoryImpl();
+    BoothRepository<Booth> boothRepository = new BoothRepositoryImpl();
 
-        String a = " ";
-        int a1 = a.length();
-        DelicacyRepository<Delicacy> delicacyRepository; // TODO: new DelicacyRepositoryImpl<>();
-        CocktailRepository<Cocktail> cocktailRepository; // TODO: new CocktailRepositoryImpl<>();
-        BoothRepository<Booth> boothRepository; // TODO: new BoothRepositoryImpl<>();
+    Controller controller = new ControllerImpl(delicacyRepository, cocktailRepository, boothRepository);
 
-        Controller controller; // TODO: new ControllerImpl(delicacyRepository, cocktailRepository, boothRepository);
+    ConsoleReader reader = new ConsoleReader();
+    ConsoleWriter writer = new ConsoleWriter();
+    EngineImpl engine = new EngineImpl(reader, writer, controller);
+    engine.run();
 
-        // TODO:OPTIONAL
-
-        //  ConsoleReader reader = new ConsoleReader();
-        // ConsoleWriter writer = new ConsoleWriter();
-        // EngineImpl engine = new EngineImpl(reader, writer, controller);
-        // engine.run();
-
-    }
+  }
 }
