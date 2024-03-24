6affabae9e934645c462e95613a30c6428cc379f
wolfie <yavorhristozov@gmail.com>
2024-03-24 14:01:52 +0200

    add : FactoryHouse

---
+++ 13. Exams practice/Java OOP Regular Exam - 11 December 2021/01. High Quality Structure + 02. Business Logic/CatHouseApp/src/main/java/catHouse/factory/FactoryHouse.java
@@ -0,0 +1,20 @@
+package catHouse.factory;
+
+import catHouse.common.ExceptionMessages;
+import catHouse.entities.houses.House;
+import catHouse.entities.houses.LongHouse;
+import catHouse.entities.houses.ShortHouse;
+
+public class FactoryHouse {
+  public static House create(String type, String name) {
+    House house = null;
+
+    switch (type) {
+      case "ShortHouse" -> house = new ShortHouse(name);
+      case "LongHouse" -> house = new LongHouse(name);
+      default: throw new IllegalArgumentException(ExceptionMessages.INVALID_HOUSE_TYPE);
+    }
+
+    return house;
+  }
+}
