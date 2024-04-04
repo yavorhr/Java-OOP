0185285bed103e15c174a771eb80269fbcba34e5
wolfie <yavorhristozov@gmail.com>
2024-04-04 19:33:19 +0300

    add : action() impl

--- 13. Exams practice/Java OOP Retake Exam - 19 December 2020/01. High Quality Structure + 02. Business Logic/ViceCity/src/main/java/viceCity/models/neighbourhood/GangNeighbourhood.java
+++ 13. Exams practice/Java OOP Retake Exam - 19 December 2020/01. High Quality Structure + 02. Business Logic/ViceCity/src/main/java/viceCity/models/neighbourhood/GangNeighbourhood.java
@@ -4,19 +4,48 @@ import viceCity.models.guns.Gun;
 import viceCity.models.players.Player;
 
 import java.util.Collection;
+import java.util.stream.Collectors;
 
 public class GangNeighbourhood implements Neighbourhood {
 
   @Override
   public void action(Player mainPlayer, Collection<Player> civilPlayers) {
-    Collection<Gun> playerGuns = mainPlayer.getGunRepository().getModels();
+    Collection<Gun> playerGuns = getPlayerGuns(mainPlayer);
 
-    for (Player currentCivilPlayer : civilPlayers) {
-      for (Gun currentGun : playerGuns) {
-        while (currentGun.canFire() && currentCivilPlayer.isAlive()) {
-            currentCivilPlayer.takeLifePoints(currentGun.fire());
+    for (Gun gun : playerGuns) {
+      for (Player civilPlayer : civilPlayers) {
+        while (gun.canFire() && civilPlayer.isAlive()) {
+          civilPlayer.takeLifePoints(gun.fire());
         }
       }
     }
+    civilPlayers = filterAliveCivilPlayers(civilPlayers);
+
+    for (Player civilPlayer : civilPlayers) {
+      Collection<Gun> civilPlayerGuns = getPlayerGuns(civilPlayer);
+
+      for (Gun gun : civilPlayerGuns) {
+        while (gun.canFire() && mainPlayer.isAlive()) {
+          mainPlayer.takeLifePoints(gun.fire());
+        }
+      }
+    }
+  }
+
+
+
+  private Collection<Player> filterAliveCivilPlayers(Collection<Player> civilPlayers) {
+    return civilPlayers
+            .stream()
+            .filter(Player::isAlive)
+            .collect(Collectors.toList());
+  }
+
+  private boolean allPlayersAreDead(Collection<Player> civilPlayers) {
+    return civilPlayers.stream().noneMatch(Player::isAlive);
+  }
+
+  private Collection<Gun> getPlayerGuns(Player player) {
+    return player.getGunRepository().getModels();
   }
 }
