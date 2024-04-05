e4be8f827745ab9342140036ee5b713e8f05ae96
wolfie <yavorhristozov@gmail.com>
2024-04-05 20:15:03 +0300

    add : Controller input commands in Engine

--- 13. Exams practice/Java OOP Retake Exam - 19 December 2020/01. High Quality Structure + 02. Business Logic/ViceCity/src/main/java/viceCity/core/EngineImpl.java
+++ 13. Exams practice/Java OOP Retake Exam - 19 December 2020/01. High Quality Structure + 02. Business Logic/ViceCity/src/main/java/viceCity/core/EngineImpl.java
@@ -10,58 +10,48 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class EngineImpl implements Engine {
-    private Controller controller;
-    private BufferedReader reader;
+  private Controller controller;
+  private BufferedReader reader;
 
-    public EngineImpl(Controller controller) {
-        this.controller = controller;
-        this.reader = new BufferedReader(new InputStreamReader(System.in));
-    }
+  public EngineImpl(Controller controller) {
+    this.controller = controller;
+    this.reader = new BufferedReader(new InputStreamReader(System.in));
+  }
 
-    @Override
-    public void run() {
-        while (true) {
-            String result = null;
-            try {
-                result = processInput();
+  @Override
+  public void run() {
+    while (true) {
+      String result = null;
+      try {
+        result = processInput();
 
-                if (result.equals(Command.Exit.name())) {
-                    break;
-                }
-            } catch (NullPointerException | IllegalArgumentException | IOException e) {
-                result = e.getMessage();
-            }
-
-            System.out.println(result);
+        if (result.equals(Command.Exit.name())) {
+          break;
         }
+      } catch (NullPointerException | IllegalArgumentException | IOException e) {
+        result = e.getMessage();
+      }
+
+      System.out.println(result);
+    }
+  }
+
+  private String processInput() throws IOException {
+    String input = this.reader.readLine();
+    String[] tokens = input.split("\\s+");
+
+    Command command = Command.valueOf(tokens[0]);
+    String result = null;
+    String[] data = Arrays.stream(tokens).skip(1).toArray(String[]::new);
+
+    switch (command) {
+      case AddPlayer -> this.controller.addPlayer(data[0]);
+      case AddGun -> this.controller.addGun(data[0], data[1]);
+      case AddGunToPlayer -> this.controller.addGunToPlayer(data[0])
+      case Fight -> this.controller.fight();
+      case Exit -> result = Command.Exit.name();
     }
 
-    private String processInput() throws IOException {
-        String input = this.reader.readLine();
-        String[] tokens = input.split("\\s+");
-
-        Command command = Command.valueOf(tokens[0]);
-        String result = null;
-        String[] data = Arrays.stream(tokens).skip(1).toArray(String[]::new);
-
-        switch (command) {
-            case AddPlayer:
-
-                break;
-            case AddGun:
-
-                break;
-            case AddGunToPlayer:
-
-                break;
-            case Fight:
-
-                break;
-            case Exit:
-                result = Command.Exit.name();
-                break;
-        }
-
-        return result;
-    }
+    return result;
+  }
 }
