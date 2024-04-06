cc699580d7672cf6ffdd98e5964ac9aa00230880
wolfie <yavorhristozov@gmail.com>
2024-04-06 15:18:51 +0300

    add : setUp() tests func
    init new Spaceship and 3 astronauts before each test execution

--- 13. Exams practice/Java OOP Retake Exam - 19 December 2020/03. Unit testing/BlueOrigin/src/test/java/blueOrigin/SpaceshipTests.java
+++ 13. Exams practice/Java OOP Retake Exam - 19 December 2020/03. Unit testing/BlueOrigin/src/test/java/blueOrigin/SpaceshipTests.java
@@ -1,8 +1,25 @@
 package blueOrigin;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class SpaceshipTests {
-    //TODO: TEST ALL THE FUNCTIONALITY OF THE PROVIDED CLASS Spaceship
+
+  @Before
+  public void setUp() {
+    Astronaut astronaut = new Astronaut("Steven", 90);
+    Astronaut astronaut2 = new Astronaut("Mike", 50);
+    Astronaut astronaut3 = new Astronaut("George", 80);
+
+    Spaceship spaceship = new Spaceship("StarOfDeath", 3);
+    spaceship.add(astronaut);
+    spaceship.add(astronaut2);
+    spaceship.add(astronaut3);
+  }
+
+  @Test
+  public void testSpaceShipCtorWhenInitObjWorksCorrect() {
+
+  }
 }
