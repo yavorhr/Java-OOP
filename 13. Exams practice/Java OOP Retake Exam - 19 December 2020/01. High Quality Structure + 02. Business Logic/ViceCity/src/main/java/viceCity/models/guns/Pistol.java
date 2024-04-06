1bc1a8f8b47e5ace7a6364aef1c4ddcf061d0bdc
wolfie <yavorhristozov@gmail.com>
2024-04-06 10:56:41 +0300

    refact : gun.fire() should work correct

--- 13. Exams practice/Java OOP Retake Exam - 19 December 2020/01. High Quality Structure + 02. Business Logic/ViceCity/src/main/java/viceCity/models/guns/Pistol.java
+++ 13. Exams practice/Java OOP Retake Exam - 19 December 2020/01. High Quality Structure + 02. Business Logic/ViceCity/src/main/java/viceCity/models/guns/Pistol.java
@@ -3,15 +3,15 @@ package viceCity.models.guns;
 public class Pistol extends BaseGun {
   private static final int BULLETS_PER_BARREL = 10;
   private static final int FIRE_BULLETS_PER_TIME = 1;
-  private static final int TOTAL_BULLETS = 100;
+  private static final int INITIAL_TOTAL_BULLETS = 100;
 
   private int currentBullets;
   private int currentTotalBullets;
 
   public Pistol(String name) {
-    super(name, BULLETS_PER_BARREL, TOTAL_BULLETS);
+    super(name, BULLETS_PER_BARREL, INITIAL_TOTAL_BULLETS);
     this.currentBullets = BULLETS_PER_BARREL;
-    this.currentTotalBullets = TOTAL_BULLETS;
+    this.currentTotalBullets = INITIAL_TOTAL_BULLETS;
   }
 
   @Override
@@ -29,7 +29,7 @@ public class Pistol extends BaseGun {
 
   @Override
   public boolean canFire() {
-    return this.currentTotalBullets >= BULLETS_PER_BARREL;
+    return this.currentTotalBullets >= BULLETS_PER_BARREL || this.currentBullets > 0;
   }
 
   private void reloadGunAndShoot() {
--- 13. Exams practice/Java OOP Retake Exam - 19 December 2020/01. High Quality Structure + 02. Business Logic/ViceCity/src/main/java/viceCity/models/guns/Rifle.java
+++ 13. Exams practice/Java OOP Retake Exam - 19 December 2020/01. High Quality Structure + 02. Business Logic/ViceCity/src/main/java/viceCity/models/guns/Rifle.java
@@ -2,30 +2,39 @@ package viceCity.models.guns;
 
 public class Rifle extends BaseGun{
   private static final int BULLETS_PER_BARREL = 50;
-  private static  int CURRENT_BULLETS = 10;
-  private static int TOTAL_BULLETS = 500;
-  private static final int FIRE_BULLETS_PER_TIME = 5;
+  private static final int FIRE_BULLETS_PER_TIME =5;
+  private static final int INITIAL_TOTAL_BULLETS = 500;
+
+  private int currentBullets;
+  private int currentTotalBullets;
 
   public Rifle(String name) {
-    super(name,  BULLETS_PER_BARREL, TOTAL_BULLETS);
+    super(name,  BULLETS_PER_BARREL, INITIAL_TOTAL_BULLETS);
+
+    this.currentBullets = BULLETS_PER_BARREL;
+    this.currentTotalBullets = INITIAL_TOTAL_BULLETS;
   }
 
   @Override
   public int fire() {
-    if (this.canFire() && CURRENT_BULLETS > 0) {
-      CURRENT_BULLETS -= FIRE_BULLETS_PER_TIME;
-    } else if (CURRENT_BULLETS == 0 && TOTAL_BULLETS >= BULLETS_PER_BARREL) {
-      this.reloadGun();
+    if (this.canFire() && this.currentBullets > 0) {
+      this.currentBullets -= FIRE_BULLETS_PER_TIME;
+    } else if (this.currentBullets == 0 && this.currentTotalBullets >= BULLETS_PER_BARREL) {
+      this.reloadGunAndShoot();
+    } else {
+      return 0;
     }
     return FIRE_BULLETS_PER_TIME;
   }
 
-  private void reloadGun() {
-    TOTAL_BULLETS -= BULLETS_PER_BARREL;
-    CURRENT_BULLETS += BULLETS_PER_BARREL;
-  }
   @Override
   public boolean canFire() {
-    return super.getBulletsPerBarrel() >= FIRE_BULLETS_PER_TIME;
+    return this.currentTotalBullets >= BULLETS_PER_BARREL || this.currentBullets > 0;
+  }
+
+  private void reloadGunAndShoot() {
+    this.currentTotalBullets -= BULLETS_PER_BARREL;
+    this.currentBullets += BULLETS_PER_BARREL;
+    this.currentBullets -= FIRE_BULLETS_PER_TIME;
   }
 }
