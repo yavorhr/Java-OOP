45abe1a488223e5b045872f876650f36135ab95c
wolfie <yavorhristozov@gmail.com>
2024-04-06 15:21:17 +0300

    fix : setUp() func

--- 13. Exams practice/Java OOP Retake Exam - 19 December 2020/03. Unit testing/BlueOrigin/src/test/java/blueOrigin/SpaceshipTests.java
+++ 13. Exams practice/Java OOP Retake Exam - 19 December 2020/03. Unit testing/BlueOrigin/src/test/java/blueOrigin/SpaceshipTests.java
@@ -5,21 +5,25 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class SpaceshipTests {
+  private Astronaut astronaut;
+  private Astronaut astronaut2;
+  private Astronaut astronaut3;
+  private Spaceship spaceship;
 
   @Before
   public void setUp() {
-    Astronaut astronaut = new Astronaut("Steven", 90);
-    Astronaut astronaut2 = new Astronaut("Mike", 50);
-    Astronaut astronaut3 = new Astronaut("George", 80);
+   this.astronaut = new Astronaut("Steven", 90);
+   this.astronaut2 = new Astronaut("Mike", 50);
+   this.astronaut3 = new Astronaut("George", 80);
 
-    Spaceship spaceship = new Spaceship("StarOfDeath", 3);
-    spaceship.add(astronaut);
-    spaceship.add(astronaut2);
-    spaceship.add(astronaut3);
+    this.spaceship = new Spaceship("StarOfDeath", 3);
+    this.spaceship.add(astronaut);
+    this.spaceship.add(astronaut2);
+    this.spaceship.add(astronaut3);
   }
 
   @Test
   public void testSpaceShipCtorWhenInitObjWorksCorrect() {
-
+    Assert.assertEquals("StarOfDeath", this.sp);
   }
 }
