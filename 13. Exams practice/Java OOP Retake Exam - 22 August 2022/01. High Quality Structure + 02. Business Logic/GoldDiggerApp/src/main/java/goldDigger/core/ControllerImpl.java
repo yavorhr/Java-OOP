e79a075c60b77f112e01b15d41916660bf4a3f3d
wolfie <yavorhristozov@gmail.com>
2024-04-06 17:46:54 +0300

    add : resources

---
+++ 13. Exams practice/Java OOP Retake Exam - 22 August 2022/01. High Quality Structure + 02. Business Logic/GoldDiggerApp/src/main/java/goldDigger/core/ControllerImpl.java
@@ -0,0 +1,28 @@
+package goldDigger.core;
+
+public class ControllerImpl implements Controller{
+    @Override
+    public String addDiscoverer(String kind, String discovererName) {
+        return null;
+    }
+
+    @Override
+    public String addSpot(String spotName, String... exhibits) {
+        return null;
+    }
+
+    @Override
+    public String excludeDiscoverer(String discovererName) {
+        return null;
+    }
+
+    @Override
+    public String inspectSpot(String spotName) {
+        return null;
+    }
+
+    @Override
+    public String getStatistics() {
+        return null;
+    }
+}
