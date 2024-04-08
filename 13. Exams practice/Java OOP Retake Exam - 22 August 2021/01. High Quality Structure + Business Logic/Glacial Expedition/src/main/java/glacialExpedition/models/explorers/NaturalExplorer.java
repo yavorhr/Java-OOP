12d54d84fd47f1cf9038adb92494cbc24cc676f0
wolfie <yavorhristozov@gmail.com>
2024-04-08 14:49:42 +0300

    add : NaturalExplorer class
    child class of BaseExplorer

---
+++ 13. Exams practice/Java OOP Retake Exam - 22 August 2021/01. High Quality Structure + Business Logic/Glacial Expedition/src/main/java/glacialExpedition/models/explorers/NaturalExplorer.java
@@ -0,0 +1,9 @@
+package glacialExpedition.models.explorers;
+
+public class NaturalExplorer extends BaseExplorer {
+  private static double INITIAL_ENERGY = 60;
+
+  public NaturalExplorer(String name) {
+    super(name, INITIAL_ENERGY);
+  }
+}
