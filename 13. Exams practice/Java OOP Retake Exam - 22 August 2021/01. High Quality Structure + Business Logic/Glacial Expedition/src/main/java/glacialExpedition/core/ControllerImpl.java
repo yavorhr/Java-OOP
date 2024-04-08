d8654d2f54b2109b4a40afcad2e9bda77555836c
wolfie <yavorhristozov@gmail.com>
2024-04-08 16:02:56 +0300

    add : addState() impl
    ControllerImpl class

--- 13. Exams practice/Java OOP Retake Exam - 22 August 2021/01. High Quality Structure + Business Logic/Glacial Expedition/src/main/java/glacialExpedition/core/ControllerImpl.java
+++ 13. Exams practice/Java OOP Retake Exam - 22 August 2021/01. High Quality Structure + Business Logic/Glacial Expedition/src/main/java/glacialExpedition/core/ControllerImpl.java
@@ -3,14 +3,19 @@ package glacialExpedition.core;
 import glacialExpedition.common.ConstantMessages;
 import glacialExpedition.factory.ExplorerFactory;
 import glacialExpedition.models.explorers.Explorer;
+import glacialExpedition.models.states.State;
+import glacialExpedition.models.states.StateImpl;
 import glacialExpedition.repositories.ExplorerRepository;
 import glacialExpedition.repositories.Repository;
+import glacialExpedition.repositories.StateRepository;
 
 public class ControllerImpl implements Controller {
   private Repository<Explorer> explorerRepository;
+  private Repository<State> stateRepository;
 
   public ControllerImpl() {
     this.explorerRepository = new ExplorerRepository();
+    this.stateRepository = new StateRepository();
   }
 
   @Override
@@ -21,13 +26,15 @@ public class ControllerImpl implements Controller {
       return "Explorer type doesn't exists.";
     }
     this.explorerRepository.add(explorer);
-    
+
     return String.format(ConstantMessages.EXPLORER_ADDED, type, explorerName);
   }
 
   @Override
   public String addState(String stateName, String... exhibits) {
-    return null;
+    this.stateRepository.add(new StateImpl(stateName));
+
+    return String.format(ConstantMessages.STATE_ADDED, stateName);
   }
 
   @Override
