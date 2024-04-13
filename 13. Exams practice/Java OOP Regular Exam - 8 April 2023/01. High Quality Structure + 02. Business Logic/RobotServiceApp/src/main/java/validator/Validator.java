5f584f03dffc4da25988fc294bfe1dfc6b1d6423
wolfie <yavorhristozov@gmail.com>
2024-04-13 15:59:48 +0300

    add : BaseSupplements validations

---
+++ 13. Exams practice/Java OOP Regular Exam - 8 April 2023/01. High Quality Structure + 02. Business Logic/RobotServiceApp/src/main/java/validator/Validator.java
@@ -0,0 +1,23 @@
+package validator;
+
+import robotService.common.ExceptionMessages;
+
+public class Validator {
+  public static void validateName(String name) {
+    if (name == null || name.trim().isEmpty()) {
+      throw new NullPointerException(ExceptionMessages.ROBOT_NAME_CANNOT_BE_NULL_OR_EMPTY)
+    }
+  }
+
+  public static void validateKind(String kind) {
+    if (kind == null || kind.trim().isEmpty()) {
+      throw new NullPointerException(ExceptionMessages.ROBOT_KIND_CANNOT_BE_NULL_OR_EMPTY);
+    }
+  }
+
+  public static void validatePrice(double price) {
+    if (price <=0) {
+      throw new IllegalArgumentException (ExceptionMessages.ROBOT_PRICE_CANNOT_BE_BELOW_OR_EQUAL_TO_ZERO);
+    }
+  }
+}
