b906041fd3fe87c297865b81fa3ce48753740a96
wolfie <yavorhristozov@gmail.com>
2024-04-16 18:06:53 +0300

    test : add @before setup

--- 13. Exams practice/Java OOP Regular Exam - 8 April 2023/03. Unit testing/Robots/src/test/java/robots/ServiceTests.java
+++ 13. Exams practice/Java OOP Regular Exam - 8 April 2023/03. Unit testing/Robots/src/test/java/robots/ServiceTests.java
@@ -1,5 +1,19 @@
 package robots;
 
+import org.junit.Before;
+
 public class ServiceTests {
+  private Service service;
+  private Robot robot1;
+  private Robot robot2;
+  private Robot robot3;
+
+  @Before
+  public void setUp() {
+    this.service = new Service("bestService", 3);
+    robot1 = new Robot("robot1");
+    robot2 = new Robot("robot2");
+    robot3 = new Robot("robot3");
+  }
 
 }
