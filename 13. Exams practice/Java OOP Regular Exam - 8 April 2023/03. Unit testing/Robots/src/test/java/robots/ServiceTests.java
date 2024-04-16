ba3bbe200eee7080d15005723e1f37bef2f43f0e
wolfie <yavorhristozov@gmail.com>
2024-04-16 18:10:52 +0300

    test : test constructors
    init Robot and Service obj

--- 13. Exams practice/Java OOP Regular Exam - 8 April 2023/03. Unit testing/Robots/src/test/java/robots/ServiceTests.java
+++ 13. Exams practice/Java OOP Regular Exam - 8 April 2023/03. Unit testing/Robots/src/test/java/robots/ServiceTests.java
@@ -1,6 +1,8 @@
 package robots;
 
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
 public class ServiceTests {
   private Service service;
@@ -10,10 +12,22 @@ public class ServiceTests {
 
   @Before
   public void setUp() {
-    this.service = new Service("bestService", 3);
+    this.service = new Service("best", 3);
     robot1 = new Robot("robot1");
     robot2 = new Robot("robot2");
     robot3 = new Robot("robot3");
+  }
+
+  @Test
+  public void testServiceConstructorWorksAsExpected(){
+    Assert.assertEquals(3, service.getCapacity());
+    Assert.assertEquals(0, service.getCount());
+    Assert.assertEquals("best", service.getName());
+  }
+
+  @Test
+  public void testRobotConstructorWorksAsExpected(){
+    Assert.assertEquals("robot1", robot1.getName());
   }
 
 }
