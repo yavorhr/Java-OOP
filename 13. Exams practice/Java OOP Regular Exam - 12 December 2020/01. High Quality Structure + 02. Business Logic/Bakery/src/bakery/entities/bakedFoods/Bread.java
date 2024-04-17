5165bf12a9fb64d24076b39c01b07d7fcbd979b9
wolfie <yavorhristozov@gmail.com>
2024-04-17 13:06:40 +0300

    add : Bread class

---
+++ 13. Exams practice/Java OOP Regular Exam - 12 December 2020/01. High Quality Structure + 02. Business Logic/Bakery/src/bakery/entities/bakedFoods/Bread.java
@@ -0,0 +1,9 @@
+package bakery.entities.bakedFoods;
+
+public class Bread extends BaseBakedFood {
+  private static final double INITIAL_BREAD_PORTION = 200;
+
+  public Bread(String name, double price, String brand) {
+    super(name, INITIAL_BREAD_PORTION, price, brand);
+  }
+}
