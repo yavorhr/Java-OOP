3bcec9f3dc4523293872ddae280c7ab387c9472a
wolfie <yavorhristozov@gmail.com>
2024-04-17 13:02:49 +0300

    add : BakedFoodImpl validations

--- 13. Exams practice/Java OOP Regular Exam - 12 December 2020/01. High Quality Structure + 02. Business Logic/Bakery/src/bakery/entities/bakedFoods/BakedFoodImpl.java
+++ 13. Exams practice/Java OOP Regular Exam - 12 December 2020/01. High Quality Structure + 02. Business Logic/Bakery/src/bakery/entities/bakedFoods/BakedFoodImpl.java
@@ -1,6 +1,8 @@
 package bakery.entities.bakedFoods;
 
 import bakery.entities.bakedFoods.interfaces.BakedFood;
+import bakery.validator.Validator;
+
 
 public abstract class BakedFoodImpl implements BakedFood {
   private String name;
@@ -9,10 +11,10 @@ public abstract class BakedFoodImpl implements BakedFood {
   private String brand;
 
   public BakedFoodImpl(String name, double portion, double price, String brand) {
-    this.name = name;
-    this.portion = portion;
-    this.price = price;
-    this.brand = brand;
+    this.setName(name);
+    this.setPortion(portion);
+    this.setPrice(price);
+    this.setBrand(brand);
   }
 
   @Override
@@ -28,5 +30,25 @@ public abstract class BakedFoodImpl implements BakedFood {
   @Override
   public double getPrice() {
     return this.price;
+  }
+
+  private void setName(String name) {
+    Validator.validateName(name);
+    this.name = name;
+  }
+
+  private void setPortion(double portion) {
+    Validator.validatePortion(portion);
+    this.portion = portion;
+  }
+
+  private void setPrice(double price) {
+    Validator.validatePrice(price);
+    this.price = price;
+  }
+
+  private void setBrand(String brand) {
+    Validator.validateBrand(brand);
+    this.brand = brand;
   }
 }
--- 13. Exams practice/Java OOP Regular Exam - 12 December 2020/01. High Quality Structure + 02. Business Logic/Bakery/src/bakery/validator/Validator.java
+++ 13. Exams practice/Java OOP Regular Exam - 12 December 2020/01. High Quality Structure + 02. Business Logic/Bakery/src/bakery/validator/Validator.java
@@ -1,27 +1,27 @@
-package validator;
+package bakery.validator;
 
 import bakery.common.ExceptionMessages;
 
 public class Validator {
-  private static void validateName(String name) {
+  public static void validateName(String name) {
     if (name == null || name.trim().isEmpty()) {
       throw new IllegalArgumentException(ExceptionMessages.INVALID_NAME);
     }
   }
 
-  private static void validateBrand(String brand) {
+  public static void validateBrand(String brand) {
     if (brand == null || brand.trim().isEmpty()) {
       throw new IllegalArgumentException(ExceptionMessages.INVALID_BRAND);
     }
   }
 
-  private static void validatePortion(int portion) {
+  public static void validatePortion(double portion) {
     if (portion <= 0) {
       throw new IllegalArgumentException(ExceptionMessages.INVALID_PORTION);
     }
   }
 
-  private static void validatePrice(double price) {
+  public static void validatePrice(double price) {
     if (price <= 0) {
       throw new IllegalArgumentException(ExceptionMessages.INVALID_PRICE);
     }
