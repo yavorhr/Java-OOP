8836867c078e11ca5dcb82092b33530ea09ceb8b
wolfie <yavorhristozov@gmail.com>
2024-04-21 11:45:49 +0300

    add : enhanced switch

--- 13. Exams practice/Java OOP Regular Exam - 12 December 2020/01. High Quality Structure + 02. Business Logic/Bakery/src/bakery/core/ControllerImpl.java
+++ 13. Exams practice/Java OOP Regular Exam - 12 December 2020/01. High Quality Structure + 02. Business Logic/Bakery/src/bakery/core/ControllerImpl.java
@@ -11,63 +11,64 @@ import bakery.repositories.interfaces.*;
 public class ControllerImpl implements Controller {
 
 
-    public ControllerImpl(FoodRepository<BakedFood> foodRepository, DrinkRepository<Drink> drinkRepository, TableRepository<Table> tableRepository) {
 
-    }
+  public ControllerImpl(FoodRepository<BakedFood> foodRepository, DrinkRepository<Drink> drinkRepository, TableRepository<Table> tableRepository) {
+
+  }
 
 
-    @Override
-    public String addFood(String type, String name, double price) {
-        //TODO:
-        return null;
-    }
+  @Override
+  public String addFood(String type, String name, double price) {
+    //TODO:
+    return null;
+  }
 
-    @Override
-    public String addDrink(String type, String name, int portion, String brand) {
-        //TODO:
-        return null;
-    }
+  @Override
+  public String addDrink(String type, String name, int portion, String brand) {
+    //TODO:
+    return null;
+  }
 
-    @Override
-    public String addTable(String type, int tableNumber, int capacity) {
-        //TODO:
-        return null;
-    }
+  @Override
+  public String addTable(String type, int tableNumber, int capacity) {
+    //TODO:
+    return null;
+  }
 
-    @Override
-    public String reserveTable(int numberOfPeople) {
-        //TODO:
-        return null;
-    }
+  @Override
+  public String reserveTable(int numberOfPeople) {
+    //TODO:
+    return null;
+  }
 
-    @Override
-    public String orderFood(int tableNumber, String foodName) {
-        //TODO:
-        return null;
-    }
+  @Override
+  public String orderFood(int tableNumber, String foodName) {
+    //TODO:
+    return null;
+  }
 
-    @Override
-    public String orderDrink(int tableNumber, String drinkName, String drinkBrand) {
-        //TODO:
-        return null;
+  @Override
+  public String orderDrink(int tableNumber, String drinkName, String drinkBrand) {
+    //TODO:
+    return null;
 
-    }
+  }
 
-    @Override
-    public String leaveTable(int tableNumber) {
-        //TODO:
-        return null;
-    }
+  @Override
+  public String leaveTable(int tableNumber) {
+    //TODO:
+    return null;
+  }
 
-    @Override
-    public String getFreeTablesInfo() {
-        //TODO:
-        return null;
-    }
+  @Override
+  public String getFreeTablesInfo() {
+    //TODO:
+    return null;
+  }
 
-    @Override
-    public String getTotalIncome() {
-        //TODO:
-        return null;
-    }
+  @Override
+  public String getTotalIncome() {
+    //TODO:
+    return null;
+  }
 }
--- 13. Exams practice/Java OOP Regular Exam - 12 December 2020/01. High Quality Structure + 02. Business Logic/Bakery/src/bakery/core/EngineImpl.java
+++ 13. Exams practice/Java OOP Regular Exam - 12 December 2020/01. High Quality Structure + 02. Business Logic/Bakery/src/bakery/core/EngineImpl.java
@@ -10,85 +10,62 @@ import java.io.IOException;
 import java.util.Arrays;
 
 public class EngineImpl implements Engine {
-    private ConsoleReader reader;
-    private ConsoleWriter writer;
-    private Controller controller;
+  private ConsoleReader reader;
+  private ConsoleWriter writer;
+  private Controller controller;
 
-    public EngineImpl(ConsoleReader reader, ConsoleWriter writer, Controller controller) {
-        this.reader = reader;
-        this.writer = writer;
-        this.controller = controller;
-    }
+  public EngineImpl(ConsoleReader reader, ConsoleWriter writer, Controller controller) {
+    this.reader = reader;
+    this.writer = writer;
+    this.controller = controller;
+  }
 
-    public void run() {
-        while (true) {
-            String result = null;
-            try {
-                result = processInput();
+  public void run() {
+    while (true) {
+      String result = null;
+      try {
+        result = processInput();
 
-                if (result.equals(Commands.END.name())) {
-                    break;
-                }
-
-            } catch (IOException | IllegalArgumentException | NullPointerException e) {
-                result = e.getMessage();
-            }
-
-            this.writer.writeLine(result);
+        if (result.equals(Commands.END.name())) {
+          break;
         }
 
+      } catch (IOException | IllegalArgumentException | NullPointerException e) {
+        result = e.getMessage();
+      }
+
+      this.writer.writeLine(result);
     }
 
-    private String processInput() throws IOException {
-        String input = this.reader.readLine();
-        String[] tokens = input.split("\\s+");
+  }
 
-        Commands command = Commands.valueOf(tokens[0]);
-        String[] data = Arrays.stream(tokens).skip(1).toArray(String[]::new);
+  private String processInput() throws IOException {
+    String input = this.reader.readLine();
+    String[] tokens = input.split("\\s+");
 
-        String result = null;
+    Commands command = Commands.valueOf(tokens[0]);
+    String[] data = Arrays.stream(tokens).skip(1).toArray(String[]::new);
 
-        switch (command) {
-            case AddDrink:
-                result = this.controller.addDrink(
-                        data[0], data[1], Integer.parseInt(data[2]), data[3]
-                );
-                break;
-            case AddFood:
-                result = this.controller.addFood(
-                        data[0], data[1], Double.parseDouble(data[2])
-                );
-                break;
-            case AddTable:
-                result = this.controller.addTable(
-                        data[0],Integer.parseInt(data[1]),Integer.parseInt(data[2])
-                        );
-                break;
-            case ReserveTable:
-                result = this.controller.reserveTable(Integer.parseInt(data[0]));
-                break;
-            case OrderDrink:
-                result = this.controller.orderDrink(Integer.parseInt(data[0]),data[1],data[2]);
-                break;
-            case OrderFood:
-                result = this.controller.orderFood(Integer.parseInt(data[0]),data[1]);
-                break;
-            case LeaveTable:
-                result = this.controller.leaveTable(Integer.parseInt(data[0]));
-                break;
-            case GetFreeTablesInfo:
-                result = this.controller.getFreeTablesInfo();
-                break;
-            case GetTotalIncome:
-                result = this.controller.getTotalIncome();
-                break;
-            case END:
-                result = Commands.END.name();
-                break;
-        }
+    String result = switch (command) {
+      case AddDrink -> this.controller.addDrink(
+              data[0], data[1], Integer.parseInt(data[2]), data[3]
+      );
+      case AddFood -> this.controller.addFood(
+              data[0], data[1], Double.parseDouble(data[2])
+      );
+      case AddTable -> this.controller.addTable(
+              data[0], Integer.parseInt(data[1]), Integer.parseInt(data[2])
+      );
+      case ReserveTable -> this.controller.reserveTable(Integer.parseInt(data[0]));
+      case OrderDrink -> this.controller.orderDrink(Integer.parseInt(data[0]), data[1], data[2]);
+      case OrderFood -> this.controller.orderFood(Integer.parseInt(data[0]), data[1]);
+      case LeaveTable -> this.controller.leaveTable(Integer.parseInt(data[0]));
+      case GetFreeTablesInfo -> this.controller.getFreeTablesInfo();
+      case GetTotalIncome -> this.controller.getTotalIncome();
+      case END -> Commands.END.name();
+    };
 
 
-        return result.trim();
-    }
-
+    return result.trim();
+  }
 }
