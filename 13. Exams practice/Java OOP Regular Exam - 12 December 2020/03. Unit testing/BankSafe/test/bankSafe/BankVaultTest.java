768e5ecd0616f86848c23482456198a08ca9b5df
wolfie <yavorhristozov@gmail.com>
2024-04-22 11:41:14 +0300

    test : add setUp()
    @Before init BankVault and Item obj

--- 13. Exams practice/Java OOP Regular Exam - 12 December 2020/03. Unit testing/BankSafe/test/bankSafe/BankVaultTest.java
+++ 13. Exams practice/Java OOP Regular Exam - 12 December 2020/03. Unit testing/BankSafe/test/bankSafe/BankVaultTest.java
@@ -1,8 +1,20 @@
 package bankSafe;
 
 
-public class BankVaultTest {
+import org.junit.Before;
 
-    //TODO: Write your tests here
+public class BankVaultTest {
+  private BankVault bankVault;
+  private Item item1;
+  private Item item2;
+
+
+  @Before
+  public void setUp() {
+    this.bankVault = new BankVault();
+    item1 = new Item("George", "123");
+    item2 = new Item("Mike", "1234");
+  }
+
 
 }
