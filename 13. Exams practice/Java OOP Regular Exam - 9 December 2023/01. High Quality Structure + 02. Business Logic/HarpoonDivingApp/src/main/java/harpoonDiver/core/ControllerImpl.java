3729236806f995dad833857301485d6051f4e764
wolfie <yavorhristozov@gmail.com>
2024-04-23 18:15:23 +0300

    add : startDiving()
    in Controller class

--- 13. Exams practice/Java OOP Regular Exam - 9 December 2023/01. High Quality Structure + 02. Business Logic/HarpoonDivingApp/src/main/java/harpoonDiver/core/ControllerImpl.java
+++ 13. Exams practice/Java OOP Regular Exam - 9 December 2023/01. High Quality Structure + 02. Business Logic/HarpoonDivingApp/src/main/java/harpoonDiver/core/ControllerImpl.java
@@ -4,6 +4,8 @@ import harpoonDiver.common.ConstantMessages;
 import harpoonDiver.common.ExceptionMessages;
 import harpoonDiver.factory.DiverFactory;
 import harpoonDiver.models.diver.Diver;
+import harpoonDiver.models.diving.Diving;
+import harpoonDiver.models.diving.DivingImpl;
 import harpoonDiver.models.divingSite.DivingSite;
 import harpoonDiver.models.divingSite.DivingSiteImpl;
 import harpoonDiver.repositories.DiverRepository;
@@ -11,14 +13,19 @@ import harpoonDiver.repositories.DivingSiteRepository;
 import harpoonDiver.repositories.Repository;
 
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class ControllerImpl implements Controller {
   private Repository<Diver> divers;
   private Repository<DivingSite> divingSites;
+  private Diving diving;
 
   public ControllerImpl() {
     this.divers = new DiverRepository();
     this.divingSites = new DivingSiteRepository();
+    this.diving = new DivingImpl();
   }
 
   @Override
@@ -47,12 +54,17 @@ public class ControllerImpl implements Controller {
     return String.format(ConstantMessages.DIVER_REMOVE, diverName);
   }
 
-
   @Override
   public String startDiving(String siteName) {
-    //TODO
-    return null;
+    validateIfThereAreDiversWithEnoughOxygen();
+
+    DivingSite diving = this.divingSites.byName(siteName);
+    Collection<Diver> divers = getDiversWithOxygen();
+
+    this.diving.searching(diving, divers);
+    return String.format(ConstantMessages.SITE_DIVING, siteName, divers.size());
   }
+
 
   @Override
   public String getStatistics() {
@@ -70,6 +82,23 @@ public class ControllerImpl implements Controller {
     if (this.divers.byName(diverName) == null) {
       throw new IllegalArgumentException(String.format(ExceptionMessages.DIVER_DOES_NOT_EXIST, diverName));
     }
+  }
+
+  private void validateIfThereAreDiversWithEnoughOxygen() {
+    boolean noDiversWithEnoughOxygen = this.divers.getCollection()
+            .stream()
+            .noneMatch(d -> d.getOxygen() > 0);
+
+    if (noDiversWithEnoughOxygen) {
+      throw new IllegalArgumentException(ExceptionMessages.SITE_DIVERS_DOES_NOT_EXISTS);
+    }
+  }
+
+  private Collection<Diver> getDiversWithOxygen() {
+    return this.divers.getCollection()
+            .stream()
+            .filter(d -> d.getOxygen() >= 30)
+            .collect(Collectors.toList());
   }
 
 }
