8d99e378ee68a0b6d064e97195e03a59ceb12579
wolfie <yavorhristozov@gmail.com>
2024-04-23 16:58:49 +0300

    add : DivingImpl  class

---
+++ 13. Exams practice/Java OOP Regular Exam - 9 December 2023/01. High Quality Structure + 02. Business Logic/HarpoonDivingApp/src/main/java/harpoonDiver/models/diving/DivingImpl.java
@@ -0,0 +1,15 @@
+package harpoonDiver.models.diving;
+
+import harpoonDiver.models.diver.Diver;
+import harpoonDiver.models.divingSite.DivingSite;
+
+import java.util.Collection;
+
+public class DivingImpl implements Diving {
+
+
+  @Override
+  public void searching(DivingSite divingSite, Collection<Diver> divers) {
+
+  }
+}
