236991156c14fe16824214103d0e792e0d92bc65
wolfie <yavorhristozov@gmail.com>
2024-04-23 17:39:30 +0300

    add : removeDiver() impl
    Controller class

--- 13. Exams practice/Java OOP Regular Exam - 9 December 2023/01. High Quality Structure + 02. Business Logic/HarpoonDivingApp/src/main/java/harpoonDiver/core/ControllerImpl.java
+++ 13. Exams practice/Java OOP Regular Exam - 9 December 2023/01. High Quality Structure + 02. Business Logic/HarpoonDivingApp/src/main/java/harpoonDiver/core/ControllerImpl.java
@@ -1,20 +1,24 @@
 package harpoonDiver.core;
 
 import harpoonDiver.common.ConstantMessages;
+import harpoonDiver.common.ExceptionMessages;
 import harpoonDiver.factory.DiverFactory;
 import harpoonDiver.models.diver.Diver;
 import harpoonDiver.models.divingSite.DivingSite;
 import harpoonDiver.models.divingSite.DivingSiteImpl;
 import harpoonDiver.repositories.DiverRepository;
+import harpoonDiver.repositories.DivingSiteRepository;
 import harpoonDiver.repositories.Repository;
 
 import java.util.Arrays;
 
 public class ControllerImpl implements Controller {
   private Repository<Diver> divers;
+  private Repository<DivingSite> divingSites;
 
   public ControllerImpl() {
     this.divers = new DiverRepository();
+    this.divingSites = new DivingSiteRepository();
   }
 
   @Override
@@ -28,14 +32,21 @@ public class ControllerImpl implements Controller {
   public String addDivingSite(String siteName, String... seaCreatures) {
     DivingSite divingSite = new DivingSiteImpl(siteName);
     addSeaCreaturesToSite(divingSite, seaCreatures);
-    return String.format(ConstantMessages.DIVING_SITE_ADDED,siteName);
+    this.divingSites.add(divingSite);
+
+    return String.format(ConstantMessages.DIVING_SITE_ADDED, siteName);
   }
 
   @Override
   public String removeDiver(String diverName) {
-    //TODO
-    return null;
+    validateIfDiverExistInRepository(diverName);
+
+    Diver diver = this.divers.byName(diverName);
+    this.divers.remove(diver);
+
+    return String.format(ConstantMessages.DIVER_REMOVE, diverName);
   }
+
 
   @Override
   public String startDiving(String siteName) {
@@ -54,4 +65,11 @@ public class ControllerImpl implements Controller {
   private void addSeaCreaturesToSite(DivingSite divingSite, String[] seaCreatures) {
     Arrays.stream(seaCreatures).forEach(c -> divingSite.getSeaCreatures().add(c));
   }
+
+  private void validateIfDiverExistInRepository(String diverName) {
+    if (this.divers.byName(diverName) == null) {
+      throw new IllegalArgumentException(String.format(ExceptionMessages.DIVER_DOES_NOT_EXIST, diverName));
+    }
+  }
+
 }
