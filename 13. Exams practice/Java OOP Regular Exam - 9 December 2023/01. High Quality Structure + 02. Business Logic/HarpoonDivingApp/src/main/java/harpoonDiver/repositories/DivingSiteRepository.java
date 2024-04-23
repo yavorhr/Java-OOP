6f8e986c79c7e933d57cf76a66f3d5a59c7d99ea
wolfie <yavorhristozov@gmail.com>
2024-04-23 17:11:13 +0300

    add : DivingSiteRepository class

---
+++ 13. Exams practice/Java OOP Regular Exam - 9 December 2023/01. High Quality Structure + 02. Business Logic/HarpoonDivingApp/src/main/java/harpoonDiver/repositories/DivingSiteRepository.java
@@ -0,0 +1,39 @@
+package harpoonDiver.repositories;
+
+import harpoonDiver.models.divingSite.DivingSite;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class DivingSiteRepository implements Repository<DivingSite> {
+  private Collection<DivingSite> divingSites;
+
+  public DivingSiteRepository() {
+    this.divingSites = new ArrayList<>();
+  }
+
+  @Override
+  public Collection<DivingSite> getCollection() {
+    return Collections.unmodifiableCollection(this.divingSites;
+  }
+
+  @Override
+  public void add(DivingSite divingSite) {
+    this.divingSites.add(divingSite);
+  }
+
+  @Override
+  public boolean remove(DivingSite divingSite) {
+    return this.divingSites.remove(divingSite);
+  }
+
+  @Override
+  public DivingSite byName(String name) {
+    return this.divingSites
+            .stream()
+            .filter(d -> d.getName().equals(name))
+            .findFirst()
+            .orElse(null);
+  }
+}
