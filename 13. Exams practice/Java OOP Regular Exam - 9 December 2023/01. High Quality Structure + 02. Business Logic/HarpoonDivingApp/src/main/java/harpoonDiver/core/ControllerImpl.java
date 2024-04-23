80e604ceea69bf7d5725a766338fce888e148d57
wolfie <yavorhristozov@gmail.com>
2024-04-23 17:23:52 +0300

    add : addDiver() impl
    in ControllerImpl class

--- 13. Exams practice/Java OOP Regular Exam - 9 December 2023/01. High Quality Structure + 02. Business Logic/HarpoonDivingApp/src/main/java/harpoonDiver/core/ControllerImpl.java
+++ 13. Exams practice/Java OOP Regular Exam - 9 December 2023/01. High Quality Structure + 02. Business Logic/HarpoonDivingApp/src/main/java/harpoonDiver/core/ControllerImpl.java
@@ -1,33 +1,46 @@
 package harpoonDiver.core;
 
-public class ControllerImpl implements Controller{
-    @Override
-    public String addDiver(String kind, String diverName) {
-        //TODO
-        return null;
-    }
+import harpoonDiver.common.ConstantMessages;
+import harpoonDiver.factory.DiverFactory;
+import harpoonDiver.models.diver.Diver;
+import harpoonDiver.repositories.DiverRepository;
+import harpoonDiver.repositories.Repository;
 
-    @Override
-    public String addDivingSite(String siteName, String... seaCreatures) {
-        //TODO
-        return null;
-    }
+public class ControllerImpl implements Controller {
+  private Repository<Diver> divers;
 
-    @Override
-    public String removeDiver(String diverName) {
-        //TODO
-        return null;
-    }
+  public ControllerImpl() {
+    this.divers = new DiverRepository();
+  }
 
-    @Override
-    public String startDiving(String siteName) {
-        //TODO
-        return null;
-    }
+  @Override
+  public String addDiver(String kind, String diverName) {
+    Diver diver = DiverFactory.create(kind, diverName);
+    this.divers.add(diver);
+    return String.format(ConstantMessages.DIVER_ADDED,kind,diverName);
+  }
 
-    @Override
-    public String getStatistics() {
-        //TODO
-        return null;
-    }
+  @Override
+  public String addDivingSite(String siteName, String... seaCreatures) {
+    //TODO
+    return null;
+  }
+
+  @Override
+  public String removeDiver(String diverName) {
+    //TODO
+    return null;
+  }
+
+  @Override
+  public String startDiving(String siteName) {
+    //TODO
+    return null;
+  }
+
+  @Override
+  public String getStatistics() {
+    //TODO
+    return null;
+  }
 }
