d16e72338a5aba32939eb370278e0a688e1ee8d1
wolfie <yavorhristozov@gmail.com>
2024-04-25 13:10:31 +0300

    test : Diving constructor

--- 13. Exams practice/Java OOP Regular Exam - 9 December 2023/03. Unit testing/scubaDive/src/test/java/scubaDive/DivingTests.java
+++ 13. Exams practice/Java OOP Regular Exam - 9 December 2023/03. Unit testing/scubaDive/src/test/java/scubaDive/DivingTests.java
@@ -1,7 +1,9 @@
 package scubaDive;
 
 
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
 public class DivingTests {
   private Diving emptyDiving;
@@ -18,4 +20,27 @@ public class DivingTests {
     this.diving.addDeepWaterDiver(deepWaterDiver1);
     this.diving.addDeepWaterDiver(deepWaterDiver2);
   }
+
+  // test diving constructor
+  @Test
+  public void testDivingCtorShouldWorksCorrect() {
+    Assert.assertEquals("emptyDiving", emptyDiving.getName());
+    Assert.assertEquals(3, emptyDiving.getCapacity());
+    Assert.assertEquals(0, emptyDiving.getCount());
+  }
+
+  @Test (expected = IllegalArgumentException.class)
+  public void testDivingCtorShouldThrowErrorWhenNegativeCapacity() {
+   new Diving("test", -1);
+  }
+
+  @Test (expected = NullPointerException.class)
+  public void testDivingCtorShouldThrowErrorWhenEmptyName() {
+    new Diving("", 1);
+  }
+  @Test (expected = NullPointerException.class)
+  public void testDivingCtorShouldThrowErrorWhenNullName() {
+    new Diving(null, 1);
+  }
+
 }
