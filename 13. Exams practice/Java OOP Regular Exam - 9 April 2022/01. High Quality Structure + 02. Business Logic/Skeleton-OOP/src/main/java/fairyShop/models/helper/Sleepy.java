e5fa334f3c6541bc1e16c27631531fddd0015b1c
wolfie <yavorhristozov@gmail.com>
2024-04-26 13:25:16 +0300

    add : Sleepy class
    child class of BaseHelper

---
+++ 13. Exams practice/Java OOP Regular Exam - 9 April 2022/01. High Quality Structure + 02. Business Logic/Skeleton-OOP/src/main/java/fairyShop/models/helper/Sleepy.java
@@ -0,0 +1,15 @@
+package fairyShop.models.helper;
+
+public class Sleepy extends BaseHelper {
+  private static final int INITIAL_ENERGY_UNITS = 50;
+  private static final int DECREASE_UNITS_VALUE = 15;
+
+  protected Sleepy(String name) {
+    super(name, INITIAL_ENERGY_UNITS);
+  }
+
+  @Override
+  public void work() {
+    super.decreaseEnergy(DECREASE_UNITS_VALUE);
+  }
+}
