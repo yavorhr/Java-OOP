bbacffb994450e5bb1cc21cb3882f7af63237008
wolfie <yavorhristozov@gmail.com>
2024-04-26 13:41:15 +0300

    add : ShopImpl class
    method craft() still not implemented

---
+++ 13. Exams practice/Java OOP Regular Exam - 9 April 2022/01. High Quality Structure + 02. Business Logic/Skeleton-OOP/src/main/java/fairyShop/models/shop/ShopImpl.java
@@ -0,0 +1,12 @@
+package fairyShop.models.shop;
+
+import fairyShop.models.helper.Helper;
+import fairyShop.models.present.Present;
+
+public class ShopImpl implements Shop {
+
+  @Override
+  public void craft(Present present, Helper helper) {
+
+  }
+}
