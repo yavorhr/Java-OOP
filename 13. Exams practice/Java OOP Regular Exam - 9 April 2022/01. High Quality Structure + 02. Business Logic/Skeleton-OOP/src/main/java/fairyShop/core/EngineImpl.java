0a485898ec6190f81bf3f435457247b5ae38166f
wolfie <yavorhristozov@gmail.com>
2024-04-26 14:02:37 +0300

    add : ProcessInput commands

--- 13. Exams practice/Java OOP Regular Exam - 9 April 2022/01. High Quality Structure + 02. Business Logic/Skeleton-OOP/src/main/java/fairyShop/core/EngineImpl.java
+++ 13. Exams practice/Java OOP Regular Exam - 9 April 2022/01. High Quality Structure + 02. Business Logic/Skeleton-OOP/src/main/java/fairyShop/core/EngineImpl.java
@@ -8,85 +8,83 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class EngineImpl implements Engine {
-    private Controller controller;
-    private BufferedReader reader;
+  private Controller controller;
+  private BufferedReader reader;
 
-    public EngineImpl() {
-        this.controller = new ControllerImpl();
-        this.reader = new BufferedReader(new InputStreamReader(System.in));
-    }
+  public EngineImpl() {
+    this.controller = new ControllerImpl();
+    this.reader = new BufferedReader(new InputStreamReader(System.in));
+  }
 
-    @Override
-    public void run() {
-        while (true) {
-            String result = null;
-            try {
-                result = processInput();
+  @Override
+  public void run() {
+    while (true) {
+      String result = null;
+      try {
+        result = processInput();
 
-                if (result.equals("Exit")) {
-                    break;
-                }
-            } catch (NullPointerException | IllegalArgumentException | IOException e) {
-                result = e.getMessage();
-            }
-
-            System.out.println(result);
+        if (result.equals("Exit")) {
+          break;
         }
+      } catch (NullPointerException | IllegalArgumentException | IOException e) {
+        result = e.getMessage();
+      }
+
+      System.out.println(result);
     }
+  }
 
-    private String processInput() throws IOException {
-        String input = this.reader.readLine();
-        String[] tokens = input.split("\\s+");
+  private String processInput() throws IOException {
+    String input = this.reader.readLine();
+    String[] tokens = input.split("\\s+");
 
-        Command command = Command.valueOf(tokens[0]);
-        String result = null;
-        String[] data = Arrays.stream(tokens).skip(1).toArray(String[]::new);
+    Command command = Command.valueOf(tokens[0]);
+    String result = null;
+    String[] data = Arrays.stream(tokens).skip(1).toArray(String[]::new);
 
-        switch (command) {
-            case AddHelper:
-                result = addHelper(data);
-                break;
-            case AddPresent:
-                result = addPresent(data);
-                break;
-            case AddInstrumentToHelper:
-                result = addInstrumentToHelper(data);
-                break;
-            case CraftPresent:
-                result = craftPresent(data);
-                break;
-            case Report:
-                result = report();
-                break;
-            case Exit:
-                result = Command.Exit.name();
-                break;
-        }
-        return result;
+    switch (command) {
+      case AddHelper:
+        result = addHelper(data);
+        break;
+      case AddPresent:
+        result = addPresent(data);
+        break;
+      case AddInstrumentToHelper:
+        result = addInstrumentToHelper(data);
+        break;
+      case CraftPresent:
+        result = craftPresent(data);
+        break;
+      case Report:
+        result = report();
+        break;
+      case Exit:
+        result = Command.Exit.name();
+        break;
     }
+    return result;
+  }
 
-    private String addHelper(String[] data) {
-        //TODO
-        return null;
-    }
+  private String addHelper(String[] data) {
+    return this.controller.addHelper(data[0], data[1]);
+  }
 
-    private String addPresent(String[] data) {
-        //TODO
-        return null;
-    }
+  private String addPresent(String[] data) {
+    return this.controller.addPresent(data[0],
+            Integer.parseInt(data[1]));
+  }
 
-    private String addInstrumentToHelper(String[] data) {
-        //TODO
-        return null;
-    }
+  private String addInstrumentToHelper(String[] data) {
+    return this.controller.addInstrumentToHelper(data[0],
+            Integer.parseInt(data[1]));
+  }
 
-    private String report() {
-        //TODO
-        return null;
-    }
+  private String report() {
+    return this.controller.report();
 
-    private String craftPresent(String[] data) {
-        //TODO
-        return null;
-    }
+  }
+
+  private String craftPresent(String[] data) {
+    return this.controller.craftPresent(data[0]);
+  }
 }
