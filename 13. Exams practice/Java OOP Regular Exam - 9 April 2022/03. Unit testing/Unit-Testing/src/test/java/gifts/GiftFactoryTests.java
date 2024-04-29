e7fb5c7a94fe147cf535439d17732c91d904ce48
wolfie <yavorhristozov@gmail.com>
2024-04-29 16:26:06 +0300

    test : add void setUp() func
    @before each test init GiftFactory & Gift obj

--- 13. Exams practice/Java OOP Regular Exam - 9 April 2022/03. Unit testing/Unit-Testing/src/test/java/gifts/GiftFactoryTests.java
+++ 13. Exams practice/Java OOP Regular Exam - 9 April 2022/03. Unit testing/Unit-Testing/src/test/java/gifts/GiftFactoryTests.java
@@ -1,5 +1,22 @@
 package gifts;
 
+import org.junit.Before;
+
 public class GiftFactoryTests {
+  private GiftFactory emptyFactory;
+  private GiftFactory giftFactory;
+  private Gift gift1;
+  private Gift gift2;
+
+  @Before
+  public void setUp() {
+    this.emptyFactory = new GiftFactory();
+    this.giftFactory = new GiftFactory();
+    this.gift1 = new Gift("car", 30);
+    this.gift2 = new Gift("doll", 40);
+    
+    this.giftFactory.createGift(gift1);
+    this.giftFactory.createGift(gift2);
+  }
 
 }
