6992dde328800f255ffa837fcd246338665731a2
wolfie <yavorhristozov@gmail.com>
2024-04-29 16:28:33 +0300

    test : GiftFactory ctor

--- 13. Exams practice/Java OOP Regular Exam - 9 April 2022/03. Unit testing/Unit-Testing/src/test/java/gifts/GiftFactoryTests.java
+++ 13. Exams practice/Java OOP Regular Exam - 9 April 2022/03. Unit testing/Unit-Testing/src/test/java/gifts/GiftFactoryTests.java
@@ -1,6 +1,8 @@
 package gifts;
 
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
 public class GiftFactoryTests {
   private GiftFactory emptyFactory;
@@ -14,9 +16,15 @@ public class GiftFactoryTests {
     this.giftFactory = new GiftFactory();
     this.gift1 = new Gift("car", 30);
     this.gift2 = new Gift("doll", 40);
-    
+
     this.giftFactory.createGift(gift1);
     this.giftFactory.createGift(gift2);
   }
 
+  // test GiftFactory constructor
+  @Test
+  public void testGiftFactoryCtorShouldWorksCorrect() {
+    Assert.assertEquals(0, this.emptyFactory.getPresents().size());
+    Assert.assertEquals(0, this.emptyFactory.getCount());
+  }
 }
