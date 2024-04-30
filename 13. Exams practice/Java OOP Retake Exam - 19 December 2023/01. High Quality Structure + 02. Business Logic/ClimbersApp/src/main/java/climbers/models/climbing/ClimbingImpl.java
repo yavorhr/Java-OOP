00d6e599ca3dfbbc236799a856d92c9c8afb2635
wolfie <yavorhristozov@gmail.com>
2024-04-30 11:34:02 +0300

    add : ClimbingImpl  class

---
+++ 13. Exams practice/Java OOP Retake Exam - 19 December 2023/01. High Quality Structure + 02. Business Logic/ClimbersApp/src/main/java/climbers/models/climbing/ClimbingImpl.java
@@ -0,0 +1,14 @@
+package climbers.models.climbing;
+
+import climbers.models.climber.Climber;
+import climbers.models.mountain.Mountain;
+
+import java.util.Collection;
+
+public class ClimbingImpl implements Climbing {
+
+  @Override
+  public void conqueringPeaks(Mountain mountain, Collection<Climber> climbers) {
+
+  }
+}
