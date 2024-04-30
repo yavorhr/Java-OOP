27eb7ff0cd7efba88f011dfc4973cccb5ad29f7d
wolfie <yavorhristozov@gmail.com>
2024-04-30 11:09:53 +0300

    add : BaseClimer
    abstract class

---
+++ 13. Exams practice/Java OOP Retake Exam - 19 December 2023/01. High Quality Structure + 02. Business Logic/ClimbersApp/src/main/java/climbers/models/climber/BaseClimber.java
@@ -0,0 +1,40 @@
+package climbers.models.climber;
+
+import climbers.models.roster.Roster;
+
+public abstract class BaseClimber implements Climber {
+  private String name;
+  private double strength;
+  private Roster roster;
+
+  protected BaseClimber(String name, double strength) {
+    this.name = name;
+    this.strength = strength;
+    this.roster = new RosterImpl();
+  }
+
+  @Override
+  public String getName() {
+    return this.name;
+  }
+
+  @Override
+  public double getStrength() {
+    return this.strength;
+  }
+
+  @Override
+  public boolean canClimb() {
+    return this.strength > 0;
+  }
+
+  @Override
+  public Roster getRoster() {
+    return this.roster;
+  }
+
+  @Override
+  public void climb() {
+
+  }
+}
