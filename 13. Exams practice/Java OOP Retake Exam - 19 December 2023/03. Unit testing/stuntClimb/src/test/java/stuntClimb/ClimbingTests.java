9fef7a466b01eedfab39773059155a0355758bab
wolfie <yavorhristozov@gmail.com>
2024-05-01 14:52:34 +0300

    test : Climbing class constructor

--- 13. Exams practice/Java OOP Retake Exam - 19 December 2023/03. Unit testing/stuntClimb/src/test/java/stuntClimb/ClimbingTests.java
+++ 13. Exams practice/Java OOP Retake Exam - 19 December 2023/03. Unit testing/stuntClimb/src/test/java/stuntClimb/ClimbingTests.java
@@ -1,7 +1,9 @@
 package stuntClimb;
 
 
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
 public class ClimbingTests {
   private Climbing climbing;
@@ -12,12 +14,20 @@ public class ClimbingTests {
   @Before
   public void setUp() {
     this.emptyClimbing = new Climbing("empty", 5);
-
     this.climbing = new Climbing("best", 2);
+
     this.rockClimber1 = new RockClimber("Mike", 50);
     this.rockClimber2 = new RockClimber("Dean", 100);
     this.climbing.addRockClimber(rockClimber1);
     this.climbing.addRockClimber(rockClimber2);
+  }
+
+  //test constructors
+  @Test
+  public void testClimbingCtorShouldWorksCorrect(){
+    Assert.assertEquals("empty", this.emptyClimbing.getName());
+    Assert.assertEquals(0, this.emptyClimbing.getCount());
+    Assert.assertEquals(5, this.emptyClimbing.getCapacity());
   }
 
 }
