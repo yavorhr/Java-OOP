d4e83a311d10d53d4580b2995da501465a18fb67
wolfie <yavorhristozov@gmail.com>
2024-05-01 12:39:06 +0300

    add : startClimbing()

--- 13. Exams practice/Java OOP Retake Exam - 19 December 2023/01. High Quality Structure + 02. Business Logic/ClimbersApp/src/main/java/climbers/core/ControllerImpl.java
+++ 13. Exams practice/Java OOP Retake Exam - 19 December 2023/01. High Quality Structure + 02. Business Logic/ClimbersApp/src/main/java/climbers/core/ControllerImpl.java
@@ -7,15 +7,21 @@ import climbers.models.climber.Climber;
 import climbers.models.mountain.Mountain;
 import climbers.models.mountain.MountainImpl;
 import climbers.repositories.ClimberRepository;
+import climbers.repositories.MountainRepository;
 import climbers.repositories.Repository;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class ControllerImpl implements Controller {
   private Repository<Climber> climberRepository;
+  private Repository<Mountain> mountainRepository;
+  private static int CLIMBED_MOUNTAINS = 0;
 
   public ControllerImpl() {
     this.climberRepository = new ClimberRepository();
+    this.mountainRepository = new MountainRepository();
   }
 
   @Override
@@ -41,16 +47,28 @@ public class ControllerImpl implements Controller {
     return String.format(ConstantMessages.CLIMBER_REMOVE, climberName);
   }
 
-  private void doesClimberExist(String climberName, Climber climber) {
-    if (climber == null) {
-      throw new IllegalArgumentException(String.format(ExceptionMessages.CLIMBER_DOES_NOT_EXIST, climberName));
-    }
-  }
-
   @Override
   public String startClimbing(String mountainName) {
-    return null;
+    validateIfClimbers();
+    Mountain mountain = this.mountainRepository.byName(mountainName);
+    CLIMBED_MOUNTAINS++;
+
+    List<String> peaks = new ArrayList<>(mountain.getPeaksList());
+    int removedClimbers = 0;
+
+    for (Climber climber : this.climberRepository.getCollection()) {
+      while (climber.canClimb() && peaks.size() > 0) {
+        climber.climb();
+        climber.getRoster().getPeaks().add(peaks.remove(0));
+
+        removedClimbers = deleteClimberWithoutStrength(removedClimbers, climber);
+      }
+    }
+
+
+    return String.format(ConstantMessages.PEAK_CLIMBING, mountainName, removedClimbers);
   }
+
 
   @Override
   public String getStatistics() {
@@ -63,5 +81,26 @@ public class ControllerImpl implements Controller {
     if (peaks.length > 0) {
       Arrays.stream(peaks).forEach(p -> mountain.getPeaksList().add(p));
     }
+  }
+
+  private void doesClimberExist(String climberName, Climber climber) {
+    if (climber == null) {
+      throw new IllegalArgumentException(String.format(ExceptionMessages.CLIMBER_DOES_NOT_EXIST, climberName));
+    }
+  }
+
+  private void validateIfClimbers() {
+    if (this.climberRepository.getCollection().stream().noneMatch(Climber::canClimb)) {
+      throw new IllegalArgumentException(ExceptionMessages.THERE_ARE_NO_CLIMBERS);
+    }
+  }
+
+
+  private int deleteClimberWithoutStrength(int removedClimbers, Climber climber) {
+    if (!climber.canClimb()) {
+      removedClimbers++;
+      this.climberRepository.remove(climber);
+    }
+    return removedClimbers;
   }
 }
