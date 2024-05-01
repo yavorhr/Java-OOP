c2ae8beee7658d8bac1bb6e254b31c4d858f98b2
wolfie <yavorhristozov@gmail.com>
2024-05-01 14:49:46 +0300

    add : setUp()
    init new objects @Before each test

--- 13. Exams practice/Java OOP Retake Exam - 19 December 2023/03. Unit testing/stuntClimb/src/test/java/stuntClimb/ClimbingTests.java
+++ 13. Exams practice/Java OOP Retake Exam - 19 December 2023/03. Unit testing/stuntClimb/src/test/java/stuntClimb/ClimbingTests.java
@@ -1,8 +1,23 @@
 package stuntClimb;
 
 
+import org.junit.Before;
 
 public class ClimbingTests {
+  private Climbing climbing;
+  private Climbing emptyClimbing;
+  private RockClimber rockClimber1;
+  private RockClimber rockClimber2;
 
+  @Before
+  public void setUp() {
+    this.emptyClimbing = new Climbing("empty", 5);
+
+    this.climbing = new Climbing("best", 2);
+    this.rockClimber1 = new RockClimber("Mike", 50);
+    this.rockClimber2 = new RockClimber("Dean", 100);
+    this.climbing.addRockClimber(rockClimber1);
+    this.climbing.addRockClimber(rockClimber2);
+  }
 
 }
