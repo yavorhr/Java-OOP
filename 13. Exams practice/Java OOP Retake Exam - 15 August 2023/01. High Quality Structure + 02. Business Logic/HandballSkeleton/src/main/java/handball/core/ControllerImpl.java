81d63960c8ec528ebfd515d6ba7c964cacd0e678
wolfie <yavorhristozov@gmail.com>
2024-05-02 16:32:22 +0300

    add : ControllerImpl class

---
+++ 13. Exams practice/Java OOP Retake Exam - 15 August 2023/01. High Quality Structure + 02. Business Logic/HandballSkeleton/src/main/java/handball/core/ControllerImpl.java
@@ -0,0 +1,40 @@
+package handball.core;
+
+public class ControllerImpl implements Controller {
+
+
+  @Override
+  public String addGameplay(String gameplayType, String gameplayName) {
+    return null;
+  }
+
+  @Override
+  public String addEquipment(String equipmentType) {
+    return null;
+  }
+
+  @Override
+  public String equipmentRequirement(String gameplayName, String equipmentType) {
+    return null;
+  }
+
+  @Override
+  public String addTeam(String gameplayName, String teamType, String teamName, String country, int advantage) {
+    return null;
+  }
+
+  @Override
+  public String playInGameplay(String gameplayName) {
+    return null;
+  }
+
+  @Override
+  public String percentAdvantage(String gameplayName) {
+    return null;
+  }
+
+  @Override
+  public String getStatistics() {
+    return null;
+  }
+}
