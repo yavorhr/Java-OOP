12d322dca2ef68d69225c3feda5d3c419a2e3062
wolfie <yavorhristozov@gmail.com>
2024-05-02 15:18:25 +0300

    add : ElbowPad class

--- 13. Exams practice/Java OOP Retake Exam - 15 August 2023/01. High Quality Structure + 02. Business Logic/HandballSkeleton/src/main/java/handball/entities/equipment/Kneepad.java
+++ 13. Exams practice/Java OOP Retake Exam - 15 August 2023/01. High Quality Structure + 02. Business Logic/HandballSkeleton/src/main/java/handball/entities/equipment/Kneepad.java
@@ -1,8 +1,8 @@
 package handball.entities.equipment;
 
 public class Kneepad extends BaseEquipment {
-  private final int PROTECTION = 120;
-  private final double PRICE = 15;
+  private static final int PROTECTION = 120;
+  private static final double PRICE = 15;
 
   public Kneepad() {
     super(PROTECTION, PRICE);
