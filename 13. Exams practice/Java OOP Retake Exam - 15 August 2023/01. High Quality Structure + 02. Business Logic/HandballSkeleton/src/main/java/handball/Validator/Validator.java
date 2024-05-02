cf7741f404bfbce49ebd8ee2e812ebd39919bd5e
wolfie <yavorhristozov@gmail.com>
2024-05-02 16:27:07 +0300

    add : Validator class

---
+++ 13. Exams practice/Java OOP Retake Exam - 15 August 2023/01. High Quality Structure + 02. Business Logic/HandballSkeleton/src/main/java/handball/Validator/Validator.java
@@ -0,0 +1,23 @@
+package handball.Validator;
+
+import handball.common.ExceptionMessages;
+
+public class Validator {
+  public static void validateTeamName(String name) {
+    if (name == null || name.trim().isEmpty()) {
+      throw new NullPointerException(ExceptionMessages.TEAM_NAME_NULL_OR_EMPTY);
+    }
+  }
+
+  public static void validateCountryName(String name) {
+    if (name == null || name.trim().isEmpty()) {
+      throw new NullPointerException(ExceptionMessages.TEAM_COUNTRY_NULL_OR_EMPTY);
+    }
+  }
+
+  public static void validateAdvantage(int advantage) {
+    if (advantage < 0) {
+      throw new IllegalArgumentException(ExceptionMessages.TEAM_ADVANTAGE_BELOW_OR_EQUAL_ZERO);
+    }
+  }
+}
