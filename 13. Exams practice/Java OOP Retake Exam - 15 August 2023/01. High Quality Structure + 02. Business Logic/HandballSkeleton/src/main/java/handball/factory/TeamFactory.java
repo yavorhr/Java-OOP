ab1581e6c1a01e60a07b3225dde9af6fa354e3e7
wolfie <yavorhristozov@gmail.com>
2024-05-03 09:59:31 +0300

    add : TeamFactory class

---
+++ 13. Exams practice/Java OOP Retake Exam - 15 August 2023/01. High Quality Structure + 02. Business Logic/HandballSkeleton/src/main/java/handball/factory/TeamFactory.java
@@ -0,0 +1,20 @@
+package handball.factory;
+
+import handball.common.ExceptionMessages;
+import handball.entities.team.Bulgaria;
+import handball.entities.team.Germany;
+import handball.entities.team.Team;
+
+public class TeamFactory {
+  public static Team create(String type, String teamName, String country, int advantage) {
+    Team team;
+
+    switch (type) {
+      case "Germany" -> team = new Germany(teamName, country, advantage);
+      case "Bulgaria" -> team = new Bulgaria(teamName, country, advantage);
+      default -> throw new IllegalArgumentException(ExceptionMessages.TEAM_NAME_NULL_OR_EMPTY)
+    }
+
+    return team;
+  }
+}
