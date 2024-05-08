573af695b8d3bb9ded1246e7d2adc65dbbe0c512
wolfie <yavorhristozov@gmail.com>
2024-05-08 16:27:58 +0300

    add : setup()
    init objects @before each test

--- 13. Exams practice/Java OOP Retake Exam - 15 August 2023/03. Unit testing/Handball-Unit-Testing/src/test/java/handball/TeamTests.java
+++ 13. Exams practice/Java OOP Retake Exam - 15 August 2023/03. Unit testing/Handball-Unit-Testing/src/test/java/handball/TeamTests.java
@@ -1,4 +1,19 @@
 package handball;
 
+import org.junit.Before;
+
 public class TeamTests {
+  private Team team1;
+  private Team team2;
+  private HandballPlayer player1;
+  private HandballPlayer player2;
+
+  @Before
+  public void setup() {
+    this.team1 = new Team("Arsenal", 3);
+    this.team1.add(player1);
+    this.team1.add(player2);
+
+    this.team2 = new Team("Chelsea", 5);
+  }
 }
