0e5a83ec8c585d8a9e194d775719d3ee7bae5c9d
wolfie <yavorhristozov@gmail.com>
2024-05-09 15:36:19 +0300

    add : BaseAstronaut class
    abstract class

---
+++ 13. Exams practice/Java OOP Retake Exam - 18 April 2021/01. High Quality Structure + 02. Business Logic/SpaceStation/src/main/java/spaceStation/models/astronauts/BaseAstronaut.java
@@ -0,0 +1,39 @@
+package spaceStation.models.astronauts;
+
+import spaceStation.models.bags.Bag;
+
+public abstract class BaseAstronaut implements Astronaut {
+  private String name;
+  private double oxygen;
+  private Bag bag;
+
+  protected BaseAstronaut(String name, double oxygen) {
+    this.name = name;
+    this.oxygen = oxygen;
+  }
+
+  @Override
+  public String getName() {
+    return this.name;
+  }
+
+  @Override
+  public double getOxygen() {
+    return this.oxygen;
+  }
+
+  @Override
+  public boolean canBreath() {
+    return this.oxygen > 0;
+  }
+
+  @Override
+  public Bag getBag() {
+    return this.bag;
+  }
+
+  @Override
+  public void breath() {
+
+  }
+}
