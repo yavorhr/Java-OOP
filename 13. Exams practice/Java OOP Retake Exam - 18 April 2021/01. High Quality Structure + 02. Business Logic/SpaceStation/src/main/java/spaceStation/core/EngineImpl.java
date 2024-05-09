6932b17e66afd2fb6f29536f6998bc273f4449e1
wolfie <yavorhristozov@gmail.com>
2024-05-09 16:23:54 +0300

    add : process input commands
    EngineImpl

--- 13. Exams practice/Java OOP Retake Exam - 18 April 2021/01. High Quality Structure + 02. Business Logic/SpaceStation/src/main/java/spaceStation/core/EngineImpl.java
+++ 13. Exams practice/Java OOP Retake Exam - 18 April 2021/01. High Quality Structure + 02. Business Logic/SpaceStation/src/main/java/spaceStation/core/EngineImpl.java
@@ -8,81 +8,82 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class EngineImpl implements Engine {
-    private Controller controller;
-    private BufferedReader reader;
+  private Controller controller;
+  private BufferedReader reader;
 
-    public EngineImpl(Controller controller) {
-        this.controller = controller;
-        this.reader = new BufferedReader(new InputStreamReader(System.in));
-    }
+  public EngineImpl(Controller controller) {
+    this.controller = controller;
+    this.reader = new BufferedReader(new InputStreamReader(System.in));
+  }
 
-    @Override
-    public void run() {
-        while (true) {
-            String result = null;
-            try {
-                result = processInput();
+  @Override
+  public void run() {
+    while (true) {
+      String result = null;
+      try {
+        result = processInput();
 
-                if (result.equals(Command.Exit.name())) {
-                    break;
-                }
-            } catch (NullPointerException | IllegalArgumentException | IOException e) {
-                result = e.getMessage();
-            }
-
-            System.out.println(result);
+        if (result.equals(Command.Exit.name())) {
+          break;
         }
+      } catch (NullPointerException | IllegalArgumentException | IOException e) {
+        result = e.getMessage();
+      }
+
+      System.out.println(result);
+    }
+  }
+
+  private String processInput() throws IOException {
+    String input = this.reader.readLine();
+    String[] tokens = input.split("\\s+");
+
+    Command command = Command.valueOf(tokens[0]);
+    String result = null;
+    String[] data = Arrays.stream(tokens).skip(1).toArray(String[]::new);
+
+    switch (command) {
+      case AddAstronaut:
+        result = addAstronaut(data);
+        break;
+      case AddPlanet:
+        result = addPlanet(data);
+        break;
+      case RetireAstronaut:
+        result = retireAstronaut(data);
+        break;
+      case ExplorePlanet:
+        result = explorePlanet(data);
+        break;
+      case Report:
+        result = report();
+        break;
+      case Exit:
+        result = Command.Exit.name();
+        break;
     }
 
-    private String processInput() throws IOException {
-        String input = this.reader.readLine();
-        String[] tokens = input.split("\\s+");
+    return result;
+  }
 
-        Command command = Command.valueOf(tokens[0]);
-        String result = null;
-        String[] data = Arrays.stream(tokens).skip(1).toArray(String[]::new);
+  private String retireAstronaut(String[] data) {
+    return this.controller.retireAstronaut(data[0]);
+  }
 
-        switch (command) {
-            case AddAstronaut:
-                result = addAstronaut(data);
-                break;
-            case AddPlanet:
-                result = addPlanet(data);
-                break;
-            case RetireAstronaut:
-                result = retireAstronaut(data);
-                break;
-            case ExplorePlanet:
-                result = explorePlanet(data);
-                break;
-            case Report:
-                result = report();
-                break;
-            case Exit:
-                result = Command.Exit.name();
-                break;
-        }
+  private String report() {
+    return this.controller.report();
+  }
 
-        return result;
-    }
+  private String explorePlanet(String[] data) {
+    return this.controller.explorePlanet(data[0]);
+  }
 
-    private String retireAstronaut(String[] data) {
-        return null;
-    }
+  private String addPlanet(String[] data) {
+    return this.controller.addPlanet(data[0],
+            Arrays.stream(data).skip(1).toArray(String[]::new));
+  }
 
-    private String report() {
-        return null;
-    }
-
-    private String explorePlanet(String[] data) {
-        return null;
-    }
-
-    private String addPlanet(String[] data) {
-        return null;
-    }
-
-    private String addAstronaut(String[] data) {
-        return null;
-    }
+  private String addAstronaut(String[] data) {
+    return this.controller.addAstronaut(data[0], data[1]);
+  }
 }
