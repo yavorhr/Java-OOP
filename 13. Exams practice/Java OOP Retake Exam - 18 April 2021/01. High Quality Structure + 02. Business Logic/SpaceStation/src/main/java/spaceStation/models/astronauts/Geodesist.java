8495b930d5fb7a0b10d34efb118345ab90efefe0
wolfie <yavorhristozov@gmail.com>
2024-05-09 15:46:54 +0300

    add : Geodesist class

---
+++ 13. Exams practice/Java OOP Retake Exam - 18 April 2021/01. High Quality Structure + 02. Business Logic/SpaceStation/src/main/java/spaceStation/models/astronauts/Geodesist.java
@@ -0,0 +1,15 @@
+package spaceStation.models.astronauts;
+
+public class Geodesist extends BaseAstronaut {
+  private static final double INITIAL_OXYGEN = 50;
+  private static final double DECREASE_VALUE_ON_BREATHE = 10;
+
+  protected Geodesist(String name) {
+    super(name, INITIAL_OXYGEN);
+  }
+
+  @Override
+  public void breath() {
+    super.decreaseOxygenWhenBreathe(DECREASE_VALUE_ON_BREATHE);
+  }
+}
