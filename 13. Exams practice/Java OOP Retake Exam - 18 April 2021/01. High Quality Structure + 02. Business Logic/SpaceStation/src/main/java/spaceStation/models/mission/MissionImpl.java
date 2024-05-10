d9e0c06705e32b5a8e8c1126800bb0b72c93ffdc
wolfie <yavorhristozov@gmail.com>
2024-05-10 11:26:48 +0300

    add : explore() impl In MissionImpl

--- 13. Exams practice/Java OOP Retake Exam - 18 April 2021/01. High Quality Structure + 02. Business Logic/SpaceStation/src/main/java/spaceStation/models/mission/MissionImpl.java
+++ 13. Exams practice/Java OOP Retake Exam - 18 April 2021/01. High Quality Structure + 02. Business Logic/SpaceStation/src/main/java/spaceStation/models/mission/MissionImpl.java
@@ -5,9 +5,31 @@ import spaceStation.models.planets.Planet;
 
 import java.util.Collection;
 
-public class MissionImpl implements Mission{
+public class MissionImpl implements Mission {
+
   @Override
   public void explore(Planet planet, Collection<Astronaut> astronauts) {
 
+    while (planet.getItems().size() > 0 && astronautsLeft(astronauts)) {
+      for (Astronaut astronaut : astronauts) {
+        while (astronaut.canBreath() && !"".equals(getItem(planet))) {
+          String item = getItem(planet);
+
+          astronaut.getBag().getItems().add(item);
+          planet.getItems().remove(item);
+          astronaut.breath();
+        }
+      }
+    }
+  }
+
+
+  // Helpers
+  private boolean astronautsLeft(Collection<Astronaut> astronauts) {
+    return astronauts.stream().anyMatch(Astronaut::canBreath);
+  }
+
+  private String getItem(Planet planet) {
+    return planet.getItems().stream().findFirst().orElse("");
   }
 }
