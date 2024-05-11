9f1bf15883ff64faeffd2c770395ffbbd770d602
wolfie <yavorhristozov@gmail.com>
2024-05-11 14:52:14 +0300

    test : setup
    @before

--- 13. Exams practice/Java OOP Retake Exam - 18 April 2021/03. Unit testing/HeroRepository/src/test/java/heroRepository/HeroRepositoryTests.java
+++ 13. Exams practice/Java OOP Retake Exam - 18 April 2021/03. Unit testing/HeroRepository/src/test/java/heroRepository/HeroRepositoryTests.java
@@ -1,8 +1,21 @@
 package heroRepository;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class HeroRepositoryTests {
-    //TODO: TEST ALL THE FUNCTIONALITY OF THE PROVIDED CLASS HeroRepository
+  private HeroRepository heroRepository;
+  private HeroRepository emptyRepository;
+  private Hero hero1;
+  private Hero hero2;
+
+  @Before
+  public void setup() {
+    this.emptyRepository = new HeroRepository();
+    this.heroRepository = new HeroRepository();
+    this.heroRepository.create(hero1);
+    this.heroRepository.create(hero2);
+  }
+
 }
