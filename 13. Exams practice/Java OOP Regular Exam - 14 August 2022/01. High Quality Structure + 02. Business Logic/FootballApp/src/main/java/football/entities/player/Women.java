bd80ebad4cd2961ed4b091f268b5c2b4115ce4a4
wolfie <yavorhristozov@gmail.com>
2024-05-16 14:23:46 +0300

    add : Men class

--- 13. Exams practice/Java OOP Regular Exam - 14 August 2022/01. High Quality Structure + 02. Business Logic/FootballApp/src/main/java/football/entities/player/Women.java
+++ 13. Exams practice/Java OOP Regular Exam - 14 August 2022/01. High Quality Structure + 02. Business Logic/FootballApp/src/main/java/football/entities/player/Women.java
@@ -4,7 +4,7 @@ public class Women extends BasePlayer {
   private static final double INITIAL_KG = 60;
   private static final int STIMULATE_STRENGTH_VALUE = 115;
 
-  protected Women(String name, String nationality, int strength) {
+  public Women(String name, String nationality, int strength) {
     super(name, nationality, INITIAL_KG, strength);
   }
 
