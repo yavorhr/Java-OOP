8139408e7bdc11c1f30a8c57cf90ec907d8858d7
wolfie <yavorhristozov@gmail.com>
2024-05-16 15:12:20 +0300

    add : BasePlayer validations

--- 13. Exams practice/Java OOP Regular Exam - 14 August 2022/01. High Quality Structure + 02. Business Logic/FootballApp/src/main/java/football/Validator/Validator.java
+++ 13. Exams practice/Java OOP Regular Exam - 14 August 2022/01. High Quality Structure + 02. Business Logic/FootballApp/src/main/java/football/Validator/Validator.java
@@ -1,12 +1,31 @@
 package football.Validator;
 
 import football.common.ConstantMessages;
+import football.common.ExceptionMessages;
 
 public class Validator {
 
   public static void validateIfCapacity(int capacity) {
     if (capacity == 0) {
       throw new IllegalArgumentException(ConstantMessages.NOT_ENOUGH_CAPACITY);
+    }
+  }
+
+  public static void validatePlayerName(String name) {
+    if (name == null || name.trim().isEmpty()) {
+      throw new IllegalArgumentException(ExceptionMessages.PLAYER_NAME_NULL_OR_EMPTY);
+    }
+  }
+
+  public static void validateNationality(String nationality) {
+    if (nationality == null || nationality.trim().isEmpty()) {
+      throw new IllegalArgumentException(ExceptionMessages.PLAYER_NATIONALITY_NULL_OR_EMPTY);
+    }
+  }
+
+  public static void validateStrength(int strength) {
+    if (strength <= 0) {
+      throw new IllegalArgumentException(ExceptionMessages.PLAYER_STRENGTH_BELOW_OR_EQUAL_ZERO);
     }
   }
 
