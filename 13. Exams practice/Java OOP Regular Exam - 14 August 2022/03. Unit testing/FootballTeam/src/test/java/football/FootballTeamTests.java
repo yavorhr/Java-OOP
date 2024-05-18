ef218cd8ccd1ad8a77bb69477e4890dce588a66b
wolfie <yavorhristozov@gmail.com>
2024-05-18 16:00:45 +0300

    add : setUp

--- 13. Exams practice/Java OOP Regular Exam - 14 August 2022/03. Unit testing/FootballTeam/src/test/java/football/FootballTeamTests.java
+++ 13. Exams practice/Java OOP Regular Exam - 14 August 2022/03. Unit testing/FootballTeam/src/test/java/football/FootballTeamTests.java
@@ -1,5 +1,21 @@
 package football;
 
+import org.junit.Before;
+
 public class FootballTeamTests {
+  private FootballTeam footballTeam;
+  private FootballTeam emptyFootballTeam;
+  private Footballer player1;
+  private Footballer player2;
+
+  @Before
+  public void setUp(){
+    this.emptyFootballTeam = new FootballTeam("test",10);
+    this.footballTeam = new FootballTeam("CSKA",2);
+    this.player1 = new Footballer("Mike");
+    this.player2 = new Footballer("George");
+    this.footballTeam.addFootballer(player1);
+    this.footballTeam.addFootballer(player2);
+  }
 
 }
