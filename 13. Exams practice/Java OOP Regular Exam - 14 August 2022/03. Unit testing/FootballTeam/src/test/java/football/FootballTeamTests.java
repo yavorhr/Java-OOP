067c152185b660fa2eff586218aa16849a0043e1
wolfie <yavorhristozov@gmail.com>
2024-05-18 16:21:11 +0300

    test : football team ctor
    testFootballTeamCtorShouldWorksCorrect

--- 13. Exams practice/Java OOP Regular Exam - 14 August 2022/03. Unit testing/FootballTeam/src/test/java/football/FootballTeamTests.java
+++ 13. Exams practice/Java OOP Regular Exam - 14 August 2022/03. Unit testing/FootballTeam/src/test/java/football/FootballTeamTests.java
@@ -1,6 +1,8 @@
 package football;
 
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
 public class FootballTeamTests {
   private FootballTeam footballTeam;
@@ -9,13 +11,21 @@ public class FootballTeamTests {
   private Footballer player2;
 
   @Before
-  public void setUp(){
-    this.emptyFootballTeam = new FootballTeam("test",10);
-    this.footballTeam = new FootballTeam("CSKA",2);
+  public void setUp() {
+    this.emptyFootballTeam = new FootballTeam("test", 10);
+    this.footballTeam = new FootballTeam("CSKA", 2);
     this.player1 = new Footballer("Mike");
     this.player2 = new Footballer("George");
     this.footballTeam.addFootballer(player1);
     this.footballTeam.addFootballer(player2);
+  }
+
+  // test constructor
+  @Test
+  public void testFootballTeamCtorShouldWorksCorrect() {
+    Assert.assertEquals("test", emptyFootballTeam.getName());
+    Assert.assertEquals(0, emptyFootballTeam.getCount());
+    Assert.assertEquals(10, emptyFootballTeam.getVacantPositions());
   }
 
 }
