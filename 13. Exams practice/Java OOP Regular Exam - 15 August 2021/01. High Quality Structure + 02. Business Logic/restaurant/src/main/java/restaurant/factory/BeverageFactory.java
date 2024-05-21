863c72f8ef74e1a1f378c68794cada482d7a8bee
wolfie <yavorhristozov@gmail.com>
2024-05-21 15:21:04 +0300

    add : BeverageFactory class

---
+++ 13. Exams practice/Java OOP Regular Exam - 15 August 2021/01. High Quality Structure + 02. Business Logic/restaurant/src/main/java/restaurant/factory/BeverageFactory.java
@@ -0,0 +1,19 @@
+package restaurant.factory;
+
+import restaurant.entities.drinks.Fresh;
+import restaurant.entities.drinks.Smoothie;
+import restaurant.entities.drinks.interfaces.Beverages;
+
+public class BeverageFactory {
+
+  public static Beverages create(String type, int counter, String brand, String name) {
+    Beverages beverages = null;
+
+    switch (type) {
+      case "Smoothie" -> beverages = new Smoothie(name, counter, brand);
+      case "Fresh" -> beverages = new Fresh(name, counter, brand);
+      default -> throw new IllegalArgumentException("Invalid beverage type!")
+    }
+    return beverages;
+  }
+}
