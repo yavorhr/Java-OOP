dcf38103e34bd77f556169f36d3537435410c1dd
wolfie <yavorhristozov@gmail.com>
2024-05-21 15:09:19 +0300

    add : Main init
    init Controller, Repositories and Engine

--- 13. Exams practice/Java OOP Regular Exam - 15 August 2021/01. High Quality Structure + 02. Business Logic/restaurant/src/main/java/restaurant/Main.java
+++ 13. Exams practice/Java OOP Regular Exam - 15 August 2021/01. High Quality Structure + 02. Business Logic/restaurant/src/main/java/restaurant/Main.java
@@ -1,27 +1,32 @@
 package restaurant;
 
+import restaurant.core.ControllerImpl;
+import restaurant.core.EngineImpl;
 import restaurant.core.interfaces.Controller;
 import restaurant.entities.drinks.interfaces.Beverages;
 import restaurant.entities.healthyFoods.interfaces.HealthyFood;
 import restaurant.entities.tables.interfaces.Table;
 
+import restaurant.io.ConsoleReader;
+import restaurant.io.ConsoleWriter;
+import restaurant.repositories.BeveragesRepositoryImpl;
+import restaurant.repositories.HealthFoodRepositoryImpl;
+import restaurant.repositories.TableRepositoryImpl;
 import restaurant.repositories.interfaces.*;
 
 public class Main {
     public static void main(String[] args) {
 
+        HealthFoodRepository<HealthyFood> healthFoodRepository = new HealthFoodRepositoryImpl();
+        BeverageRepository<Beverages> beverageRepository = new BeveragesRepositoryImpl();
+        TableRepository<Table> tableRepository = new TableRepositoryImpl();
 
-        HealthFoodRepository<HealthyFood> healthFoodRepository;
-        BeverageRepository<Beverages> beverageRepository;
-        TableRepository<Table> tableRepository;
-
-        /*
         Controller controller = new ControllerImpl(healthFoodRepository, beverageRepository, tableRepository);
 
         ConsoleReader reader = new ConsoleReader();
         ConsoleWriter writer = new ConsoleWriter();
         EngineImpl engine = new EngineImpl(reader, writer, controller);
+
         engine.run();
-        */
     }
 }
