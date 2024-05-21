69c6ecc9f057742e743505e67645acb679baa5e3
wolfie <yavorhristozov@gmail.com>
2024-05-21 15:11:52 +0300

    refact : init repositories in Controller class

--- 13. Exams practice/Java OOP Regular Exam - 15 August 2021/01. High Quality Structure + 02. Business Logic/restaurant/src/main/java/restaurant/core/ControllerImpl.java
+++ 13. Exams practice/Java OOP Regular Exam - 15 August 2021/01. High Quality Structure + 02. Business Logic/restaurant/src/main/java/restaurant/core/ControllerImpl.java
@@ -7,58 +7,62 @@ import restaurant.entities.tables.interfaces.Table;
 import restaurant.repositories.interfaces.*;
 
 public class ControllerImpl implements Controller {
+  private HealthFoodRepository<HealthyFood> healthFoodRepository;
+  private BeverageRepository<Beverages> beverageRepository;
+  private BeverageRepository<Table> tableBeverageRepository;
+
+  public ControllerImpl(HealthFoodRepository<HealthyFood> healthFoodRepository, BeverageRepository<Beverages> beverageRepository, BeverageRepository<Table> tableRepository) {
+    this.healthFoodRepository = healthFoodRepository;
+    this.beverageRepository = beverageRepository;
+    this.tableBeverageRepository = tableRepository;
+  }
+
+  @Override
+  public String addHealthyFood(String type, double price, String name) {
+    //TODO:
+    return null;
+  }
+
+  @Override
+  public String addBeverage(String type, int counter, String brand, String name) {
+    //TODO:
+    return null;
+  }
+
+  @Override
+  public String addTable(String type, int tableNumber, int capacity) {
+    //TODO:
+    return null;
+  }
+
+  @Override
+  public String reserve(int numberOfPeople) {
+    //TODO:
+    return null;
+  }
+
+  @Override
+  public String orderHealthyFood(int tableNumber, String healthyFoodName) {
+    //TODO:
+    return null;
+  }
+
+  @Override
+  public String orderBeverage(int tableNumber, String name, String brand) {
+    //TODO:
+    return null;
+  }
+
+  @Override
+  public String closedBill(int tableNumber) {
+    //TODO:
+    return null;
+  }
 
 
-    public ControllerImpl(HealthFoodRepository<HealthyFood> healthFoodRepository, BeverageRepository<Beverages> beverageRepository, TableRepository<Table> tableRepository) {
-
-    }
-
-    @Override
-    public String addHealthyFood(String type, double price, String name) {
-        //TODO:
-        return null;
-    }
-
-    @Override
-    public String addBeverage(String type, int counter, String brand, String name){
-        //TODO:
-        return null;
-    }
-
-    @Override
-    public String addTable(String type, int tableNumber, int capacity) {
-        //TODO:
-        return null;
-    }
-
-    @Override
-    public String reserve(int numberOfPeople) {
-        //TODO:
-        return null;
-    }
-
-    @Override
-    public String orderHealthyFood(int tableNumber, String healthyFoodName) {
-        //TODO:
-        return null;
-    }
-
-    @Override
-    public String orderBeverage(int tableNumber, String name, String brand) {
-        //TODO:
-        return null;
-    }
-
-    @Override
-    public String closedBill(int tableNumber) {
-        //TODO:
-        return null;
-    }
-
-
-    @Override
-    public String totalMoney() {
-        //TODO:
-        return null;
-    }
+  @Override
+  public String totalMoney() {
+    //TODO:
+    return null;
+  }
 }
