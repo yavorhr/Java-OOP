59304e317a7e865d85fe39bcf18373112db22778
wolfie <yavorhristozov@gmail.com>
2024-05-23 09:37:30 +0300

    test : add setup()
    init objects @Before each test

--- 13. Exams practice/Java OOP Regular Exam - 15 August 2021/03. Unit testing/03. Unit Testing_Unit-Testing/src/test/java/shopAndGoods/ShopTest.java
+++ 13. Exams practice/Java OOP Regular Exam - 15 August 2021/03. Unit testing/03. Unit Testing_Unit-Testing/src/test/java/shopAndGoods/ShopTest.java
@@ -1,6 +1,26 @@
 package shopAndGoods;
 
 
+import org.junit.Before;
+
 public class ShopTest {
-    // TODO
+  private Shop shop;
+  private Shop emptyShop;
+  private Goods tomato;
+  private Goods banana;
+  private Goods apple;
+
+  @Before
+  public void setUp() {
+    this.emptyShop = new Shop();
+    this.shop = new Shop();
+
+    this.tomato = new Goods("tomato", "10");
+    this.banana = new Goods("banana", "20");
+    this.apple = new Goods("apple", "30");
+
+    this.shop.addGoods("Shelves1", tomato);
+    this.shop.addGoods("Shelves1", banana);
+    this.shop.addGoods("Shelves1", apple);
+  }
 }
