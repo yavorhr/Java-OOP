af19c678ae4eda424d3d203e9da7fffe7b86e3ff
wolfie <yavorhristozov@gmail.com>
2024-05-25 11:08:44 +0300

    Update PetStoreTests.java

--- 13. Exams practice/Java OOP Retake Exam - 18 April 2022/03. Unit testing/PetStore/src/test/java/petStore/PetStoreTests.java
+++ 13. Exams practice/Java OOP Retake Exam - 18 April 2022/03. Unit testing/PetStore/src/test/java/petStore/PetStoreTests.java
@@ -1,6 +1,23 @@
 package petStore;
 
+import org.junit.Before;
+
 public class PetStoreTests {
+  private PetStore store;
+  private PetStore emptyStore;
+  private Animal cat;
+  private Animal dog;
+
+  @Before
+  public void setup(){
+    this.emptyStore = new PetStore();
+
+    this.store = new PetStore();
+    this.cat = new Animal("cat", 8, 50);
+    this.dog = new Animal("dog", 15, 100);
+    this.store.addAnimal(cat);
+    this.store.addAnimal(dog);
+  }
 
 }
 
