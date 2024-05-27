b22b8dced90a51f718ccf237c98df72981f0de54
wolfie <yavorhristozov@gmail.com>
2024-05-27 17:02:44 +0300

    add : validation methods in Validator class
    validating Animal input

--- 13. Exams practice/Java OOP Retake Exam - 18 April 2022/01. High Quality Structure + 02. Business Logic/ZooApp/src/main/java/validator/Validator.java
+++ 13. Exams practice/Java OOP Retake Exam - 18 April 2022/01. High Quality Structure + 02. Business Logic/ZooApp/src/main/java/validator/Validator.java
@@ -9,4 +9,23 @@ public class Validator {
       throw new IllegalArgumentException(ExceptionMessages.NOT_ENOUGH_CAPACITY);
     }
   }
+
+  public static void validateAnimalName(String name) {
+    if (name == null || name.trim().isEmpty()) {
+      throw new IllegalArgumentException(ExceptionMessages.ANIMAL_NAME_NULL_OR_EMPTY);
+    }
+  }
+
+  public static void validateAnimalKind(String kind) {
+    if (kind == null || kind.trim().isEmpty()) {
+      throw new IllegalArgumentException(ExceptionMessages.ANIMAL_KIND_NULL_OR_EMPTY);
+    }
+  }
+
+  public static void validateAnimalPrice(int price) {
+    if (price <= 0) {
+      throw new IllegalArgumentException(ExceptionMessages.ANIMAL_PRICE_BELOW_OR_EQUAL_ZERO);
+    }
+  }
+
 }
