698794a59431b296c9c0259506e5e9b7f08414da
wolfie <yavorhristozov@gmail.com>
2024-05-27 15:43:35 +0300

    add : BaseFood
    abstract class

---
+++ 13. Exams practice/Java OOP Retake Exam - 18 April 2022/01. High Quality Structure + 02. Business Logic/ZooApp/src/main/java/zoo/entities/foods/BaseFood.java
@@ -0,0 +1,21 @@
+package zoo.entities.foods;
+
+public abstract class BaseFood implements Food {
+  private double price;
+  private int calories;
+
+  protected BaseFood(double price, int calories) {
+    this.price = price;
+    this.calories = calories;
+  }
+
+  @Override
+  public int getCalories() {
+    return this.calories;
+  }
+
+  @Override
+  public double getPrice() {
+    return this.price;
+  }
+}
