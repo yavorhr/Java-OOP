6a58171ca1ae921a90a638c91186c32a65bb7b2a
wolfie <yavorhristozov@gmail.com>
2024-05-29 12:39:39 +0300

    add :  read commands in EngineImpl

--- 13. Exams practice/Java OOP Retake Exam - 18 April 2022/01. High Quality Structure + 02. Business Logic/ZooApp/src/main/java/zoo/core/EngineImpl.java
+++ 13. Exams practice/Java OOP Retake Exam - 18 April 2022/01. High Quality Structure + 02. Business Logic/ZooApp/src/main/java/zoo/core/EngineImpl.java
@@ -8,83 +8,80 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class EngineImpl implements Engine {
-    private Controller controller;
-    private BufferedReader reader;
+  private Controller controller;
+  private BufferedReader reader;
 
-    public EngineImpl() {
-        this.controller = new ControllerImpl();
-        this.reader = new BufferedReader(new InputStreamReader(System.in));
-    }
+  public EngineImpl() {
+    this.controller = new ControllerImpl();
+    this.reader = new BufferedReader(new InputStreamReader(System.in));
+  }
 
-    @Override
-    public void run() {
-        while (true) {
-            String result = null;
-            try {
-                result = processInput();
+  @Override
+  public void run() {
+    while (true) {
+      String result = null;
+      try {
+        result = processInput();
 
-                if (result.equals("Exit")) {
-                    break;
-                }
-            } catch (NullPointerException | IllegalArgumentException | IllegalStateException | IOException e) {
-                result = e.getMessage();
-            }
-
-            System.out.println(result);
+        if (result.equals("Exit")) {
+          break;
         }
-    }
-    private String processInput() throws IOException {
-        String input = this.reader.readLine();
-        String[] tokens = input.split("\\s+");
+      } catch (NullPointerException | IllegalArgumentException | IllegalStateException | IOException e) {
+        result = e.getMessage();
+      }
 
-        Command command = Command.valueOf(tokens[0]);
-        String result = null;
-        String[] data = Arrays.stream(tokens).skip(1).toArray(String[]::new);
+      System.out.println(result);
+    }
+  }
 
-        result = switch (command) {
-            case AddArea -> addArea(data);
-            case BuyFood -> buyFood(data);
-            case FoodForArea -> foodForArea(data);
-            case AddAnimal -> addAnimal(data);
-            case FeedAnimal -> feedAnimal(data);
-            case CalculateKg -> calculateKg(data);
-            case GetStatistics -> getStatistics();
-            case Exit -> Command.Exit.name();
-        };
-        return result;
-    }
-    private String addArea(String[] data) {
-        //TODO
-        return null;
-    }
+  private String processInput() throws IOException {
+    String input = this.reader.readLine();
+    String[] tokens = input.split("\\s+");
 
-    private String buyFood(String[] data) {
-        //TODO
-        return null;
-    }
+    Command command = Command.valueOf(tokens[0]);
+    String result = null;
+    String[] data = Arrays.stream(tokens).skip(1).toArray(String[]::new);
 
-    private String foodForArea(String[] data) {
-        //TODO
-        return null;
-    }
+    result = switch (command) {
+      case AddArea -> addArea(data);
+      case BuyFood -> buyFood(data);
+      case FoodForArea -> foodForArea(data);
+      case AddAnimal -> addAnimal(data);
+      case FeedAnimal -> feedAnimal(data);
+      case CalculateKg -> calculateKg(data);
+      case GetStatistics -> getStatistics();
+      case Exit -> Command.Exit.name();
+    };
+    return result;
+  }
 
-    private String addAnimal(String[] data) {
-        //TODO
-        return null;
-    }
+  private String addArea(String[] data) {
+    return this.controller.addArea(data[0], data[1]);
+  }
 
-    private String feedAnimal(String[] data) {
-        //TODO
-        return null;
-    }
+  private String buyFood(String[] data) {
+    return this.controller.buyFood(data[0]);
+  }
 
-    private String calculateKg(String[] data) {
-        //TODO
-        return null;
-    }
+  private String foodForArea(String[] data) {
+    return this.controller.foodForArea(data[0], data[1]);
+  }
 
-    private String getStatistics() {
-        //TODO
-        return null;
-    }
+  private String addAnimal(String[] data) {
+    return this.controller.addAnimal(data[0], data[1],
+            data[2], data[3], Double.parseDouble(data[4]));
+  }
+
+  private String feedAnimal(String[] data) {
+    return this.controller.feedAnimal(data[0]);
+  }
+
+  private String calculateKg(String[] data) {
+    return this.controller.calculateKg(data[0]);
+
+  }
+
+  private String getStatistics() {
+    return this.controller.getStatistics();
+  }
 }
