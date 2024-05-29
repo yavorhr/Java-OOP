52160cd026cafa2fd177cb737166d5a63b8683e2
wolfie <yavorhristozov@gmail.com>
2024-05-29 12:46:32 +0300

    add : addArea() impl
    ControllerImpl class

--- 13. Exams practice/Java OOP Retake Exam - 18 April 2022/01. High Quality Structure + 02. Business Logic/ZooApp/src/main/java/zoo/core/ControllerImpl.java
+++ 13. Exams practice/Java OOP Retake Exam - 18 April 2022/01. High Quality Structure + 02. Business Logic/ZooApp/src/main/java/zoo/core/ControllerImpl.java
@@ -1,39 +1,55 @@
 package zoo.core;
 
+import factory.AreaFactory;
+import zoo.common.ConstantMessages;
+import zoo.entities.areas.Area;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class ControllerImpl implements Controller {
+  private Map<String, Area> areas;
 
-    @Override
-    public String addArea(String areaType, String areaName) {
-        return null;
-    }
+  public ControllerImpl() {
+    this.areas = new HashMap<>();
+  }
 
-    @Override
-    public String buyFood(String foodType) {
-        return null;
-    }
+  @Override
+  public String addArea(String areaType, String areaName) {
+    Area area = AreaFactory.create(areaType, areaName);
+    this.areas.put(areaName, area);
 
-    @Override
-    public String foodForArea(String areaName, String foodType) {
-        return null;
-    }
+    return String.format(ConstantMessages.SUCCESSFULLY_ADDED_AREA_TYPE, areaType);
+  }
 
-    @Override
-    public String addAnimal(String areaName, String animalType, String animalName, String kind, double price) {
-        return null;
-    }
+  @Override
+  public String buyFood(String foodType) {
+    return null;
+  }
 
-    @Override
-    public String feedAnimal(String areaName) {
-        return null;
-    }
+  @Override
+  public String foodForArea(String areaName, String foodType) {
+    return null;
+  }
 
-    @Override
-    public String calculateKg(String areaName) {
-        return null;
-    }
+  @Override
+  public String addAnimal(String areaName, String animalType, String animalName, String kind, double price) {
+    return null;
+  }
 
-    @Override
-    public String getStatistics() {
-        return null;
-    }
+  @Override
+  public String feedAnimal(String areaName) {
+    return null;
+  }
+
+  @Override
+  public String calculateKg(String areaName) {
+    return null;
+  }
+
+  @Override
+  public String getStatistics() {
+    return null;
+  }
 }
