e667ae6bc637a2b8e8cc660523eeaff6422f82af
wolfie <yavorhristozov@gmail.com>
2024-05-31 18:35:52 +0300

    add : ShopImpl class

---
+++ 13. Exams practice/Java OOP Retake Exam - 18 April 2023/01. High Quality Structure + 02. Business Logic/Vehicle-Shop-App/src/main/java/vehicleShop/models/shop/ShopImpl.java
@@ -0,0 +1,13 @@
+package vehicleShop.models.shop;
+
+import vehicleShop.models.vehicle.Vehicle;
+import vehicleShop.models.worker.Worker;
+
+public class ShopImpl implements Shop {
+
+  @Override
+  public void make(Vehicle vehicle, Worker worker) {
+
+
+  }
+}
