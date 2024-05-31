d49a1160fe411772f4b2d2f8fed841891a550e62
wolfie <yavorhristozov@gmail.com>
2024-05-31 18:31:00 +0300

    add : SecondShift class

---
+++ 13. Exams practice/Java OOP Retake Exam - 18 April 2023/01. High Quality Structure + 02. Business Logic/Vehicle-Shop-App/src/main/java/vehicleShop/models/worker/SecondShift.java
@@ -0,0 +1,9 @@
+package vehicleShop.models.worker;
+
+public class SecondShift extends BaseWorker {
+  private static final int INIT_STRENGTH = 70;
+
+  public SecondShift(String name) {
+    super(name, INIT_STRENGTH);
+  }
+}
