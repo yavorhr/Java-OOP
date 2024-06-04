411d3bb1cf5ceb9d5c5b3bfbb28da7846fa119d3
wolfie <yavorhristozov@gmail.com>
2024-06-04 17:02:17 +0300

    add : ControllerImpl class

---
+++ 13. Exams practice/Java OOP Retake Exam - 18 April 2023/01. High Quality Structure + 02. Business Logic/Vehicle-Shop-App/src/main/java/vehicleShop/core/ControllerImpl.java
@@ -0,0 +1,28 @@
+package vehicleShop.core;
+
+public class ControllerImpl implements Controller{
+  @Override
+  public String addWorker(String type, String workerName) {
+    return null;
+  }
+
+  @Override
+  public String addVehicle(String vehicleName, int strengthRequired) {
+    return null;
+  }
+
+  @Override
+  public String addToolToWorker(String workerName, int power) {
+    return null;
+  }
+
+  @Override
+  public String makingVehicle(String vehicleName) {
+    return null;
+  }
+
+  @Override
+  public String statistics() {
+    return null;
+  }
+}
