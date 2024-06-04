40084eade8d55fdf695d640f45f27af6c7868c60
wolfie <yavorhristozov@gmail.com>
2024-06-04 17:18:14 +0300

    add : BaseWorrker validations
    name cant be null or empty
    strength cant be below 0

--- 13. Exams practice/Java OOP Retake Exam - 18 April 2023/01. High Quality Structure + 02. Business Logic/Vehicle-Shop-App/src/main/java/vehicleShop/validator/Validator.java
+++ 13. Exams practice/Java OOP Retake Exam - 18 April 2023/01. High Quality Structure + 02. Business Logic/Vehicle-Shop-App/src/main/java/vehicleShop/validator/Validator.java
@@ -3,13 +3,13 @@ package vehicleShop.validator;
 import vehicleShop.common.ExceptionMessages;
 
 public class Validator {
-  public void validateWorkersName(String name) {
+  public static void validateWorkersName(String name) {
     if (name == null || name.trim().isEmpty()) {
       throw new IllegalArgumentException(ExceptionMessages.WORKER_NAME_NULL_OR_EMPTY);
     }
   }
 
-  public void validateStrength(int strength) {
+  public static void validateStrength(int strength) {
     if (strength < 0) {
       throw new IllegalArgumentException(ExceptionMessages.WORKER_STRENGTH_LESS_THAN_ZERO);
     }
