f01ea8f6238ff213932cf17dcfe44b3b868b8f5f
wolfie <yavorhristozov@gmail.com>
2024-06-13 19:36:57 +0300

    add : processInput commands
    EngineImpl

--- 13. Exams practice/Java OOP Retake Exam - 19 December 2022/01. High Quality Structure + 02. Business Logic/MagicGameApp/src/main/java/magicGame/core/EngineImpl.java
+++ 13. Exams practice/Java OOP Retake Exam - 19 December 2022/01. High Quality Structure + 02. Business Logic/MagicGameApp/src/main/java/magicGame/core/EngineImpl.java
@@ -8,77 +8,82 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class EngineImpl implements Engine {
-    private Controller controller;
-    private BufferedReader reader;
+  private Controller controller;
+  private BufferedReader reader;
 
-    public EngineImpl() {
-        this.controller = new ControllerImpl(); 
-        this.reader = new BufferedReader(new InputStreamReader(System.in));
-    }
+  public EngineImpl() {
+    this.controller = new ControllerImpl();
+    this.reader = new BufferedReader(new InputStreamReader(System.in));
+  }
 
-    @Override
-    public void run() {
-        while (true) {
-            String result = null;
-            try {
-                result = processInput();
+  @Override
+  public void run() {
+    while (true) {
+      String result = null;
+      try {
+        result = processInput();
 
-                if (result.equals(Command.Exit.name())) {
-                    break;
-                }
-
-            } catch (IOException | IllegalArgumentException | NullPointerException e) {
-                result = e.getMessage();
-            }
-
-            System.out.println(result);
-        }
-    }
-
-    private String processInput() throws IOException {
-        String input = this.reader.readLine();
-        String[] tokens = input.split("\\s");
-
-        Command command = Command.valueOf(tokens[0]);
-        String[] data = Arrays.stream(tokens).skip(1).toArray(String[]::new);
-
-        String result = null;
-
-        switch (command) {
-            case AddMagician:
-                result = this.addMagician(data);
-                break;
-            case AddMagic:
-                result = this.addMagic(data);
-                break;
-            case StartGame:
-                result = this.start();
-                break;
-            case Report:
-                result = this.report();
-                break;
-            case Exit:
-                result = Command.Exit.name();
-                break;
+        if (result.equals(Command.Exit.name())) {
+          break;
         }
 
-        return result;
-    }
+      } catch (IOException | IllegalArgumentException | NullPointerException e) {
+        result = e.getMessage();
+      }
 
-    private String addMagician(String[] data) {
-        //TODO
-        return null;
+      System.out.println(result);
     }
-    private String addMagic(String[] data) {
-        //TODO
-        return null;
+  }
+
+  private String processInput() throws IOException {
+    String input = this.reader.readLine();
+    String[] tokens = input.split("\\s");
+
+    Command command = Command.valueOf(tokens[0]);
+    String[] data = Arrays.stream(tokens).skip(1).toArray(String[]::new);
+
+    String result = null;
+
+    switch (command) {
+      case AddMagician:
+        result = this.addMagician(data);
+        break;
+      case AddMagic:
+        result = this.addMagic(data);
+        break;
+      case StartGame:
+        result = this.start();
+        break;
+      case Report:
+        result = this.report();
+        break;
+      case Exit:
+        result = Command.Exit.name();
+        break;
     }
-    private String start() {
-        //TODO
-        return null;
-    }
-    private String report() {
-        //TODO
-        return null;
-    }
+    return result;
+  }
+
+  private String addMagician(String[] data) {
+    return this.controller.addMagician(data[0],
+            data[1],
+            Integer.parseInt(data[2]),
+            Integer.parseInt(data[3]),
+            data[4]);
+  }
+
+  private String addMagic(String[] data) {
+    return this.controller.addMagic(
+            data[0],
+            data[1],
+            Integer.parseInt(data[2]));
+  }
+
+  private String start() {
+    return controller.startGame();
+  }
+
+  private String report() {
+    return this.controller.report();
+  }
 }
