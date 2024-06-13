6ffe004fa9cd0a612e634aa8c61c4550f5b5192c
wolfie <yavorhristozov@gmail.com>
2024-06-13 19:26:35 +0300

    add : RegionImpl class

---
+++ 13. Exams practice/Java OOP Retake Exam - 19 December 2022/01. High Quality Structure + 02. Business Logic/MagicGameApp/src/main/java/magicGame/models/region/RegionImpl.java
@@ -0,0 +1,12 @@
+package magicGame.models.region;
+
+import magicGame.models.magicians.Magician;
+
+import java.util.Collection;
+
+public class RegionImpl implements Region {
+  @Override
+  public String start(Collection<Magician> magicians) {
+    return null;
+  }
+}
