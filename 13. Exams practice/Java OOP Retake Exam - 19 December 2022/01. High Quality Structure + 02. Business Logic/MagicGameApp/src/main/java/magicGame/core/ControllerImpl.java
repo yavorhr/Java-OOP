8038a3dcfe0572159e68e37835d390c256fa6c98
wolfie <yavorhristozov@gmail.com>
2024-06-13 19:33:07 +0300

    add : ControllerImpl class
    impl of Controller

---
+++ 13. Exams practice/Java OOP Retake Exam - 19 December 2022/01. High Quality Structure + 02. Business Logic/MagicGameApp/src/main/java/magicGame/core/ControllerImpl.java
@@ -0,0 +1,23 @@
+package magicGame.core;
+
+public class ControllerImpl implements Controller{
+  @Override
+  public String addMagic(String type, String name, int bulletsCount) {
+    return null;
+  }
+
+  @Override
+  public String addMagician(String type, String username, int health, int protection, String magicName) {
+    return null;
+  }
+
+  @Override
+  public String startGame() {
+    return null;
+  }
+
+  @Override
+  public String report() {
+    return null;
+  }
+}
