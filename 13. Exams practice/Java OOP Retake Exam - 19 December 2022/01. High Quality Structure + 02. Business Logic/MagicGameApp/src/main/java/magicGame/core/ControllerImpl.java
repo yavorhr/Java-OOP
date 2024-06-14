8c392f5275228668c6737c5269c9cc47baf5e6bd
wolfie <yavorhristozov@gmail.com>
2024-06-14 12:20:07 +0300

    add : ControllerImpl dependencies

--- 13. Exams practice/Java OOP Retake Exam - 19 December 2022/01. High Quality Structure + 02. Business Logic/MagicGameApp/src/main/java/magicGame/core/ControllerImpl.java
+++ 13. Exams practice/Java OOP Retake Exam - 19 December 2022/01. High Quality Structure + 02. Business Logic/MagicGameApp/src/main/java/magicGame/core/ControllerImpl.java
@@ -1,6 +1,25 @@
 package magicGame.core;
 
-public class ControllerImpl implements Controller{
+import magicGame.models.magicians.Magician;
+import magicGame.models.magics.Magic;
+import magicGame.models.region.Region;
+import magicGame.models.region.RegionImpl;
+import magicGame.repositories.MagicRepositoryImpl;
+import magicGame.repositories.interfaces.MagicRepository;
+import magicGame.repositories.interfaces.MagicianRepository;
+import magicGame.repositories.interfaces.MagicianRepositoryImpl;
+
+public class ControllerImpl implements Controller {
+  private MagicRepository<Magic> magicRepository;
+  private MagicianRepository<Magician> magicianRepository;
+  private Region region;
+
+  public ControllerImpl() {
+    this.magicRepository = new MagicRepositoryImpl();
+    this.magicianRepository = new MagicianRepositoryImpl();
+    this.region = new RegionImpl();
+  }
+
   @Override
   public String addMagic(String type, String name, int bulletsCount) {
     return null;
--- 13. Exams practice/Java OOP Retake Exam - 19 December 2022/01. High Quality Structure + 02. Business Logic/MagicGameApp/src/main/java/magicGame/models/region/RegionImpl.java
+++ 13. Exams practice/Java OOP Retake Exam - 19 December 2022/01. High Quality Structure + 02. Business Logic/MagicGameApp/src/main/java/magicGame/models/region/RegionImpl.java
@@ -5,6 +5,11 @@ import magicGame.models.magicians.Magician;
 import java.util.Collection;
 
 public class RegionImpl implements Region {
+
+  public RegionImpl() {
+
+  }
+
   @Override
   public String start(Collection<Magician> magicians) {
     return null;
