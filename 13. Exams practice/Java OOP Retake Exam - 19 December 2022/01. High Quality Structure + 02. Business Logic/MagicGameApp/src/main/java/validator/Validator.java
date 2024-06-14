b97c77d1c9c32c6c0ba4505792035764394e3707
wolfie <yavorhristozov@gmail.com>
2024-06-14 12:13:37 +0300

    add : Magician validations
    Validator class

--- 13. Exams practice/Java OOP Retake Exam - 19 December 2022/01. High Quality Structure + 02. Business Logic/MagicGameApp/src/main/java/validator/Validator.java
+++ 13. Exams practice/Java OOP Retake Exam - 19 December 2022/01. High Quality Structure + 02. Business Logic/MagicGameApp/src/main/java/validator/Validator.java
@@ -1,6 +1,7 @@
 package validator;
 
 import magicGame.common.ExceptionMessages;
+import magicGame.models.magics.Magic;
 
 public class Validator {
   public static void validateMagicName(String name) {
@@ -12,6 +13,30 @@ public class Validator {
   public static void validateBulletsCount(int count) {
     if (count < 0) {
       throw new IllegalArgumentException(ExceptionMessages.INVALID_MAGIC_BULLETS_COUNT);
+    }
+  }
+
+  public static void validateMagicianName(String name) {
+    if (name == null || name.trim().isEmpty()) {
+      throw new NullPointerException(ExceptionMessages.INVALID_MAGICIAN_NAME);
+    }
+  }
+
+  public static void validateHealthPoints(int healthPoints) {
+    if (healthPoints < 0) {
+      throw new IllegalArgumentException(ExceptionMessages.INVALID_MAGICIAN_HEALTH);
+    }
+  }
+
+  public static void validateProtectionPoints(int protectionPoints) {
+    if (protectionPoints < 0) {
+      throw new IllegalArgumentException(ExceptionMessages.INVALID_MAGICIAN_PROTECTION);
+    }
+  }
+
+  public static void validateMagic(Magic magic) {
+    if (magic == null) {
+      throw new NullPointerException(ExceptionMessages.INVALID_MAGIC);
     }
   }
 
