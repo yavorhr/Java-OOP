bbe281649e3750ccb4a2221f12dfdeb0a59f8427
wolfie <yavorhristozov@gmail.com>
2024-06-15 09:53:15 +0300

    refact : start()
    RegionImpl

--- 13. Exams practice/Java OOP Retake Exam - 19 December 2022/01. High Quality Structure + 02. Business Logic/MagicGameApp/src/main/java/magicGame/models/region/RegionImpl.java
+++ 13. Exams practice/Java OOP Retake Exam - 19 December 2022/01. High Quality Structure + 02. Business Logic/MagicGameApp/src/main/java/magicGame/models/region/RegionImpl.java
@@ -1,5 +1,6 @@
 package magicGame.models.region;
 
+import magicGame.common.OutputMessages;
 import magicGame.models.magicians.Magician;
 
 import java.util.Collection;
@@ -18,32 +19,30 @@ public class RegionImpl implements Region {
     Collection<Magician> wizards = getMagicians(magicians, "Wizard");
     Collection<Magician> blackWidows = getMagicians(magicians, "Black Widow");
 
-    while (!wizards.isEmpty() || !blackWidows.isEmpty()) {
-      for (Magician attacker : wizards) {
-        for (Magician defender : blackWidows) {
-          int damage = attacker.getMagic().fire();
-
-          if (damage == 0) {
-            break;
-          }
-          defender.takeDamage(damage);
-        }
-      }
+    while (aliveMagiciansLeft(wizards) && aliveMagiciansLeft(blackWidows)) {
+      fight(wizards, blackWidows);
 
       blackWidows = filterAliveMagicians(blackWidows);
+
+      fight(blackWidows, wizards);
+
+      wizards = filterAliveMagicians(wizards);
     }
 
-
-    return null;
+    return getOutput(wizards, blackWidows);
   }
 
-  //Helpers
+  // Helpers
+
+  private boolean aliveMagiciansLeft(Collection<Magician> magicians) {
+    return magicians.stream().anyMatch(Magician::isAlive);
+  }
 
   private List<Magician> filterAliveMagicians(Collection<Magician> magicians) {
     return magicians
             .stream()
             .filter(Magician::isAlive)
-            .collect(Collectors.toList());;
+            .collect(Collectors.toList());
   }
 
   private List<Magician> getMagicians(Collection<Magician> magicians, String type) {
@@ -51,5 +50,25 @@ public class RegionImpl implements Region {
             .stream()
             .filter(m -> m.getClass().getSimpleName().equals(type))
             .collect(Collectors.toList());
+  }
+
+  private String getOutput(Collection<Magician> wizards, Collection<Magician> blackWidows) {
+    if (!aliveMagiciansLeft(wizards)) {
+      return "Black widows win!";
+    }
+    return "Wizards win!";
+  }
+
+  private void fight(Collection<Magician> wizards, Collection<Magician> blackWidows) {
+    for (Magician attacker : wizards) {
+      for (Magician defender : blackWidows) {
+        int damage = attacker.getMagic().fire();
+
+        if (damage == 0) {
+          break;
+        }
+        defender.takeDamage(damage);
+      }
+    }
   }
 }
