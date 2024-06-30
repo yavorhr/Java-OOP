428e005a7015b21045122eec8c3a0b40020a033a
wolfie <yavorhristozov@gmail.com>
2024-06-30 16:49:49 +0300

    add : setIp() func

--- 13. Exams practice/Java OOP Retake Exam - 19 December 2022/03. Unit testing/UnitTesting/src/test/java/magicGame/MagicianTests.java
+++ 13. Exams practice/Java OOP Retake Exam - 19 December 2022/03. Unit testing/UnitTesting/src/test/java/magicGame/MagicianTests.java
@@ -1,5 +1,21 @@
 package magicGame;
 
+import org.junit.Before;
+
 public class MagicianTests {
-    //TODO
+  private Magician magician;
+  private Magician magicianWithNoMagics;
+  private Magic magic1;
+  private Magic magic2;
+
+  @Before
+  public void setUp() {
+    this.magicianWithNoMagics = new Magician("test", 100);
+
+    this.magician = new Magician("Harry", 50);
+    this.magic1 = new Magic("Oslo", 30);
+    this.magic2 = new Magic("Predator", 50);
+    this.magician.addMagic(magic1);
+    this.magician.addMagic(magic2);
+  }
 }
