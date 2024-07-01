4d941572ca6a48a70b142659eeffcf0556be7de4
wolfie <yavorhristozov@gmail.com>
2024-07-01 17:50:00 +0300

    refact : SportsCar

--- 13. Exams practice/Java OOP Retake Exam - 20 December 2021/01. High Quality Structure + 02. Business Logic/ChristmasRaces/src/main/java/christmasRaces/entities/cars/SportsCar.java
+++ 13. Exams practice/Java OOP Retake Exam - 20 December 2021/01. High Quality Structure + 02. Business Logic/ChristmasRaces/src/main/java/christmasRaces/entities/cars/SportsCar.java
@@ -4,12 +4,12 @@ package christmasRaces.entities.cars;
 import validations.Validator;
 
 public class SportsCar extends BaseCar {
-  private static final double CUBIC_CM = 3000;
+  private static final double CUBIC_CENTIMETERS = 3000;
   private static final int MIN_HORSE_POWER = 250;
   private static final int MAX_HORSE_POWER = 450;
 
   public SportsCar(String model, int horsePower) {
-    super(model, horsePower, CUBIC_CM);
+    super(model, horsePower, CUBIC_CENTIMETERS);
   }
 
   @Override
