04bc5fee7ac6cddd8b9a2d3717d68ae21e5e2428
wolfie <yavorhristozov@gmail.com>
2024-07-01 16:38:41 +0300

    add : BaseCar class
    abstract class

---
+++ 13. Exams practice/Java OOP Retake Exam - 20 December 2021/01. High Quality Structure + 02. Business Logic/ChristmasRaces/src/main/java/christmasRaces/entities/cars/BaseCar.java
@@ -0,0 +1,33 @@
+package christmasRaces.entities.cars;
+
+public abstract class BaseCar implements Car {
+  private String model;
+  private int horsePower;
+  private double cubicCentimeters;
+
+  protected BaseCar(String model, int horsePower, double cubicCentimeters) {
+    this.model = model;
+    this.horsePower = horsePower;
+    this.cubicCentimeters = cubicCentimeters;
+  }
+
+  @Override
+  public String getModel() {
+    return this.model;
+  }
+
+  @Override
+  public int getHorsePower() {
+    return this.horsePower;
+  }
+
+  @Override
+  public double getCubicCentimeters() {
+    return this.cubicCentimeters;
+  }
+
+  @Override
+  public double calculateRacePoints(int laps) {
+    return this.cubicCentimeters / this.horsePower * laps;
+  }
+}
