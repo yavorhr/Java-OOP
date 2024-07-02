f3c914e380b92b34847208a14b27e7a3673e2394
wolfie <yavorhristozov@gmail.com>
2024-07-02 15:23:44 +0300

    add : Car validations

--- 13. Exams practice/Java OOP Retake Exam - 20 December 2021/01. High Quality Structure + 02. Business Logic/ChristmasRaces/src/main/java/validations/Validator.java
+++ 13. Exams practice/Java OOP Retake Exam - 20 December 2021/01. High Quality Structure + 02. Business Logic/ChristmasRaces/src/main/java/validations/Validator.java
@@ -1,12 +1,19 @@
 package validations;
 
 import christmasRaces.common.ExceptionMessages;
+import christmasRaces.entities.cars.Car;
 
 public class Validator {
   // CAR
   public static void throwExceptionIfInvalidHorsePower(int horsePower, int lowerBound, int upperBound) {
     if (horsePower < lowerBound || horsePower > upperBound) {
       throw new IllegalArgumentException(String.format(ExceptionMessages.INVALID_HORSE_POWER, horsePower));
+    }
+  }
+
+  public static void throwExceptionIfCarIsNull(Car car) {
+    if (car == null) {
+      throw new IllegalArgumentException(ExceptionMessages.CAR_INVALID);
     }
   }
 }
