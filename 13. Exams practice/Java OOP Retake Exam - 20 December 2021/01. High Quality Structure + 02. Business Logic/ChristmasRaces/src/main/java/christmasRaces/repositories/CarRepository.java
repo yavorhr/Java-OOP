d75b67c15bf58a476de40bd170c70078ab34c29b
wolfie <yavorhristozov@gmail.com>
2024-07-02 15:32:24 +0300

    add : CarRepository

---
+++ 13. Exams practice/Java OOP Retake Exam - 20 December 2021/01. High Quality Structure + 02. Business Logic/ChristmasRaces/src/main/java/christmasRaces/repositories/CarRepository.java
@@ -0,0 +1,39 @@
+package christmasRaces.repositories;
+
+import christmasRaces.entities.cars.Car;
+import christmasRaces.repositories.interfaces.Repository;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class CarRepository implements Repository<Car> {
+  List<Car> cars;
+
+  public CarRepository() {
+    this.cars = new ArrayList<>();
+  }
+
+  @Override
+  public Car getByName(String model) {
+    return this.cars.stream()
+            .filter(c -> c.getModel().equals(model))
+            .findFirst()
+            .orElse(null);
+  }
+
+  @Override
+  public Collection<Car> getAll() {
+    return this.cars;
+  }
+
+  @Override
+  public void add(Car car) {
+    this.cars.add(car);
+  }
+
+  @Override
+  public boolean remove(Car car) {
+    return this.cars.remove(car);
+  }
+}
