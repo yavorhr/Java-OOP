723cb1ebdadfa0e07da33447a083831983417a21
wolfie <yavorhristozov@gmail.com>
2024-07-02 15:36:05 +0300

    add : init ControllerImpl class

---
+++ 13. Exams practice/Java OOP Retake Exam - 20 December 2021/01. High Quality Structure + 02. Business Logic/ChristmasRaces/src/main/java/christmasRaces/core/ControllerImpl.java
@@ -0,0 +1,49 @@
+package christmasRaces.core;
+
+import christmasRaces.core.interfaces.Controller;
+import christmasRaces.entities.cars.Car;
+import christmasRaces.entities.drivers.Driver;
+import christmasRaces.entities.races.Race;
+import christmasRaces.repositories.interfaces.Repository;
+
+public class ControllerImpl implements Controller {
+  private Repository<Car> cars;
+  private Repository<Driver> drivers;
+  private Repository<Race> races;
+
+  public ControllerImpl(Repository<Driver> drivers, Repository<Car> cars, Repository<Race> races) {
+    this.drivers = drivers;
+    this.cars = cars;
+    this.races = races;
+  }
+
+  @Override
+  public String createDriver(String driver) {
+    return null;
+  }
+
+  @Override
+  public String createCar(String type, String model, int horsePower) {
+    return null;
+  }
+
+  @Override
+  public String addCarToDriver(String driverName, String carModel) {
+    return null;
+  }
+
+  @Override
+  public String addDriverToRace(String raceName, String driverName) {
+    return null;
+  }
+
+  @Override
+  public String startRace(String raceName) {
+    return null;
+  }
+
+  @Override
+  public String createRace(String name, int laps) {
+    return null;
+  }
+}
