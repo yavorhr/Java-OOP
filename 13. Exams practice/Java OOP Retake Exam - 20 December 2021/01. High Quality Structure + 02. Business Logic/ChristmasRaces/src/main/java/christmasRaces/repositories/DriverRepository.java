c92abca2ec6bdb034db057f1f571ca23f4cd6bff
wolfie <yavorhristozov@gmail.com>
2024-07-02 15:32:53 +0300

    add : DriverRepository class

---
+++ 13. Exams practice/Java OOP Retake Exam - 20 December 2021/01. High Quality Structure + 02. Business Logic/ChristmasRaces/src/main/java/christmasRaces/repositories/DriverRepository.java
@@ -0,0 +1,38 @@
+package christmasRaces.repositories;
+
+import christmasRaces.entities.drivers.Driver;
+import christmasRaces.repositories.interfaces.Repository;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class DriverRepository implements Repository<Driver> {
+  List<Driver> drivers;
+
+  public DriverRepository() {
+    this.drivers = new ArrayList<>();
+  }
+
+  @Override
+  public Driver getByName(String name) {
+    return this.drivers.stream()
+            .filter(c -> c.getName().equals(name))
+            .findFirst()
+            .orElse(null);
+  }
+
+  @Override
+  public Collection<Driver> getAll() {
+    return this.drivers;
+  }
+
+  @Override
+  public void add(Driver driver) {
+    this.drivers.add(driver);
+  }
+
+  @Override
+  public boolean remove(Driver driver) {
+    return this.drivers.remove(driver);
+  }
+}
