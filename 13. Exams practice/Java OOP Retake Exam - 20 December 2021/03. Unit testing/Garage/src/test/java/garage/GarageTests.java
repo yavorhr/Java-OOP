0058e41aaec094abb3417678cf85360ec5be38db
wolfie <yavorhristozov@gmail.com>
2024-07-03 17:03:00 +0300

    add : tests setup()

--- 13. Exams practice/Java OOP Retake Exam - 20 December 2021/03. Unit testing/Garage/src/test/java/garage/GarageTests.java
+++ 13. Exams practice/Java OOP Retake Exam - 20 December 2021/03. Unit testing/Garage/src/test/java/garage/GarageTests.java
@@ -1,8 +1,27 @@
 package garage;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class GarageTests {
-    //TODO: Test Garage class
+  private Car bmw;
+  private Car mercedes;
+  private Car trabant;
+  private Garage garage;
+  private Garage emptyGarage;
+
+  @Before
+  public void setUp() {
+    this.garage = new Garage();
+    this.bmw = new Car("BMW", 320, 45000);
+    this.mercedes = new Car("Mercedes-Benz", 280, 50000);
+    this.trabant = new Car("Trabant", 100, 2000);
+
+    this.garage.addCar(this.bmw);
+    this.garage.addCar(this.mercedes);
+    this.garage.addCar(this.trabant);
+
+    this.emptyGarage = new Garage();
+  }
 }
